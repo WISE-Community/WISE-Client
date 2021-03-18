bd7e0aab0330b716e428f62ab58d274178c7ff94
Hiroki Terashima <honchikun@gmail.com>
2021-03-17 18:52:26 -0700

    Updated webpack dependency. Updated path to unit tests.

--- src/test.ts
+++ src/test.ts
@@ -22,7 +22,7 @@ __karma__.loaded = function () {};
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 // Then we find all the tests.
-const context = require.context('../../', true, /\.spec\.ts$/);
+const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
 // Finally, start Karma to run the tests.
