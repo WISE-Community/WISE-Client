65f42f24eadf146ad5f6c446f21abb8e1700ef7e
Hiroki Terashima <honchikun@gmail.com>
2021-04-20 12:57:01 -0700

    Created common parent class for editing component field.
    Modified edit component width/max score/exclude from total score to extend new class.
    #88

--- src/app/authoring-tool/edit-component-exclude-from-total-score/edit-component-exclude-from-total-score.component.ts
+++ src/app/authoring-tool/edit-component-exclude-from-total-score/edit-component-exclude-from-total-score.component.ts
@@ -1,29 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { Subject, Subscription } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-import { TeacherProjectService } from '../../../assets/wise5/services/teacherProjectService';
+import { Component } from '@angular/core';
+import { EditComponentFieldComponent } from '../edit-component-field.component';
 
 @Component({
   selector: 'edit-component-exclude-from-total-score',
   templateUrl: 'edit-component-exclude-from-total-score.component.html'
 })
-export class EditComponentExcludeFromTotalScoreComponent {
-  @Input()
-  authoringComponentContent: any;
-  excludeFromTotalScoreChanged: Subject<string> = new Subject<string>();
-  excludeFromTotalScoreChangedSubscription: Subscription;
-
-  constructor(private ProjectService: TeacherProjectService) {}
-
-  ngOnInit() {
-    this.excludeFromTotalScoreChangedSubscription = this.excludeFromTotalScoreChanged
-      .pipe(debounceTime(1000), distinctUntilChanged())
-      .subscribe(() => {
-        this.ProjectService.componentChanged();
-      });
-  }
-
-  ngOnDestroy() {
-    this.excludeFromTotalScoreChangedSubscription.unsubscribe();
-  }
-}
+export class EditComponentExcludeFromTotalScoreComponent extends EditComponentFieldComponent {}
