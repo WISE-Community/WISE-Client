bdc2186751c08e13ceb7d41aafa4e53f2ba9e88a
Hiroki Terashima <honchikun@gmail.com>
2021-05-10 11:55:52 -0700

    Extracted component save and submit button.
    Updated components that have been already converted to angular:
    Label, MC, and AudioOscillator.
    
    #128

--- src/app/student-hybrid-angular.module.ts
+++ src/app/student-hybrid-angular.module.ts
@@ -21,11 +21,13 @@ import { MultipleChoiceStudent } from '../assets/wise5/components/multipleChoice
 import { OutsideUrlStudent } from '../assets/wise5/components/outsideURL/outside-url-student/outside-url-student.component';
 import { AudioOscillatorStudent } from '../assets/wise5/components/audioOscillator/audio-oscillator-student/audio-oscillator-student.component';
 import { LabelStudent } from '../assets/wise5/components/label/label-student/label-student.component';
+import { ComponentSaveSubmitButtons } from '../assets/wise5/directives/component-save-submit-buttons/component-save-submit-buttons.component';
 
 @NgModule({
   declarations: [
     AudioOscillatorStudent,
     ComponentAnnotationsComponent,
+    ComponentSaveSubmitButtons,
     HtmlStudent,
     LabelStudent,
     MultipleChoiceStudent,
