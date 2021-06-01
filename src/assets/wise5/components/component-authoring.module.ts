d163f95ff63eab3a2440b25902f99177d4799428
Hiroki Terashima <honchikun@gmail.com>
2021-06-01 10:30:15 -0700

    Separated authoring tool module.
    Separated code into project/node/component-authoring.module files.
    No new functionality was added.
    
    #171

--- src/assets/wise5/components/component-authoring.module.ts
+++ src/assets/wise5/components/component-authoring.module.ts
@@ -14,21 +14,57 @@ import './openResponse/openResponseAuthoringComponentModule';
 import './outsideURL/outsideURLAuthoringComponentModule';
 import './summary/summaryAuthoringComponentModule';
 import './table/tableAuthoringComponentModule';
+import { downgradeComponent } from '@angular/upgrade/static';
+import { EditComponentJsonComponent } from '../../../app/authoring-tool/edit-component-json/edit-component-json.component';
+import { EditComponentMaxScoreComponent } from '../../../app/authoring-tool/edit-component-max-score/edit-component-max-score.component';
+import { EditComponentRubricComponent } from '../../../app/authoring-tool/edit-component-rubric/edit-component-rubric.component';
+import { EditComponentTagsComponent } from '../../../app/authoring-tool/edit-component-tags/edit-component-tags.component';
+import { EditComponentWidthComponent } from '../../../app/authoring-tool/edit-component-width/edit-component-width.component';
+import { EditComponentExcludeFromTotalScoreComponent } from '../../../app/authoring-tool/edit-component-exclude-from-total-score/edit-component-exclude-from-total-score.component';
+import WISELinkAuthoringController from '../authoringTool/wiseLink/wiseLinkAuthoringController';
 
-export default angular.module('componentAuthoringModule', [
-  'animationAuthoringComponentModule',
-  'audioOscillatorAuthoringComponentModule',
-  'conceptMapAuthoringComponentModule',
-  'discussionAuthoringComponentModule',
-  'drawAuthoringComponentModule',
-  'embeddedAuthoringComponentModule',
-  'graphAuthoringComponentModule',
-  'htmlAuthoringComponentModule',
-  'labelAuthoringComponentModule',
-  'matchAuthoringComponentModule',
-  'multipleChoiceAuthoringComponentModule',
-  'openResponseAuthoringComponentModule',
-  'outsideURLAuthoringComponentModule',
-  'summaryAuthoringComponentModule',
-  'tableAuthoringComponentModule'
-]);
+export default angular
+  .module('componentAuthoringModule', [
+    'animationAuthoringComponentModule',
+    'audioOscillatorAuthoringComponentModule',
+    'conceptMapAuthoringComponentModule',
+    'discussionAuthoringComponentModule',
+    'drawAuthoringComponentModule',
+    'embeddedAuthoringComponentModule',
+    'graphAuthoringComponentModule',
+    'htmlAuthoringComponentModule',
+    'labelAuthoringComponentModule',
+    'matchAuthoringComponentModule',
+    'multipleChoiceAuthoringComponentModule',
+    'openResponseAuthoringComponentModule',
+    'outsideURLAuthoringComponentModule',
+    'summaryAuthoringComponentModule',
+    'tableAuthoringComponentModule'
+  ])
+  .controller('WISELinkAuthoringController', WISELinkAuthoringController)
+  .directive(
+    'editComponentExcludeFromTotalScore',
+    downgradeComponent({
+      component: EditComponentExcludeFromTotalScoreComponent
+    }) as angular.IDirectiveFactory
+  )
+  .directive(
+    'editComponentJson',
+    downgradeComponent({ component: EditComponentJsonComponent }) as angular.IDirectiveFactory
+  )
+  .directive(
+    'editComponentRubric',
+    downgradeComponent({ component: EditComponentRubricComponent }) as angular.IDirectiveFactory
+  )
+  .directive(
+    'editComponentTags',
+    downgradeComponent({ component: EditComponentTagsComponent }) as angular.IDirectiveFactory
+  )
+  .directive(
+    'editComponentWidth',
+    downgradeComponent({ component: EditComponentWidthComponent }) as angular.IDirectiveFactory
+  )
+  .directive(
+    'editComponentMaxScore',
+    downgradeComponent({ component: EditComponentMaxScoreComponent }) as angular.IDirectiveFactory
+  );
