94ea0e13c69f948d10598fb09b6bac7be0f8270e
breity <breity10@gmail.com>
2021-08-06 14:49:10 -0700

    Show all periods in Manage Students. #286

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/manageStudents/manage-students/manage-students.component.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/manageStudents/manage-students/manage-students.component.ts
@@ -4,21 +4,30 @@ import { TeacherDataService } from '../../../../services/teacherDataService';
 
 @Component({
   selector: 'manage-students',
+  styleUrls: ['manage-students.component.scss'],
   templateUrl: 'manage-students.component.html'
 })
 export class ManageStudentsComponent {
-  period: any;
+  periods: any[];
   subscriptions: Subscription = new Subscription();
 
   constructor(private TeacherDataService: TeacherDataService) {}
 
   ngOnInit() {
-    this.period = this.TeacherDataService.getCurrentPeriod();
+    this.setVisiblePeriods(this.TeacherDataService.getCurrentPeriod());
     this.subscriptions.add(
       this.TeacherDataService.currentPeriodChanged$.subscribe(({ currentPeriod }) => {
-        this.period = currentPeriod;
+        this.setVisiblePeriods(currentPeriod);
       })
     );
+  }
+
+  setVisiblePeriods(currentPeriod: any): void {
+    if (currentPeriod.periodId === -1) {
+      this.periods = this.TeacherDataService.getPeriods();
+    } else {
+      this.periods = [currentPeriod];
+    }
   }
 
   ngOnDestroy() {
