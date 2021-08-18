d00b73b10662f308bd0c868cfa89e8c899ea364a
breity <breity10@gmail.com>
2021-08-17 17:23:45 -0700

    Added unit tests. #287

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/manageStudents/manage-user/manage-user.component.spec.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/manageStudents/manage-user/manage-user.component.spec.ts
@@ -1,5 +1,7 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { By } from '@angular/platform-browser';
 import { configureTestSuite } from 'ng-bullet';
 import { ConfigService } from '../../../../services/configService';
@@ -7,16 +9,21 @@ import { ManageUserComponent } from './manage-user.component';
 
 class ConfigServiceStub {
   getPermissions() {}
+  getRunId() {
+    return 123;
+  }
 }
 
 let configService: ConfigService;
 let fixture: ComponentFixture<ManageUserComponent>;
 let component: ManageUserComponent;
+let http: HttpTestingController;
 
 describe('ManageUserComponent', () => {
   configureTestSuite(() => {
     TestBed.configureTestingModule({
       declarations: [ManageUserComponent],
+      imports: [HttpClientTestingModule, MatSnackBarModule],
       providers: [
         { provide: ConfigService, useClass: ConfigServiceStub },
         { provide: MatDialog, useValue: {} }
@@ -25,11 +32,13 @@ describe('ManageUserComponent', () => {
     configService = TestBed.inject(ConfigService);
   });
   beforeEach(() => {
+    http = TestBed.inject(HttpTestingController);
     fixture = TestBed.createComponent(ManageUserComponent);
     component = fixture.componentInstance;
     component.user = { id: 1, name: 'oski bear', username: 'oskib0101' };
   });
   userNameDisplay();
+  makeRequestToRemoveStudent();
 });
 
 function userNameDisplay() {
@@ -57,4 +66,16 @@ function spyOnCanViewStudentNames(canView: boolean) {
 
 function getUsername(): string {
   return fixture.debugElement.query(By.css('.username')).nativeElement.textContent;
+}
+
+function makeRequestToRemoveStudent() {
+  describe('preformRemoveUser()', () => {
+    it('should make request to remove student from unit and then to retrieve config', () => {
+      component.performRemoveUser();
+      const req = http.expectOne(`/api/teacher/run/123/student/1/remove`);
+      expect(req.request.method).toEqual('DELETE');
+      req.flush({});
+      http.verify();
+    });
+  });
 }
