cd615900b730cf7901cd0706f103fec2e24d10b3
Geoffrey Kwan <geoffreykwan@gmail.com>
2021-08-19 17:29:17 -0400

    Change student password popup now makes the request to change the student password. #291

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/manageStudents/change-student-password-dialog/change-student-password-dialog.component.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/manageStudents/change-student-password-dialog/change-student-password-dialog.component.ts
@@ -1,8 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ConfigService } from '../../../../services/configService';
 import { passwordMatchValidator } from '../../../../../../app/modules/shared/validators/password-match.validator';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-change-student-password-dialog',
@@ -22,7 +24,13 @@ export class ChangeStudentPasswordDialogComponent implements OnInit {
   isChangingPassword: boolean;
   isTeacherGoogleUser: boolean;
 
-  constructor(protected ConfigService: ConfigService, @Inject(MAT_DIALOG_DATA) public user: any) {}
+  constructor(
+    private ConfigService: ConfigService,
+    private dialog: MatDialog,
+    private http: HttpClient,
+    private snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public user: any
+  ) {}
 
   ngOnInit(): void {
     this.canViewStudentNames = this.ConfigService.getPermissions().canViewStudentNames;
@@ -32,5 +40,40 @@ export class ChangeStudentPasswordDialogComponent implements OnInit {
     }
   }
 
-  changePassword() {}
+  changePassword() {
+    this.isChangingPassword = true;
+    let params = new HttpParams();
+    params = params.set(
+      'newStudentPassword',
+      this.changePasswordForm.controls['newPassword'].value
+    );
+    params = params.set(
+      'teacherPassword',
+      this.changePasswordForm.controls['teacherPassword'].value
+    );
+    this.http
+      .post(
+        `/api/teacher/run/${this.ConfigService.getRunId()}/student/${this.user.id}/change-password`,
+        params
+      )
+      .subscribe(
+        (response) => {
+          this.isChangingPassword = false;
+          let message = '';
+          if (this.canViewStudentNames) {
+            message = $localize`Changed password for ${this.user.name} (${this.user.username}).`;
+          } else {
+            message = $localize`Changed password for Student ${this.user.id}.`;
+          }
+          this.snackBar.open(message);
+          this.dialog.closeAll();
+        },
+        (err) => {
+          this.isChangingPassword = false;
+          this.snackBar.open(
+            $localize`Failed to change student password. Please refresh this page and try again.`
+          );
+        }
+      );
+  }
 }
