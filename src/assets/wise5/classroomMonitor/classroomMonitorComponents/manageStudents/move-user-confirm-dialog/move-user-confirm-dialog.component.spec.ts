3dc3a37f91f4a141ed4e78f7fb7b349badbb7360
Hiroki Terashima <honchikun@gmail.com>
2021-09-01 10:52:31 -0700

    Updated link to new Manage Students page from teacher run listing. Updated MoveUserConfirmDialog test to account for isMovingFromWorkgroup flag

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/manageStudents/move-user-confirm-dialog/move-user-confirm-dialog.component.spec.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/manageStudents/move-user-confirm-dialog/move-user-confirm-dialog.component.spec.ts
@@ -1,4 +1,6 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { MoveUserConfirmDialogComponent } from './move-user-confirm-dialog.component';
 
@@ -8,9 +10,10 @@ describe('MoveUserConfirmDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MoveUserConfirmDialogComponent ]
-    })
-    .compileComponents();
+      declarations: [MoveUserConfirmDialogComponent],
+      providers: [{ provide: MAT_DIALOG_DATA, useValue: true }],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +22,18 @@ describe('MoveUserConfirmDialogComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create and show warning text', () => {
     expect(component).toBeTruthy();
+    expect(getWarningIcon()).toBeTruthy();
   });
+
+  it('warning text should not be shown if not moving from a workgroup', () => {
+    component.isMovingFromWorkgroup = false;
+    fixture.detectChanges();
+    expect(getWarningIcon()).toBeFalsy();
+  });
+
+  function getWarningIcon() {
+    return fixture.debugElement.nativeElement.querySelector('mat-icon[color="warn"]');
+  }
 });
