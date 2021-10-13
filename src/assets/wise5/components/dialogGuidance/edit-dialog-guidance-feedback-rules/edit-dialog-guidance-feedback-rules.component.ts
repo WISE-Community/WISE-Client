b3abad90e014d62d9269e42e6c1340ebc1647ba3
Hiroki Terashima <honchikun@gmail.com>
2021-10-13 15:04:39 -0700

    feat(Dialog Guidance): Add author feedback rules (#348)
    Provide authoring tools for Dialog Guidance components that allow authors to:
    - add, edit, delete, and order rules
    - specify rule expression and feedback text
    - specify max submission count
    
    Previously, authors needed to directly edit the component json to author these settings.
    
    Co-authored-by: breity <breity10@gmail.com>
    
    Closes #347.

--- src/assets/wise5/components/dialogGuidance/edit-dialog-guidance-feedback-rules/edit-dialog-guidance-feedback-rules.component.ts
+++ src/assets/wise5/components/dialogGuidance/edit-dialog-guidance-feedback-rules/edit-dialog-guidance-feedback-rules.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { TeacherProjectService } from '../../../services/teacherProjectService';
 
 @Component({
   selector: 'edit-dialog-guidance-feedback-rules',
@@ -6,10 +10,53 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./edit-dialog-guidance-feedback-rules.component.scss']
 })
 export class EditDialogGuidanceFeedbackRulesComponent implements OnInit {
-  @Input()
-  feedbackRules: any;
+  @Input() feedbackRules: any = [];
+  inputChanged: Subject<string> = new Subject<string>();
+  subscriptions: Subscription = new Subscription();
 
-  constructor() {}
+  constructor(private ProjectService: TeacherProjectService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.inputChanged.pipe(debounceTime(1000), distinctUntilChanged()).subscribe(() => {
+        this.ProjectService.nodeChanged();
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+  drop(event: CdkDragDrop<string[]>): void {
+    this.moveRuleItem(event.previousIndex, event.currentIndex);
+  }
+
+  moveUp(ruleIndex: number): void {
+    this.moveRuleItem(ruleIndex, ruleIndex - 1);
+  }
+
+  moveDown(ruleIndex: number): void {
+    this.moveRuleItem(ruleIndex, ruleIndex + 1);
+  }
+
+  moveRuleItem(previousIndex: number, currentIndex: number): void {
+    moveItemInArray(this.feedbackRules, previousIndex, currentIndex);
+    this.ProjectService.nodeChanged();
+  }
+
+  addNewRule(position: string): void {
+    const newFeedbackRule = { expression: '', feedback: '' };
+    if (position === 'beginning') {
+      this.feedbackRules.unshift(newFeedbackRule);
+    } else {
+      this.feedbackRules.push(newFeedbackRule);
+    }
+    this.ProjectService.nodeChanged();
+  }
+
+  deleteRule(ruleIndex: number): void {
+    this.feedbackRules.splice(ruleIndex, 1);
+    this.ProjectService.nodeChanged();
+  }
 }
