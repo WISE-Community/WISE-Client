e219eb7ebd8aa3f8fa3eedce03df3ea3ad151013
Geoffrey Kwan <geoffreykwan@gmail.com>
2021-12-13 16:59:04 -0500

    feat(Peer Chat): Add manual grouping option to Peer Chat authoring
    #424

--- src/assets/wise5/components/peerChat/peer-chat-authoring/peer-chat-authoring.component.spec.ts
+++ src/assets/wise5/components/peerChat/peer-chat-authoring/peer-chat-authoring.component.spec.ts
@@ -1,8 +1,15 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UpgradeModule } from '@angular/upgrade/static';
 import { Observable, Subject } from 'rxjs';
+import { EditComponentPrompt } from '../../../../../app/authoring-tool/edit-component-prompt/edit-component-prompt.component';
 import { ProjectAssetService } from '../../../../../app/services/projectAssetService';
 import { ConfigService } from '../../../services/configService';
 import { NodeService } from '../../../services/nodeService';
@@ -39,14 +46,24 @@ const componentContent = {
   secondPrompt: 'Discuss. Use the Question Bank.'
 };
 
-describe('PeerChatAuthoringComponent', () => {
+fdescribe('PeerChatAuthoringComponent', () => {
   let component: PeerChatAuthoringComponent;
   let fixture: ComponentFixture<PeerChatAuthoringComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, MatDialogModule, UpgradeModule],
-      declarations: [PeerChatAuthoringComponent],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientTestingModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatSelectModule,
+        UpgradeModule
+      ],
+      declarations: [EditComponentPrompt, PeerChatAuthoringComponent],
       providers: [
         ConfigService,
         { provide: NodeService, useClass: MockNodeService },
@@ -79,7 +96,63 @@ describe('PeerChatAuthoringComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  function addLogic() {
+    it('should add grouping logic', () => {
+      const logic = component.authoringComponentContent.logic;
+      expect(logic.length).toEqual(1);
+      component.addLogic();
+      expect(logic.length).toEqual(2);
+    });
+  }
+
+  function deleteLogic() {
+    it('should not delete grouping logic when there is only one', () => {
+      const logic = component.authoringComponentContent.logic;
+      expect(logic.length).toEqual(1);
+      const alertSpy = spyOn(window, 'alert');
+      component.deleteLogic(0);
+      expect(alertSpy).toHaveBeenCalled();
+      expect(logic.length).toEqual(1);
+    });
+
+    it('should ask to delete grouping logic when there is more than one', () => {
+      const logic = component.authoringComponentContent.logic;
+      logic.push({ name: 'random' });
+      const confirmSpy = spyOn(window, 'confirm').and.returnValue(true);
+      expect(logic.length).toEqual(2);
+      component.deleteLogic(0);
+      expect(confirmSpy).toHaveBeenCalled();
+      expect(logic.length).toEqual(1);
+    });
+  }
+
+  function logicNameChanged() {
+    it('should change logic name to random', () => {
+      shouldChangeLogicNameWithNoNodeIdComponentId('random');
+    });
+
+    it('should change logic name to manual', () => {
+      shouldChangeLogicNameWithNoNodeIdComponentId('manual');
+    });
+  }
+
+  function shouldChangeLogicNameWithNoNodeIdComponentId(logicName: string): void {
+    const logicObject = component.authoringComponentContent.logic[0];
+    expectLogicName(component.authoringComponentContent.logic[0], 'maximizeSimilarIdeas');
+    expect(logicObject.nodeId).not.toBeNull();
+    expect(logicObject.componentId).not.toBeNull();
+    logicObject.name = logicName;
+    component.logicNameChanged(logicObject);
+    expectLogicName(component.authoringComponentContent.logic[0], logicName);
+    expect(logicObject.nodeId).toBeUndefined();
+    expect(logicObject.componentId).toBeUndefined();
+  }
+
+  function expectLogicName(logicObject: any, expectedName: string): void {
+    expect(logicObject.name).toEqual(expectedName);
+  }
+
+  addLogic();
+  deleteLogic();
+  logicNameChanged();
 });
