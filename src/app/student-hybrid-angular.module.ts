743ac519a55bbbc43131549a656200698e3db080
Hiroki Terashima <honchikun@gmail.com>
2022-01-05 14:20:20 -0800

    chore(VLE): Convert component.ts to Angular
    Removed unused attributes like mode and instead hard-code it
    to 'student' in the component itself since only students use this component

--- src/app/student-hybrid-angular.module.ts
+++ src/app/student-hybrid-angular.module.ts
@@ -35,12 +35,14 @@ import { SummaryDisplay } from '../assets/wise5/directives/summaryDisplay/summar
 import { HighchartsChartModule } from 'highcharts-angular';
 import { GraphStudent } from '../assets/wise5/components/graph/graph-student/graph-student.component';
 import { DialogGuidanceStudentModule } from '../assets/wise5/components/dialogGuidance/dialogGuidanceStudentModule';
+import { ComponentComponent } from '../assets/wise5/components/component/component.component';
 import { StepToolsComponent } from '../assets/wise5/themes/default/themeComponents/stepTools/step-tools.component';
 
 @NgModule({
   declarations: [
     AnimationStudent,
     AudioOscillatorStudent,
+    ComponentComponent,
     ConceptMapStudent,
     DiscussionStudent,
     DrawStudent,
