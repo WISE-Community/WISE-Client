28d8c360159f18dce9c48668c98ed161f57d429a
Hiroki Terashima <honchikun@gmail.com>
2022-01-06 16:47:17 -0800

    chore(Match): Remove AngularJSModule import
    Now, only the necessary dependencies are imported in the
    MatchCommonModule.
    
    #449

--- src/assets/wise5/components/match/match-common.module.ts
+++ src/assets/wise5/components/match/match-common.module.ts
@@ -1,23 +1,39 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AngularJSModule } from '../../../../app/common-hybrid-angular.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { DeleteChoiceButton } from './delete-choice-button/delete-choice-button.component';
 import { MatchChoiceItem } from './match-choice-item/match-choice-item.component';
 import { MatchStatusIcon } from './match-status-icon/match-status-icon.component';
 import { MatchFeedbackSection } from './match-student/match-feedback-section/match-feedback-section.component';
 
 @NgModule({
-  declarations: [
-    DeleteChoiceButton,
-    MatchChoiceItem,
-    MatchFeedbackSection,
-    MatchStatusIcon
+  declarations: [DeleteChoiceButton, MatchChoiceItem, MatchFeedbackSection, MatchStatusIcon],
+  imports: [
+    CommonModule,
+    DragDropModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule
   ],
-  imports: [AngularJSModule],
   exports: [
+    CommonModule,
     DeleteChoiceButton,
+    DragDropModule,
+    FlexLayoutModule,
+    MatCardModule,
     MatchChoiceItem,
     MatchFeedbackSection,
-    MatchStatusIcon
+    MatchStatusIcon,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule
   ]
 })
 export class MatchCommonModule {}
--- src/assets/wise5/components/match/match-grading/match-grading-module.ts
+++ src/assets/wise5/components/match/match-grading/match-grading-module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { AngularJSModule } from '../../../../../app/common-hybrid-angular.module';
 import { MatchGrading } from './match-grading.component';
 import { MatchCommonModule } from '../match-common.module';
 
 @NgModule({
   declarations: [MatchGrading],
-  imports: [AngularJSModule, MatchCommonModule],
+  imports: [MatchCommonModule],
   exports: [MatchGrading]
 })
 export class MatchGradingModule {}
