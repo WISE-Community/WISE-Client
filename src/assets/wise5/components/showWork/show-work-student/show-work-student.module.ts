78871736c5bc0b3960681c86c4efe29825ea7e32
Hiroki Terashima <honchikun@gmail.com>
2022-01-11 12:18:07 -0800

    Add more components that can be shown in ShowWork component

--- src/assets/wise5/components/showWork/show-work-student/show-work-student.module.ts
+++ src/assets/wise5/components/showWork/show-work-student/show-work-student.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { AngularJSModule } from '../../../../../app/common-hybrid-angular.module';
+import { ConceptMapStudentModule } from '../../conceptMap/concept-map-student/concept-map-student.module';
+import { DialogGuidanceStudentModule } from '../../dialogGuidance/dialogGuidanceStudentModule';
+import { DiscussionStudentModule } from '../../discussion/discussion-student/discussion-student.module';
+import { DrawStudentModule } from '../../draw/draw-student/draw-student-module';
+import { EmbeddedStudentModule } from '../../embedded/embedded-student/embedded-student.module';
+import { GraphStudentModule } from '../../graph/graph-student/graph-student.module';
+import { LabelStudentModule } from '../../label/label-student/label-student.module';
 import { MatchStudentModule } from '../../match/match-student/match-student.module';
+import { MultipleChoiceStudentModule } from '../../multipleChoice/multiple-choice-student/multiple-choice-student.module';
+import { OpenResponseStudentModule } from '../../openResponse/open-response-student/open-response-student.module';
+import { TableStudentModule } from '../../table/table-student/table-student.module';
 import { ShowWorkStudentComponent } from './show-work-student.component';
 
 @NgModule({
   declarations: [ShowWorkStudentComponent],
-  imports: [MatchStudentModule, AngularJSModule],
+  imports: [
+    AngularJSModule,
+    ConceptMapStudentModule,
+    DialogGuidanceStudentModule,
+    DiscussionStudentModule,
+    DrawStudentModule,
+    EmbeddedStudentModule,
+    GraphStudentModule,
+    LabelStudentModule,
+    MatchStudentModule,
+    MultipleChoiceStudentModule,
+    OpenResponseStudentModule,
+    TableStudentModule
+  ],
   exports: [ShowWorkStudentComponent]
 })
 export class ShowWorkStudentModule {}
