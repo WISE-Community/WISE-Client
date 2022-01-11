660e31a3051fc07b96f9ecb21beeb70d5b84728c
breity <breity10@gmail.com>
2022-01-11 12:16:49 -0800

    chore(Match): Add missing Material modules
    #449

--- src/assets/wise5/components/match/match-common.module.ts
+++ src/assets/wise5/components/match/match-common.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { DeleteChoiceButton } from './delete-choice-button/delete-choice-button.component';
 import { MatchChoiceItem } from './match-choice-item/match-choice-item.component';
 import { MatchStatusIcon } from './match-status-icon/match-status-icon.component';
@@ -17,9 +19,11 @@ import { MatchFeedbackSection } from './match-student/match-feedback-section/mat
     CommonModule,
     DragDropModule,
     FlexLayoutModule,
+    MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     ReactiveFormsModule
   ],
   exports: [
@@ -27,12 +31,14 @@ import { MatchFeedbackSection } from './match-student/match-feedback-section/mat
     DeleteChoiceButton,
     DragDropModule,
     FlexLayoutModule,
+    MatButtonModule,
     MatCardModule,
     MatchChoiceItem,
     MatchFeedbackSection,
     MatchStatusIcon,
     MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     ReactiveFormsModule
   ]
 })
