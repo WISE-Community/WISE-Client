447b55c01888fe8f631e52c360ba6a6fa24c10e1
Hiroki Terashima <honchikun@gmail.com>
2022-01-20 10:29:45 -0800

    feat(ShowWork, ShowGroupWork): Show prompt

--- src/assets/wise5/components/showGroupWork/show-group-work-student/show-group-work-student.module.ts
+++ src/assets/wise5/components/showGroupWork/show-group-work-student/show-group-work-student.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { StudentComponentModule } from '../../../../../app/student/student.component.module';
 import { ShowWorkStudentModule } from '../../showWork/show-work-student/show-work-student.module';
 import { ShowGroupWorkStudentComponent } from './show-group-work-student.component';
 
 @NgModule({
   declarations: [ShowGroupWorkStudentComponent],
-  imports: [CommonModule, ShowWorkStudentModule],
+  imports: [CommonModule, ShowWorkStudentModule, StudentComponentModule],
   exports: [ShowGroupWorkStudentComponent]
 })
 export class ShowGroupWorkStudentModule {}
