f685c34340e1180a399fe786bea647dcc28310e1
Hiroki Terashima <honchikun@gmail.com>
2022-01-25 18:24:46 -0800

    Create ShowWorkModule for each component
    Rename component-grading.module to component-show-work.module

--- src/assets/wise5/components/discussion/discussion-show-work/discussion-show-work.module.ts
+++ src/assets/wise5/components/discussion/discussion-show-work/discussion-show-work.module.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AngularJSModule } from '../../../../../app/common-hybrid-angular.module';
 import { StudentComponentModule } from '../../../../../app/student/student.component.module';
 import { DiscussionCommonModule } from '../discussion-common.module';
 import { DiscussionShowWorkComponent } from './discussion-show-work.component';
 
 @NgModule({
   declarations: [DiscussionShowWorkComponent],
-  imports: [AngularJSModule, DiscussionCommonModule, StudentComponentModule],
+  imports: [CommonModule, DiscussionCommonModule, StudentComponentModule],
   exports: [DiscussionShowWorkComponent]
 })
 export class DiscussionShowWorkModule {}
