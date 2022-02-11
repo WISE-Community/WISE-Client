0ede92fda47afdcb4092772d6cea52c4b07da5c9
Geoffrey Kwan <geoffreykwan@gmail.com>
2022-02-11 11:40:22 -0800

    test(Dialog Guidance): Add tests for computer avatar authoring
    #491

--- src/assets/wise5/components/dialogGuidance/dialog-guidance-authoring/dialog-guidance-authoring.component.spec.ts
+++ src/assets/wise5/components/dialogGuidance/dialog-guidance-authoring/dialog-guidance-authoring.component.spec.ts
@@ -13,6 +13,7 @@ import { Observable, Subject } from 'rxjs';
 import { EditComponentMaxSubmitComponent } from '../../../../../app/authoring-tool/edit-component-max-submit/edit-component-max-submit.component';
 import { EditComponentPrompt } from '../../../../../app/authoring-tool/edit-component-prompt/edit-component-prompt.component';
 import { ProjectAssetService } from '../../../../../app/services/projectAssetService';
+import { ComputerAvatar } from '../../../common/ComputerAvatar';
 import { ComputerAvatarService } from '../../../services/computerAvatarService';
 import { ConfigService } from '../../../services/configService';
 import { NodeService } from '../../../services/nodeService';
@@ -32,6 +33,7 @@ class MockNodeService {
 }
 
 describe('DialogGuidanceAuthoringComponent', () => {
+  let allComputerAvatars: ComputerAvatar[];
   let component: DialogGuidanceAuthoringComponent;
   let fixture: ComponentFixture<DialogGuidanceAuthoringComponent>;
 
@@ -75,6 +77,12 @@ describe('DialogGuidanceAuthoringComponent', () => {
     spyOn(TestBed.inject(ProjectService), 'getComponentByNodeIdAndComponentId').and.returnValue(
       JSON.parse(JSON.stringify(componentContent))
     );
+    allComputerAvatars = [
+      new ComputerAvatar('robot', 'Robot', 'robot.png'),
+      new ComputerAvatar('monkey', 'Monkey', 'Monkey.png'),
+      new ComputerAvatar('girl', 'Girl', 'girl.png')
+    ];
+    spyOn(TestBed.inject(ComputerAvatarService), 'getAvatars').and.returnValue(allComputerAvatars);
     spyOn(
       TestBed.inject(TeacherProjectService),
       'getComponentByNodeIdAndComponentId'
@@ -83,8 +91,62 @@ describe('DialogGuidanceAuthoringComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should enable computer avatars for the first time', () => {
+    expect(component.authoringComponentContent.computerAvatarIds).toBe(undefined);
+    component.authoringComponentContent.isComputerAvatarEnabled = true;
+    component.enableComputerAvatarClicked();
+    expect(component.authoringComponentContent.computerAvatarIds).toEqual({
+      girl: true,
+      monkey: true,
+      robot: true
+    });
+  });
+
+  it('should enable computer avatars when they have previously been enabled', () => {
+    const computerAvatarIds = {
+      girl: true,
+      robot: true
+    };
+    component.authoringComponentContent.computerAvatarIds = computerAvatarIds;
+    component.authoringComponentContent.isComputerAvatarEnabled = true;
+    component.enableComputerAvatarClicked();
+    expect(component.authoringComponentContent.computerAvatarIds).toEqual(computerAvatarIds);
+  });
+
+  it('should select all computer avatars', () => {
+    component.authoringComponentContent.computerAvatarIds = {};
+    expect(isAllComputerAvatarsUnselected(component.allComputerAvatars)).toEqual(true);
+    component.selectAllComputerAvatars();
+    expect(isAllComputerAvatarsSelected(component.allComputerAvatars)).toEqual(true);
+  });
+
+  it('should unselect all computer avatars', () => {
+    component.authoringComponentContent.computerAvatarIds = {
+      girl: true,
+      robot: true
+    };
+    component.unselectAllComputerAvatars();
+    expect(isAllComputerAvatarsUnselected(component.allComputerAvatars)).toEqual(true);
+  });
+
+  it('should toggle selection of a computer avatar', () => {
+    component.authoringComponentContent.computerAvatarIds = {};
+    const computerAvatar: ComputerAvatar = component.allComputerAvatars[0];
+    computerAvatar.isSelected = false;
+    component.toggleSelectComputerAvatar(computerAvatar);
+    expect(computerAvatar.isSelected).toEqual(true);
+  });
+
+  it('should save selected computer avatars', () => {
+    component.authoringComponentContent.computerAvatarIds = {};
+    component.allComputerAvatars[0].isSelected = true;
+    component.allComputerAvatars[1].isSelected = false;
+    component.allComputerAvatars[2].isSelected = true;
+    component.saveSelectedComputerAvatars();
+    expect(component.authoringComponentContent.computerAvatarIds).toEqual({
+      girl: true,
+      robot: true
+    });
   });
 });
 
@@ -97,4 +159,22 @@ function createComponentContent() {
     showSaveButton: false,
     showSubmitButton: false
   };
+}
+
+function isAllComputerAvatarsSelected(computerAvatars: ComputerAvatar[]): boolean {
+  for (const computerAvatar of computerAvatars) {
+    if (!computerAvatar.isSelected) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isAllComputerAvatarsUnselected(computerAvatars: ComputerAvatar[]): boolean {
+  for (const computerAvatar of computerAvatars) {
+    if (computerAvatar.isSelected) {
+      return false;
+    }
+  }
+  return true;
 }
