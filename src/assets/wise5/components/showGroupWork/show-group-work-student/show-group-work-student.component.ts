dc4b744c83c36cf3fbdcbf93d34117b35dc72694
Geoffrey Kwan <geoffreykwan@gmail.com>
2022-02-15 10:48:34 -0800

    fix(Show Group Work): Properly hide my work when authored to (#502)
    * fix(Show Group Work): Properly hide my work when authored to
    
    #501
    
    * Remove from peer group membership instead of work
    
    Using ngIf to remove the cards without work resulted in empty spaces
    left behind. If showMyWork is set to false, it should remove my
    workgroup from the ngFor loop altogether.
    
    Co-authored-by: Hiroki Terashima <honchikun@gmail.com>

--- src/assets/wise5/components/showGroupWork/show-group-work-student/show-group-work-student.component.ts
+++ src/assets/wise5/components/showGroupWork/show-group-work-student/show-group-work-student.component.ts
@@ -25,7 +25,6 @@ export class ShowGroupWorkStudentComponent extends ComponentStudent {
   showWorkComponentContent: any;
   showWorkComponentId: string;
   showWorkNodeId: string;
-  studentWorkFromGroupMembers: any[];
   widthLg: number = 100;
   widthMd: number = 100;
   workgroupIdToWork = new Map();
@@ -61,8 +60,10 @@ export class ShowGroupWorkStudentComponent extends ComponentStudent {
     super.ngOnInit();
     this.peerGroupService
       .retrievePeerGroup(this.componentContent.peerGroupActivityTag)
-      .subscribe((peerGroup) => {
-        this.peerGroup = peerGroup;
+      .subscribe((peerGroup: PeerGroup) => {
+        this.peerGroup = this.componentContent.isShowMyWork
+          ? peerGroup
+          : this.removeMyWorkgroup(peerGroup);
         this.setWorkgroupInfos();
         this.peerGroupService
           .retrieveStudentWork(
@@ -89,19 +90,17 @@ export class ShowGroupWorkStudentComponent extends ComponentStudent {
   }
 
   setStudentWorkFromGroupMembers(studentWorkFromGroupMembers: any[]): void {
-    this.studentWorkFromGroupMembers = this.componentContent.isShowMyWork
-      ? studentWorkFromGroupMembers
-      : this.removeMyWork(studentWorkFromGroupMembers);
-    this.studentWorkFromGroupMembers.forEach((work) => {
+    studentWorkFromGroupMembers.forEach((work) => {
       this.workgroupIdToWork.set(work.workgroupId, work);
     });
   }
 
-  private removeMyWork(studentWorkList: any[]): any[] {
+  private removeMyWorkgroup(peerGroup: PeerGroup): PeerGroup {
     const myWorkgroupId = this.configService.getWorkgroupId();
-    return studentWorkList.filter((studentWork) => {
-      return studentWork.workgroupId !== myWorkgroupId;
+    peerGroup.members = peerGroup.members.filter((workgroup) => {
+      return workgroup.id !== myWorkgroupId;
     });
+    return peerGroup;
   }
 
   setWorkgroupInfos(): void {
