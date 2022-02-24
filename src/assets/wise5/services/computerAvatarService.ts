b9d8a630608e0385ee2e5097fd1ae2b807d199bd
Jonathan Lim-Breitbart <breity10@gmail.com>
2022-02-23 22:15:28 -0800

    feat(Dialog Guidance): Update set of available avatars
    - Localize avatar names
     #491

--- src/assets/wise5/services/computerAvatarService.ts
+++ src/assets/wise5/services/computerAvatarService.ts
@@ -6,12 +6,16 @@ import { ComputerAvatar } from '../common/ComputerAvatar';
 @Injectable()
 export class ComputerAvatarService {
   avatars: ComputerAvatar[] = [
-    new ComputerAvatar('robot', 'Robot', 'robot.png'),
-    new ComputerAvatar('monkey', 'Monkey', 'monkey.jpg'),
-    new ComputerAvatar('girl', 'Girl', 'robot.png'),
-    new ComputerAvatar('boy', 'Boy', 'monkey.jpg'),
-    new ComputerAvatar('tiger', 'Tiger', 'robot.png'),
-    new ComputerAvatar('koala', 'Koala', 'monkey.jpg')
+    new ComputerAvatar('person1', $localize`:A name for a computer avatar:Ermina`, 'person-1.png'),
+    new ComputerAvatar('person2', $localize`:A name for a computer avatar:Alyx`, 'person-2.png'),
+    new ComputerAvatar('person3', $localize`:A name for a computer avatar:Kai`, 'person-3.png'),
+    new ComputerAvatar('person4', $localize`:A name for a computer avatar:Morgan`, 'person-4.png'),
+    new ComputerAvatar('person5', $localize`:A name for a computer avatar:Parker`, 'person-5.png'),
+    new ComputerAvatar('person6', $localize`:A name for a computer avatar:Milan`, 'person-6.png'),
+    new ComputerAvatar('person7', $localize`:A name for a computer avatar:Emery`, 'person-7.png'),
+    new ComputerAvatar('person8', $localize`:A name for a computer avatar:Yuna`, 'person-8.png'),
+    new ComputerAvatar('robot1', $localize`:A name for a computer avatar:Ada`, 'robot-1.png'),
+    new ComputerAvatar('robot2', $localize`:A name for a computer avatar:Nico`, 'robot-2.png')
   ];
   avatarsPath: string = '/assets/img/computer-avatars/';
 
