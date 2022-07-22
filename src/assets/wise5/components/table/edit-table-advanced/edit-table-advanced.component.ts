20243ea8ae2e20067e0c8f1904b65f654e84493e
Geoffrey Kwan <geoffreykwan@gmail.com>
2022-07-22 16:42:10 -0700

    feat(Table): Allow authors to import table
    #684

--- src/assets/wise5/components/table/edit-table-advanced/edit-table-advanced.component.ts
+++ src/assets/wise5/components/table/edit-table-advanced/edit-table-advanced.component.ts
@@ -11,11 +11,12 @@ import { TeacherProjectService } from '../../../services/teacherProjectService';
 })
 export class EditTableAdvancedComponent extends EditAdvancedComponentComponent {
   allowedConnectedComponentTypes = ['Embedded', 'Graph', 'Table'];
+  columnNames: string[] = [];
   isDataExplorerScatterPlotEnabled: boolean;
   isDataExplorerLineGraphEnabled: boolean;
   isDataExplorerBarGraphEnabled: boolean;
   numColumns: number;
-  columnNames: string[] = [];
+  importTableMessage: string;
 
   constructor(
     protected NodeService: NodeService,
@@ -176,5 +177,131 @@ export class EditTableAdvancedComponent extends EditAdvancedComponentComponent {
         params.yAxis = 0;
       }
     }
+  }
+
+  importTableFile(event: any): void {
+    if (confirm($localize`Are you sure you want to overwrite the existing table?`)) {
+      const files = event.target.files;
+      const reader: any = new FileReader();
+      reader.onload = () => {
+        const fileContent = reader.result;
+        this.importTable(fileContent);
+        event.target.value = null;
+        this.importTableMessage = $localize`Successfully imported table`;
+      };
+      reader.readAsText(files[0]);
+    } else {
+      event.target.value = null;
+    }
+  }
+
+  importTable(fileContent: string): void {
+    const twoDimensionalStringArray = this.csvToArray(fileContent);
+    const tableData = this.convertToTableData(twoDimensionalStringArray);
+    this.authoringComponentContent.tableData = tableData;
+    this.authoringComponentContent.numRows = this.getNumRows(tableData);
+    this.authoringComponentContent.numColumns = this.getNumColumns(tableData);
+    this.componentChanged();
+  }
+
+  /**
+   * This function was obtained from this blog post
+   * https://www.bennadel.com/blog/1504-ask-ben-parsing-csv-strings-with-javascript-exec-regular-expression-command.htm
+   */
+  csvToArray(strData: string, strDelimiter: string = ','): string[][] {
+    // Check to see if the delimiter is defined. If not,
+    // then default to comma.
+    strDelimiter = strDelimiter || ',';
+
+    // Create a regular expression to parse the CSV values.
+    var objPattern = new RegExp(
+      // Delimiters.
+      '(\\' +
+        strDelimiter +
+        '|\\r?\\n|\\r|^)' +
+        // Quoted fields.
+        '(?:"([^"]*(?:""[^"]*)*)"|' +
+        // Standard fields.
+        '([^"\\' +
+        strDelimiter +
+        '\\r\\n]*))',
+      'gi'
+    );
+
+    // Create an array to hold our data. Give the array
+    // a default empty first row.
+    var arrData = [[]];
+
+    // Create an array to hold our individual pattern
+    // matching groups.
+    var arrMatches = null;
+
+    // Keep looping over the regular expression matches
+    // until we can no longer find a match.
+    while ((arrMatches = objPattern.exec(strData))) {
+      // Get the delimiter that was found.
+      var strMatchedDelimiter = arrMatches[1];
+
+      // Check to see if the given delimiter has a length
+      // (is not the start of string) and if it matches
+      // field delimiter. If id does not, then we know
+      // that this delimiter is a row delimiter.
+      if (strMatchedDelimiter.length && strMatchedDelimiter !== strDelimiter) {
+        // Since we have reached a new row of data,
+        // add an empty row to our data array.
+        arrData.push([]);
+      }
+
+      var strMatchedValue;
+
+      // Now that we have our delimiter out of the way,
+      // let's check to see which kind of value we
+      // captured (quoted or unquoted).
+      if (arrMatches[2]) {
+        // We found a quoted value. When we capture
+        // this value, unescape any double quotes.
+        strMatchedValue = arrMatches[2].replace(new RegExp('""', 'g'), '"');
+      } else {
+        // We found a non-quoted value.
+        strMatchedValue = arrMatches[3];
+      }
+
+      // Now that we have our value string, let's add
+      // it to the data array.
+      arrData[arrData.length - 1].push(strMatchedValue);
+    }
+
+    // Return the parsed data.
+    return arrData;
+  }
+
+  convertToTableData(stringArray: string[][]): any[][] {
+    const table = [];
+    for (const row of stringArray) {
+      const tableRow = [];
+      for (const cell of row) {
+        tableRow.push({
+          text: cell,
+          editable: true,
+          size: null
+        });
+      }
+      table.push(tableRow);
+    }
+    return table;
+  }
+
+  getNumRows(tableData: any[][]): number {
+    return tableData.length;
+  }
+
+  getNumColumns(tableData: any[][]): number {
+    let maxColumns = 0;
+    for (const row of tableData) {
+      if (row.length > maxColumns) {
+        maxColumns = row.length;
+      }
+    }
+    return maxColumns;
   }
 }
