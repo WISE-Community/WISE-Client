fcc5bc6da7383de8561e0fe78545752942218be9
Hiroki Terashima <honchikun@gmail.com>
2022-08-25 10:21:00 -0700

    Add comments to IdeaTermEvaluator.evaluate about how it uses the 'true' token #763

--- src/assets/wise5/components/dialogGuidance/TermEvaluator/IdeaTermEvaluator.ts
+++ src/assets/wise5/components/dialogGuidance/TermEvaluator/IdeaTermEvaluator.ts
@@ -7,6 +7,14 @@ export class IdeaTermEvaluator extends TermEvaluator {
   }
 
   evaluate(response: CRaterResponse): boolean {
+    /**
+     * The term 'true' or 'false' is usually not authored, but is created and added to the termStack
+     * by the application as it processes operator expressions like "&&", "||", and "!"
+     * (see DialogGuidanceFeedbackRuleEvaluator.evaluateOperator()).
+     * For example, if the student had ideas 1, 2 and 3, evaluating the postfix expression
+     * ["1", "2", "&&", "3", "&&"] goes like this:
+     * ["1", "2", "&&", "3", "&&"] => ["true", "3", "&&"] => ["true"] => true.
+     */
     return this.term === 'true' || response.getDetectedIdeaNames().includes(this.term);
   }
 }
