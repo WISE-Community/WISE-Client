204ae93cac846dbb295aee5a1fe119ad30032cf9
Geoffrey Kwan <geoffreykwan@gmail.com>
2022-09-09 10:57:03 -0400

    refactor(Classroom Monitor): Remove NotebookItemGrading
    We were not using NotebookItemGrading anymore.
    
    #802

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/notebook/notebook.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/notebook/notebook.ts
@@ -1,13 +1,11 @@
 'use strict';
 
-import NotebookItemGrading from './notebookItemGrading/notebookItemGrading';
 import * as angular from 'angular';
 import { NotebookWorkgroupGradingComponent } from './notebook-workgroup-grading/notebook-workgroup-grading.component';
 import { downgradeComponent } from '@angular/upgrade/static';
 
 const Notebook = angular
   .module('notebookGrading', [])
-  .component('notebookItemGrading', NotebookItemGrading)
   .directive(
     'notebookWorkgroupGrading',
     downgradeComponent({ component: NotebookWorkgroupGradingComponent })
