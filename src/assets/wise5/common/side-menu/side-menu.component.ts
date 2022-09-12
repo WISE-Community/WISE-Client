99682a17b5eff2c5a0e74c2c746058aaf828059b
Geoffrey Kwan <geoffreykwan@gmail.com>
2022-09-12 14:45:22 -0400

    chore(Teacher): Convert Side Menu
    #809

---
+++ src/assets/wise5/common/side-menu/side-menu.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.scss']
+})
+export class SideMenuComponent implements OnInit {
+  @Output() goToView: EventEmitter<any> = new EventEmitter<any>();
+  sortedViews: any[] = [];
+  @Input() state: any;
+  @Input() views: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
