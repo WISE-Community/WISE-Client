5c4437862b0e5d2645faa18d9df07bc76cd2b2c8
Hiroki Terashima <honchikun@gmail.com>
2022-09-19 08:50:39 -0700

    refactor(OpenResponse): Extract AudioRecorder component (#819)

--- src/assets/wise5/components/openResponse/open-response-student/open-response-student.module.ts
+++ src/assets/wise5/components/openResponse/open-response-student/open-response-student.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { StudentTeacherCommonModule } from '../../../../../app/student-teacher-common.module';
 import { StudentComponentModule } from '../../../../../app/student/student.component.module';
+import { AudioRecorderComponent } from '../audio-recorder/audio-recorder.component';
 import { OpenResponseStudent } from './open-response-student.component';
 
 @NgModule({
-  declarations: [OpenResponseStudent],
+  declarations: [AudioRecorderComponent, OpenResponseStudent],
   imports: [StudentTeacherCommonModule, StudentComponentModule],
   exports: [OpenResponseStudent]
 })
