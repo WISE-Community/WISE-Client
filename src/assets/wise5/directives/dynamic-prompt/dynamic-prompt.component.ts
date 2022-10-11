6589077404fbf61f7852c6e8754a39eb0a577f07
Geoffrey Kwan <geoffreykwan@gmail.com>
2022-10-11 15:29:59 -0400

    feat(Dynamic Prompt): Evaluate rules using Peer Group work (#845)

--- src/assets/wise5/directives/dynamic-prompt/dynamic-prompt.component.ts
+++ src/assets/wise5/directives/dynamic-prompt/dynamic-prompt.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CRaterResponse } from '../../components/common/cRater/CRaterResponse';
 import { FeedbackRule } from '../../components/common/feedbackRule/FeedbackRule';
 import { FeedbackRuleEvaluator } from '../../components/common/feedbackRule/FeedbackRuleEvaluator';
 import { FeedbackRuleComponent } from '../../components/feedbackRule/FeedbackRuleComponent';
 import { AnnotationService } from '../../services/annotationService';
 import { ConfigService } from '../../services/configService';
+import { PeerGroupService } from '../../services/peerGroupService';
 import { ProjectService } from '../../services/projectService';
 import { StudentDataService } from '../../services/studentDataService';
 import { DynamicPrompt } from './DynamicPrompt';
@@ -20,6 +23,7 @@ export class DynamicPromptComponent implements OnInit {
   constructor(
     private annotationService: AnnotationService,
     private configService: ConfigService,
+    private peerGroupService: PeerGroupService,
     private projectService: ProjectService,
     private studentDataService: StudentDataService
   ) {}
@@ -38,6 +42,61 @@ export class DynamicPromptComponent implements OnInit {
   }
 
   private evaluateOpenResponseComponent(referenceComponent: any): void {
+    if (this.dynamicPrompt.isPeerGroupingTagSpecified()) {
+      this.evaluatePeerGroupOpenResponse(referenceComponent);
+    } else {
+      this.evaluatePersonalOpenResponse(referenceComponent);
+    }
+  }
+
+  private evaluatePeerGroupOpenResponse(referenceComponent: any): void {
+    const nodeId = this.dynamicPrompt.getReferenceNodeId();
+    const componentId = referenceComponent.id;
+    this.getPeerGroupData(nodeId, componentId).subscribe((peerGroupData: any[]) => {
+      const cRaterResponses = peerGroupData.map((peerMemberData: any) => {
+        return new CRaterResponse({
+          ideas: peerMemberData.annotation.data.ideas,
+          scores: peerMemberData.annotation.data.scores,
+          submitCounter: this.getSubmitCounter(peerMemberData.componentState)
+        });
+      });
+      const feedbackRuleEvaluator = this.getFeedbackRuleEvaluator(
+        this.dynamicPrompt.getRules(),
+        referenceComponent.maxSubmitCount
+      );
+      const feedbackRule: FeedbackRule = feedbackRuleEvaluator.getFeedbackRule(cRaterResponses);
+      this.prompt = feedbackRule.prompt;
+    });
+  }
+
+  private getPeerGroupData(nodeId: string, componentId: string): Observable<any[]> {
+    return this.peerGroupService.retrievePeerGroup(this.dynamicPrompt.getPeerGroupingTag()).pipe(
+      map((peerGroup) => {
+        return this.createDummyData(nodeId, componentId, peerGroup);
+      })
+    );
+  }
+
+  private createDummyData(nodeId: string, componentId: string, peerGroup: any): any[] {
+    const myWorkgroupId = this.configService.getWorkgroupId();
+    return peerGroup.members.map((member: any) => {
+      return {
+        workgroupId: member.id,
+        componentState: this.studentDataService.getLatestComponentStateByNodeIdAndComponentId(
+          nodeId,
+          componentId
+        ),
+        annotation: this.annotationService.getLatestScoreAnnotation(
+          nodeId,
+          componentId,
+          myWorkgroupId,
+          'autoScore'
+        )
+      };
+    });
+  }
+
+  private evaluatePersonalOpenResponse(referenceComponent: any): void {
     const nodeId = this.dynamicPrompt.getReferenceNodeId();
     const componentId = referenceComponent.id;
     const latestComponentState = this.studentDataService.getLatestComponentStateByNodeIdAndComponentId(
