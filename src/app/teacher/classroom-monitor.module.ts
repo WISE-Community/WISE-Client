929fcf19377ac5cdd6eeefe300b39517156aaeb5
Geoffrey Kwan <geoffreykwan@gmail.com>
2022-11-01 15:00:43 -0400

    chore(Classroom Monitor): Convert Top Bar (#875)
    Co-authored-by: Jonathan Lim-Breitbart <breity10@gmail.com>

--- src/app/teacher/classroom-monitor.module.ts
+++ src/app/teacher/classroom-monitor.module.ts
@@ -55,6 +55,7 @@ import { IntersectionObserverModule } from '@ng-web-apis/intersection-observer';
 import { MilestoneGradingViewComponent } from '../../assets/wise5/classroomMonitor/classroomMonitorComponents/milestones/milestone-grading-view/milestone-grading-view.component';
 import { NotificationsMenuComponent } from '../../assets/wise5/classroomMonitor/classroomMonitorComponents/shared/notifications-menu/notifications-menu.component';
 import { NavItemComponent } from '../../assets/wise5/classroomMonitor/classroomMonitorComponents/nodeProgress/nav-item/nav-item.component';
+import { TopBarComponent } from '../../assets/wise5/classroomMonitor/classroomMonitorComponents/shared/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
@@ -97,6 +98,7 @@ import { NavItemComponent } from '../../assets/wise5/classroomMonitor/classroomM
     StudentGradingToolsComponent,
     TeacherSummaryDisplay,
     ToolBarComponent,
+    TopBarComponent,
     ViewComponentRevisionsComponent,
     WorkgroupComponentGradingComponent,
     WorkgroupInfoComponent,
