ff6c4b7aff529f03a66d5bd53b70fa4aa151e074
Geoffrey Kwan <geoffreykwan@gmail.com>
2022-12-08 17:25:13 -0500

    fix(Label Export): Fix problem with revision counter returning NaN
    #923

--- src/assets/wise5/classroomMonitor/dataExport/ComponentRevisionCounter.ts
+++ src/assets/wise5/classroomMonitor/dataExport/ComponentRevisionCounter.ts
@@ -1,5 +1,8 @@
 export class ComponentRevisionCounter extends Map<string, number> {
   incrementCounter(key: string): void {
+    if (!this.has(key)) {
+      this.set(key, 1);
+    }
     this.set(key, this.get(key) + 1);
   }
 }
