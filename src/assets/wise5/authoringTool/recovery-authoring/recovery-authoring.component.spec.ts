7dd4581aa63235e94597d4d7cf12337cb1b5d296
Geoffrey Kwan <geoffreykwan@gmail.com>
2023-01-13 15:34:00 -0500

    feat(Recovery Authoring): Add button to go to Authoring view
    Also add tests.
    
    #922

--- src/assets/wise5/authoringTool/recovery-authoring/recovery-authoring.component.spec.ts
+++ src/assets/wise5/authoringTool/recovery-authoring/recovery-authoring.component.spec.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UpgradeModule } from '@angular/upgrade/static';
 import { StudentTeacherCommonServicesModule } from '../../../../app/student-teacher-common-services.module';
 import { TeacherProjectService } from '../../services/teacherProjectService';
 import { RecoveryAuthoringComponent } from './recovery-authoring.component';
@@ -12,12 +13,31 @@ class MockTeacherProjectService {
   project = {
     nodes: []
   };
+
+  saveProject() {}
 }
 
-describe('RecoveryAuthoringComponent', () => {
-  let component: RecoveryAuthoringComponent;
-  let fixture: ComponentFixture<RecoveryAuthoringComponent>;
+class Node {
+  id: string;
+  ids: string[];
+  transitionLogic: any;
 
+  constructor(id: string, ids: string[], transitions: any[]) {
+    this.id = id;
+    this.ids = ids;
+    this.transitionLogic = {
+      transitions: transitions
+    };
+  }
+}
+
+let component: RecoveryAuthoringComponent;
+let fixture: ComponentFixture<RecoveryAuthoringComponent>;
+const groupId1 = 'group1';
+const nodeId1 = 'node1';
+const nodeId2 = 'node2';
+
+describe('RecoveryAuthoringComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RecoveryAuthoringComponent],
@@ -27,7 +47,8 @@ describe('RecoveryAuthoringComponent', () => {
         HttpClientTestingModule,
         MatDialogModule,
         MatInputModule,
-        StudentTeacherCommonServicesModule
+        StudentTeacherCommonServicesModule,
+        UpgradeModule
       ],
       providers: [{ provide: TeacherProjectService, useClass: MockTeacherProjectService }]
     }).compileComponents();
@@ -39,7 +60,68 @@ describe('RecoveryAuthoringComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  projectJSONChanged();
+  save();
 });
+
+function projectJSONChanged() {
+  describe('projectJSONChanged', () => {
+    it('should detect json is invalid and disable save button', () => {
+      component.projectJSONString = 'abc';
+      component.projectJSONChanged();
+      expect(component.jsonIsValid).toBeFalsy();
+      expect(component.saveButtonEnabled).toBeFalsy();
+    });
+
+    it('should detect json is valid and enable save button', () => {
+      component.projectJSONString = '{ "nodes": [] }';
+      component.projectJSONChanged();
+      expect(component.jsonIsValid).toBeTruthy();
+      expect(component.saveButtonEnabled).toBeTruthy();
+    });
+
+    it('should detect potential problem transition to null', () => {
+      const projectJSON = {
+        nodes: [new Node(nodeId1, null, [{ to: null }])]
+      };
+      component.projectJSONString = JSON.stringify(projectJSON);
+      component.projectJSONChanged();
+      expect(component.badNodes.length).toEqual(1);
+      expect(component.badNodes[0].hasTransitionToNull).toEqual(true);
+    });
+
+    it('should detect potential problem reference to node id that does not exist', () => {
+      const projectJSON = {
+        nodes: [new Node(groupId1, [nodeId1, nodeId2], []), new Node(nodeId1, null, [])]
+      };
+      component.projectJSONString = JSON.stringify(projectJSON);
+      component.projectJSONChanged();
+      expect(component.badNodes.length).toEqual(1);
+      expect(component.badNodes[0].referencedIdsThatDoNotExist).toEqual([nodeId2]);
+    });
+
+    it('should detect potential problem reference to node id duplicate', () => {
+      const projectJSON = {
+        nodes: [
+          new Node(groupId1, [nodeId1, nodeId1], []),
+          new Node(nodeId1, null, [{ to: nodeId2 }]),
+          new Node(nodeId2, null, [])
+        ]
+      };
+      component.projectJSONString = JSON.stringify(projectJSON);
+      component.projectJSONChanged();
+      expect(component.badNodes.length).toEqual(1);
+      expect(component.badNodes[0].referencedIdsThatAreDuplicated).toEqual([nodeId1]);
+    });
+  });
+}
+
+function save() {
+  describe('save', () => {
+    it('should save and disable save button', () => {
+      component.saveButtonEnabled = true;
+      component.save();
+      expect(component.saveButtonEnabled).toBeFalsy();
+    });
+  });
+}
--- src/assets/wise5/authoringTool/recovery-authoring/recovery-authoring.component.ts
+++ src/assets/wise5/authoringTool/recovery-authoring/recovery-authoring.component.ts
@@ -4,6 +4,8 @@ import { NotificationService } from '../../services/notificationService';
 import { TeacherProjectService } from '../../services/teacherProjectService';
 import { UtilService } from '../../services/utilService';
 import { NodeRecoveryAnalysis } from '../../../../app/domain/nodeRecoveryAnalysis';
+import { UpgradeModule } from '@angular/upgrade/static';
+import { ConfigService } from '../../services/configService';
 
 @Component({
   selector: 'recovery-authoring',
@@ -13,24 +15,18 @@ import { NodeRecoveryAnalysis } from '../../../../app/domain/nodeRecoveryAnalysi
 export class RecoveryAuthoringComponent implements OnInit {
   badNodes: NodeRecoveryAnalysis[] = [];
   globalMessage: any;
+  jsonIsValid: boolean;
   projectJSONString: string;
   saveButtonEnabled: boolean = false;
   subscriptions: Subscription = new Subscription();
-  jsonIsValid: boolean;
 
   constructor(
+    private configService: ConfigService,
     private notificationService: NotificationService,
     private projectService: TeacherProjectService,
+    private upgrade: UpgradeModule,
     private utilService: UtilService
   ) {}
-
-  subscribeToGlobalMessage(): void {
-    this.subscriptions.add(
-      this.notificationService.setGlobalMessage$.subscribe(({ globalMessage }) => {
-        this.globalMessage = globalMessage;
-      })
-    );
-  }
 
   ngOnInit(): void {
     this.projectJSONString = JSON.stringify(this.projectService.project, null, 4);
@@ -39,46 +35,63 @@ export class RecoveryAuthoringComponent implements OnInit {
     this.checkNodes();
   }
 
-  ngDestroy(): void {}
+  ngDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 
   projectJSONChanged(): void {
     this.checkProjectJSONValidity();
     this.saveButtonEnabled = this.jsonIsValid;
-    this.checkNodes();
+    if (this.jsonIsValid) {
+      this.checkNodes();
+    }
   }
 
-  checkProjectJSONValidity(): void {
+  private checkProjectJSONValidity(): void {
     this.jsonIsValid = this.utilService.isValidJSONString(this.projectJSONString);
   }
 
-  checkNodes(): void {
+  private subscribeToGlobalMessage(): void {
+    this.subscriptions.add(
+      this.notificationService.setGlobalMessage$.subscribe(({ globalMessage }) => {
+        this.globalMessage = globalMessage;
+      })
+    );
+  }
+
+  private checkNodes(): void {
     const project = JSON.parse(this.projectJSONString);
     const nodeIdsFound = project.nodes.map((node: any) => {
       return node.id;
     });
     this.badNodes = [];
     for (const node of project.nodes) {
-      const nodeRecoveryAnalysis = new NodeRecoveryAnalysis(node.id);
-      if (node.ids != null) {
-        const nodesReferencesInGroup = new Map();
-        for (const referencedId of node.ids) {
-          if (!nodeIdsFound.includes(referencedId)) {
-            nodeRecoveryAnalysis.addReferencedIdThatDoesNotExist(referencedId);
-          }
-          if (nodesReferencesInGroup.get(referencedId)) {
-            nodeRecoveryAnalysis.addReferencedIdThatIsDuplicated(referencedId);
-          }
-          nodesReferencesInGroup.set(referencedId, true);
-        }
-      }
-      nodeRecoveryAnalysis.setHasTransitionToNull(this.hasNullTransition(node));
+      const nodeRecoveryAnalysis = this.getNodeRecoveryAnalysis(node, nodeIdsFound);
       if (nodeRecoveryAnalysis.hasProblem()) {
         this.badNodes.push(nodeRecoveryAnalysis);
       }
     }
   }
 
-  hasNullTransition(node: any): boolean {
+  private getNodeRecoveryAnalysis(node: any, nodeIdsFound: string[]): NodeRecoveryAnalysis {
+    const nodeRecoveryAnalysis = new NodeRecoveryAnalysis(node.id);
+    if (node.ids != null) {
+      const nodesReferencesInGroup = new Map();
+      for (const referencedId of node.ids) {
+        if (!nodeIdsFound.includes(referencedId)) {
+          nodeRecoveryAnalysis.addReferencedIdThatDoesNotExist(referencedId);
+        }
+        if (nodesReferencesInGroup.get(referencedId)) {
+          nodeRecoveryAnalysis.addReferencedIdThatIsDuplicated(referencedId);
+        }
+        nodesReferencesInGroup.set(referencedId, true);
+      }
+    }
+    nodeRecoveryAnalysis.setHasTransitionToNull(this.hasTransitionToNull(node));
+    return nodeRecoveryAnalysis;
+  }
+
+  private hasTransitionToNull(node: any): boolean {
     return node.transitionLogic.transitions.some((transition: any) => {
       return transition.to == null;
     });
@@ -88,5 +101,11 @@ export class RecoveryAuthoringComponent implements OnInit {
     this.projectService.project = JSON.parse(this.projectJSONString);
     this.projectService.saveProject();
     this.saveButtonEnabled = false;
+  }
+
+  goToAuthoringView(): void {
+    this.upgrade.$injector
+      .get('$state')
+      .go('root.at.project', { projectId: this.configService.getProjectId() });
   }
 }
