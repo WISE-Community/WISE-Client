9662c42ec11286bb1fbed8fe7a5fbc4da455c2cc
Hiroki Terashima <honchikun@gmail.com>
2023-01-23 09:46:21 -0800

    Split test into individual functions #963

--- src/assets/wise5/common/object/object.spec.ts
+++ src/assets/wise5/common/object/object.spec.ts
@@ -1,14 +1,40 @@
 import { copy } from './object';
 
 describe('copy()', () => {
-  it('should copy arrays and objects', () => {
+  copy_array_returnDeepCopy();
+  copy_object_returnDeepCopy();
+  copy_null_returnNull();
+  copy_undefined_returnUndefined();
+});
+
+function copy_array_returnDeepCopy(): void {
+  it('should deep copy an array', () => {
     const arr = [1, 2, 3];
-    expect(copy(arr)).toEqual(arr);
-    const obj = { name: 'WISE', address: 'Berkeley' };
-    expect(copy(obj)).toEqual(obj);
+    const arrCopy = copy(arr);
+    expect(arrCopy).toEqual(arr);
+    arrCopy.push(4);
+    expect(arrCopy).not.toEqual(arr);
   });
-  it('should handle null and undefined inputs', () => {
+}
+
+function copy_object_returnDeepCopy(): void {
+  it('should deep copy an object', () => {
+    const obj = { name: 'WISE', address: 'Berkeley' };
+    const objCopy = copy(obj);
+    expect(objCopy).toEqual(obj);
+    objCopy.id = 1;
+    expect(objCopy).not.toEqual(obj);
+  });
+}
+
+function copy_null_returnNull(): void {
+  it('should return null for null input', () => {
     expect(copy(null)).toEqual(null);
+  });
+}
+
+function copy_undefined_returnUndefined(): void {
+  it('should return undefined for undefined input', () => {
     expect(copy(undefined)).toEqual(undefined);
   });
-});
+}
