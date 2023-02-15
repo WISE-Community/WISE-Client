d6793bf909ef82c6b22dfe535a559097fb72dca5
Jonathan Lim-Breitbart <breity10@gmail.com>
2023-02-15 09:31:27 -0800

    Remove fdescribe in tests #1010

--- src/assets/wise5/common/array/array.spec.ts
+++ src/assets/wise5/common/array/array.spec.ts
@@ -1,7 +1,7 @@
 import { CSVToArray } from './array';
 import csvArraySample from './arraySample';
 
-fdescribe('CSVToArray()', () => {
+describe('CSVToArray()', () => {
   it('should convert a delimited string to an array of arrays', () => {
     const arrayData = CSVToArray(csvArraySample.delimitedString);
     expect(arrayData).toEqual(csvArraySample.convertedArray);
