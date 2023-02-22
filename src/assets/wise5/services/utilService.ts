cbb551bf74eaadef1e6134da279923dbf05fa5be
Hiroki Terashima <honchikun@gmail.com>
2023-02-21 17:38:07 -0800

    refactor(UtilService): Move temporarilyHighlightElement()
    #1026

--- src/assets/wise5/services/utilService.ts
+++ src/assets/wise5/services/utilService.ts
@@ -233,43 +233,6 @@ export class UtilService {
     return false;
   }
 
-  /**
-   * Temporarily highlight an element in the DOM.
-   * @param id The id of the element.
-   * @param duration The number of milliseconds to keep the element highlighted.
-   */
-  temporarilyHighlightElement(id, duration = 1000) {
-    let element = $('#' + id);
-    let originalBackgroundColor = element.css('backgroundColor');
-    element.css('background-color', '#FFFF9C');
-
-    /*
-     * Use a timeout before starting to transition back to
-     * the original background color. For some reason the
-     * element won't get highlighted in the first place
-     * unless this timeout is used.
-     */
-    setTimeout(() => {
-      // slowly fade back to the original background color
-      element.css({
-        transition: 'background-color 2s ease-in-out',
-        'background-color': originalBackgroundColor
-      });
-
-      /*
-       * remove these styling fields after we perform
-       * the fade otherwise the regular mouseover
-       * background color change will not work
-       */
-      setTimeout(() => {
-        element.css({
-          transition: '',
-          'background-color': ''
-        });
-      }, 2000);
-    }, duration);
-  }
-
   calculateMean(values) {
     return values.reduce((a, b) => a + b) / values.length;
   }
