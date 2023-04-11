e9345676d7fe8ff2c9de6bf02de1180b3bd080a2
Geoffrey Kwan <geoffreykwan@gmail.com>
2023-04-11 10:51:54 -0400

    refactor(Constraints): Clean up tests
    #1135

--- src/assets/wise5/authoringTool/constraint/node-constraint-authoring/node-constraint-authoring.component.spec.ts
+++ src/assets/wise5/authoringTool/constraint/node-constraint-authoring/node-constraint-authoring.component.spec.ts
@@ -18,6 +18,7 @@ import { NodeConstraintAuthoringComponent } from './node-constraint-authoring.co
 let component: NodeConstraintAuthoringComponent;
 let fixture: ComponentFixture<NodeConstraintAuthoringComponent>;
 const nodeId1 = 'node1';
+let removalCriteria: any;
 
 describe('NodeConstraintAuthoringComponent', () => {
   beforeEach(async () => {
@@ -81,100 +82,138 @@ function addRemovalCriteria() {
 }
 
 function removalCriteriaNameChanged() {
-  describe('removalCriteriaNameChanged', () => {
-    let removalCriteria: any;
-
-    function setRemovalCriteriaName(name: string): any {
-      removalCriteria = component.constraint.removalCriteria[0];
-      removalCriteria.name = name;
-      component.removalCriteriaNameChanged(removalCriteria);
-    }
-
-    function expectEmptyRemovalCriterialParamValue(...paramNames: string[]) {
-      paramNames.forEach((paramName) => {
-        expect(removalCriteria.params[paramName]).toEqual('');
-      });
-    }
-
-    function expectRemovalCriteriaNodeIdValueToEqualNode1() {
-      expect(removalCriteria.params['nodeId']).toEqual(nodeId1);
-    }
-
-    it('should handle removal criteria name changed to isCompleted', () => {
-      setRemovalCriteriaName('isCompleted');
-      expectRemovalCriteriaNodeIdValueToEqualNode1();
-    });
-
-    it('should handle removal criteria name changed to score', () => {
-      setRemovalCriteriaName('score');
-      expectRemovalCriteriaNodeIdValueToEqualNode1();
-      expectEmptyRemovalCriterialParamValue('componentId', 'scores');
-    });
-
-    it('should handle removal criteria name changed to branch path taken', () => {
-      setRemovalCriteriaName('branchPathTaken');
-      expectEmptyRemovalCriterialParamValue('fromNodeId', 'toNodeId');
-    });
-
-    it('should handle removal criteria name changed to choiceChosen', () => {
-      setRemovalCriteriaName('choiceChosen');
-      expectRemovalCriteriaNodeIdValueToEqualNode1();
-      expectEmptyRemovalCriterialParamValue('componentId', 'choiceIds');
-    });
-
-    it('should handle removal criteria name changed to isCorrect', () => {
-      setRemovalCriteriaName('isCorrect');
-      expectRemovalCriteriaNodeIdValueToEqualNode1();
-      expectEmptyRemovalCriterialParamValue('componentId');
-    });
-
-    it('should handle removal criteria name changed to usedXSubmits', () => {
-      setRemovalCriteriaName('usedXSubmits');
-      expectRemovalCriteriaNodeIdValueToEqualNode1();
-      expectEmptyRemovalCriterialParamValue('componentId', 'requiredSubmitCount');
-    });
-
-    it('should handle removal criteria name changed to isVisible', () => {
-      setRemovalCriteriaName('isVisible');
-      expectRemovalCriteriaNodeIdValueToEqualNode1();
-    });
-
-    it('should handle removal criteria name changed to isVisitable', () => {
-      setRemovalCriteriaName('isVisitable');
-      expectRemovalCriteriaNodeIdValueToEqualNode1();
-    });
-
-    it('should handle removal criteria name changed to isVisited', () => {
-      setRemovalCriteriaName('isVisited');
-      expectRemovalCriteriaNodeIdValueToEqualNode1();
-    });
-
-    it('should handle removal criteria name changed to wroteXNumberOfWords', () => {
-      setRemovalCriteriaName('wroteXNumberOfWords');
-      expectRemovalCriteriaNodeIdValueToEqualNode1();
-      expectEmptyRemovalCriterialParamValue('componentId', 'requiredNumberOfWords');
-    });
-
-    it('should handle removal criteria name changed to addXNumberOfNotesOnThisStep', () => {
-      setRemovalCriteriaName('addXNumberOfNotesOnThisStep');
-      expectRemovalCriteriaNodeIdValueToEqualNode1();
-      expectEmptyRemovalCriterialParamValue('requiredNumberOfNotes');
-    });
-
-    it('should handle removal criteria name changed to fillXNumberOfRows', () => {
-      setRemovalCriteriaName('fillXNumberOfRows');
-      expectRemovalCriteriaNodeIdValueToEqualNode1();
-      expectEmptyRemovalCriterialParamValue('componentId');
-      expect(removalCriteria.params['requiredNumberOfFilledRows']).toEqual('');
-      expect(removalCriteria.params['tableHasHeaderRow']).toEqual(true);
-      expect(removalCriteria.params['requireAllCellsInARowToBeFilled']).toEqual(true);
-    });
-
-    it('should handle removal criteria name changed to teacherRemoval', () => {
-      setRemovalCriteriaName('teacherRemoval');
-      expect(removalCriteria.params['nodeId']).toBeUndefined();
-    });
+  describe('removalCriteriaNameChanged()', () => {
+    removalCriteriaNameChangedIsCompleted();
+    removalCriteriaNameChangedScore();
+    removalCriteriaNameChangedBranchPathTaken();
+    removalCriteriaNameChangedChoiceChosen();
+    removalCriteriaNameChangedIsCorrect();
+    removalCriteriaNameChangedUsedXSubmits();
+    removalCriteriaNameChangedIsVisible();
+    removalCriteriaNameChangedIsVisitable();
+    removalCriteriaNameChangedIsVisited();
+    removalCriteriaNameChangedWroteXNumberWords();
+    removalCriteriaNameChangedAddXNumberNotes();
+    removalCriteriaNameChangedFillXNumberRows();
+    removalCriteriaNameChangedTeacherRemoval();
   });
+}
+
+function removalCriteriaNameChangedIsCompleted() {
+  it('should handle removal criteria name changed to isCompleted', () => {
+    setRemovalCriteriaName('isCompleted');
+    expectRemovalCriteriaNodeIdValueToEqualNode1();
+  });
+}
+
+function removalCriteriaNameChangedScore() {
+  it('should handle removal criteria name changed to score', () => {
+    setRemovalCriteriaName('score');
+    expectRemovalCriteriaNodeIdValueToEqualNode1();
+    expectEmptyRemovalCriterialParamValue('componentId', 'scores');
+  });
+}
+
+function removalCriteriaNameChangedBranchPathTaken() {
+  it('should handle removal criteria name changed to branch path taken', () => {
+    setRemovalCriteriaName('branchPathTaken');
+    expectEmptyRemovalCriterialParamValue('fromNodeId', 'toNodeId');
+  });
+}
+
+function removalCriteriaNameChangedChoiceChosen() {
+  it('should handle removal criteria name changed to choiceChosen', () => {
+    setRemovalCriteriaName('choiceChosen');
+    expectRemovalCriteriaNodeIdValueToEqualNode1();
+    expectEmptyRemovalCriterialParamValue('componentId', 'choiceIds');
+  });
+}
+
+function removalCriteriaNameChangedIsCorrect() {
+  it('should handle removal criteria name changed to isCorrect', () => {
+    setRemovalCriteriaName('isCorrect');
+    expectRemovalCriteriaNodeIdValueToEqualNode1();
+    expectEmptyRemovalCriterialParamValue('componentId');
+  });
+}
+
+function removalCriteriaNameChangedUsedXSubmits() {
+  it('should handle removal criteria name changed to usedXSubmits', () => {
+    setRemovalCriteriaName('usedXSubmits');
+    expectRemovalCriteriaNodeIdValueToEqualNode1();
+    expectEmptyRemovalCriterialParamValue('componentId', 'requiredSubmitCount');
+  });
+}
+
+function removalCriteriaNameChangedIsVisible() {
+  it('should handle removal criteria name changed to isVisible', () => {
+    setRemovalCriteriaName('isVisible');
+    expectRemovalCriteriaNodeIdValueToEqualNode1();
+  });
+}
+
+function removalCriteriaNameChangedIsVisitable() {
+  it('should handle removal criteria name changed to isVisitable', () => {
+    setRemovalCriteriaName('isVisitable');
+    expectRemovalCriteriaNodeIdValueToEqualNode1();
+  });
+}
+
+function removalCriteriaNameChangedIsVisited() {
+  it('should handle removal criteria name changed to isVisited', () => {
+    setRemovalCriteriaName('isVisited');
+    expectRemovalCriteriaNodeIdValueToEqualNode1();
+  });
+}
+
+function removalCriteriaNameChangedWroteXNumberWords() {
+  it('should handle removal criteria name changed to wroteXNumberOfWords', () => {
+    setRemovalCriteriaName('wroteXNumberOfWords');
+    expectRemovalCriteriaNodeIdValueToEqualNode1();
+    expectEmptyRemovalCriterialParamValue('componentId', 'requiredNumberOfWords');
+  });
+}
+
+function removalCriteriaNameChangedAddXNumberNotes() {
+  it('should handle removal criteria name changed to addXNumberOfNotesOnThisStep', () => {
+    setRemovalCriteriaName('addXNumberOfNotesOnThisStep');
+    expectRemovalCriteriaNodeIdValueToEqualNode1();
+    expectEmptyRemovalCriterialParamValue('requiredNumberOfNotes');
+  });
+}
+
+function removalCriteriaNameChangedFillXNumberRows() {
+  it('should handle removal criteria name changed to fillXNumberOfRows', () => {
+    setRemovalCriteriaName('fillXNumberOfRows');
+    expectRemovalCriteriaNodeIdValueToEqualNode1();
+    expectEmptyRemovalCriterialParamValue('componentId');
+    expect(removalCriteria.params['requiredNumberOfFilledRows']).toEqual('');
+    expect(removalCriteria.params['tableHasHeaderRow']).toEqual(true);
+    expect(removalCriteria.params['requireAllCellsInARowToBeFilled']).toEqual(true);
+  });
+}
+
+function removalCriteriaNameChangedTeacherRemoval() {
+  it('should handle removal criteria name changed to teacherRemoval', () => {
+    setRemovalCriteriaName('teacherRemoval');
+    expect(removalCriteria.params['nodeId']).toBeUndefined();
+  });
+}
+
+function setRemovalCriteriaName(name: string): any {
+  removalCriteria = component.constraint.removalCriteria[0];
+  removalCriteria.name = name;
+  component.removalCriteriaNameChanged(removalCriteria);
+}
+
+function expectEmptyRemovalCriterialParamValue(...paramNames: string[]): void {
+  paramNames.forEach((paramName) => {
+    expect(removalCriteria.params[paramName]).toEqual('');
+  });
+}
+
+function expectRemovalCriteriaNodeIdValueToEqualNode1(): void {
+  expect(removalCriteria.params['nodeId']).toEqual(nodeId1);
 }
 
 function deleteRemovalCriteria() {
