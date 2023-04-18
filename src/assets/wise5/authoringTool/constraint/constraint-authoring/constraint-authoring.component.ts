fc159633b80d4894f1b0fb7083732f5aa2a42017
Hiroki Terashima <honchikun@gmail.com>
2023-04-18 08:45:34 -0700

    refactor(Constraint Authoring): Extract EditConstraintRemovalCriteriaComponent (#1183)

--- src/assets/wise5/authoringTool/constraint/constraint-authoring/constraint-authoring.component.ts
+++ src/assets/wise5/authoringTool/constraint/constraint-authoring/constraint-authoring.component.ts
@@ -1,87 +1,16 @@
 import { Directive, Input, OnInit } from '@angular/core';
 import { ConstraintAction } from '../../../../../app/domain/constraintAction';
-import { RemovalCriteria } from '../../../../../app/domain/removalCriteria';
-import { RemovalCriteriaParam } from '../../../../../app/domain/removalCriteriaParam';
-import { MultipleChoiceContent } from '../../../components/multipleChoice/MultipleChoiceContent';
 import { TeacherDataService } from '../../../services/teacherDataService';
 import { TeacherProjectService } from '../../../services/teacherProjectService';
 
 @Directive()
 export abstract class ConstraintAuthoringComponent implements OnInit {
-  removalCriteriaComponentParam: RemovalCriteriaParam = new RemovalCriteriaParam(
-    'componentId',
-    $localize`Component`
-  );
-  removalCriteriaStepParam: RemovalCriteriaParam = new RemovalCriteriaParam(
-    'nodeId',
-    $localize`Step`
-  );
-
   @Input() constraint: any;
   constraintActions = [];
   node: any;
-  nodeIds: string[];
   removalConditionals = [
     { value: 'all', text: $localize`All` },
     { value: 'any', text: $localize`Any` }
-  ];
-  removalCriteria = [
-    new RemovalCriteria('', $localize`Please Choose a Removal Criteria`, []),
-    new RemovalCriteria('isCompleted', $localize`Is Completed`, [this.removalCriteriaStepParam]),
-    new RemovalCriteria('score', $localize`Score`, [
-      this.removalCriteriaStepParam,
-      this.removalCriteriaComponentParam,
-      new RemovalCriteriaParam('scores', $localize`Score(s)`)
-    ]),
-    new RemovalCriteria('branchPathTaken', $localize`Branch Path Taken`, [
-      new RemovalCriteriaParam('fromNodeId', $localize`From Step`),
-      new RemovalCriteriaParam('toNodeId', $localize`To Step`)
-    ]),
-    new RemovalCriteria('choiceChosen', $localize`Choice Chosen`, [
-      this.removalCriteriaStepParam,
-      this.removalCriteriaComponentParam,
-      new RemovalCriteriaParam('choiceIds', $localize`Choices`)
-    ]),
-    new RemovalCriteria('isCorrect', $localize`Is Correct`, [
-      this.removalCriteriaStepParam,
-      this.removalCriteriaComponentParam
-    ]),
-    new RemovalCriteria('usedXSubmits', $localize`Used X Submits`, [
-      this.removalCriteriaStepParam,
-      this.removalCriteriaComponentParam,
-      new RemovalCriteriaParam('requiredSubmitCount', $localize`Required Submit Count`)
-    ]),
-    new RemovalCriteria('isVisible', $localize`Is Visible`, [this.removalCriteriaStepParam]),
-    new RemovalCriteria('isVisitable', $localize`Is Visitable`, [this.removalCriteriaStepParam]),
-    new RemovalCriteria('isVisited', $localize`Is Visited`, [this.removalCriteriaStepParam]),
-    new RemovalCriteria('wroteXNumberOfWords', $localize`Wrote X Number of Words`, [
-      this.removalCriteriaStepParam,
-      this.removalCriteriaComponentParam,
-      new RemovalCriteriaParam('requiredNumberOfWords', $localize`Required Number of Words`)
-    ]),
-    new RemovalCriteria(
-      'addXNumberOfNotesOnThisStep',
-      $localize`Add X Number of Notes On This Step`,
-      [
-        this.removalCriteriaStepParam,
-        new RemovalCriteriaParam('requiredNumberOfNotes', $localize`Required Number of Notes`)
-      ]
-    ),
-    new RemovalCriteria('fillXNumberOfRows', $localize`Fill X Number of Rows`, [
-      this.removalCriteriaStepParam,
-      this.removalCriteriaComponentParam,
-      new RemovalCriteriaParam(
-        'requiredNumberOfFilledRows',
-        $localize`Required Number of Filled Rows (Not Including Header Row)`
-      ),
-      new RemovalCriteriaParam('tableHasHeaderRow', $localize`Table Has Header Row`, true),
-      new RemovalCriteriaParam(
-        'requireAllCellsInARowToBeFilled',
-        $localize`Require All Cells In a Row To Be Filled`,
-        true
-      )
-    ]),
-    new RemovalCriteria('teacherRemoval', $localize`Teacher Removes Constraint`, [])
   ];
 
   constructor(
@@ -90,7 +19,6 @@ export abstract class ConstraintAuthoringComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.nodeIds = this.projectService.getFlattenedProjectAsNodeIds(true);
     this.node = this.projectService.getNodeById(this.dataService.getCurrentNodeId());
     this.automaticallySetActionIfPossible();
   }
@@ -109,74 +37,10 @@ export abstract class ConstraintAuthoringComponent implements OnInit {
   }
 
   addRemovalCriteria(constraint: any): void {
-    const removalCriteria = {
+    constraint.removalCriteria.push({
       name: '',
       params: {}
-    };
-    constraint.removalCriteria.push(removalCriteria);
-    this.saveProject();
-  }
-
-  removalCriteriaNameChanged(criteria: any): void {
-    criteria.params = {};
-    const params = this.getRemovalCriteriaParamsByName(criteria.name);
-    for (const paramObject of params) {
-      const value = paramObject.value;
-      criteria.params[value] = paramObject.defaultValue;
-      if (value === 'nodeId') {
-        criteria.params[value] = this.node.id;
-      }
-    }
-    this.saveProject();
-  }
-
-  private getRemovalCriteriaParamsByName(name: string): any[] {
-    for (const singleRemovalCriteria of this.removalCriteria) {
-      if (singleRemovalCriteria.value === name) {
-        return singleRemovalCriteria.params;
-      }
-    }
-    return [];
-  }
-
-  protected constraintRemovalCriteriaNodeIdChanged(criteria: any): void {
-    criteria.params.componentId = '';
-    this.saveProject();
-  }
-
-  deleteRemovalCriteria(constraint: any, removalCriteriaIndex: number): void {
-    if (confirm($localize`Are you sure you want to delete this removal criteria?`)) {
-      constraint.removalCriteria.splice(removalCriteriaIndex, 1);
-      this.saveProject();
-    }
-  }
-
-  protected getChoices(nodeId: string, componentId: string): any[] {
-    return this.projectService.getChoices(nodeId, componentId);
-  }
-
-  protected getChoiceTypeByNodeIdAndComponentId(nodeId: string, componentId: string): string {
-    const component = this.projectService.getComponent(
-      nodeId,
-      componentId
-    ) as MultipleChoiceContent;
-    return component != null && component.choiceType != null ? component.choiceType : null;
-  }
-
-  protected getComponents(nodeId: string): any[] {
-    return this.projectService.getComponents(nodeId);
-  }
-
-  protected getNodeTitle(nodeId: string): string {
-    return this.projectService.getNodeTitle(nodeId);
-  }
-
-  protected getNodePositionById(nodeId: string): string {
-    return this.projectService.getNodePositionById(nodeId);
-  }
-
-  protected scoresChanged(value: any, params: any): void {
-    params.scores = value.split(',');
+    });
     this.saveProject();
   }
 
--- src/assets/wise5/authoringTool/constraint/node-constraint-authoring/node-constraint-authoring.component.spec.ts
+++ src/assets/wise5/authoringTool/constraint/node-constraint-authoring/node-constraint-authoring.component.spec.ts
@@ -2,8 +2,6 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Constraint } from '../../../../../app/domain/constraint';
@@ -12,25 +10,20 @@ import { ClassroomStatusService } from '../../../services/classroomStatusService
 import { TeacherDataService } from '../../../services/teacherDataService';
 import { TeacherProjectService } from '../../../services/teacherProjectService';
 import { TeacherWebSocketService } from '../../../services/teacherWebSocketService';
-import { RequiredErrorLabelComponent } from '../../node/advanced/required-error-label/required-error-label.component';
 import { NodeConstraintAuthoringComponent } from './node-constraint-authoring.component';
 
 let component: NodeConstraintAuthoringComponent;
 let fixture: ComponentFixture<NodeConstraintAuthoringComponent>;
-const nodeId1 = 'node1';
-let removalCriteria: any;
 
 describe('NodeConstraintAuthoringComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [NodeConstraintAuthoringComponent, RequiredErrorLabelComponent],
+      declarations: [NodeConstraintAuthoringComponent],
       imports: [
         BrowserAnimationsModule,
         FormsModule,
         HttpClientTestingModule,
         MatDialogModule,
-        MatFormFieldModule,
-        MatIconModule,
         MatSelectModule,
         StudentTeacherCommonServicesModule
       ],
@@ -55,175 +48,19 @@ describe('NodeConstraintAuthoringComponent', () => {
         }
       ]
     });
-    spyOn(TestBed.inject(TeacherProjectService), 'getFlattenedProjectAsNodeIds').and.returnValue([
-      nodeId1
-    ]);
-    spyOn(TestBed.inject(TeacherProjectService), 'getNodeById').and.returnValue({
-      id: nodeId1
-    });
-    spyOn(TestBed.inject(TeacherDataService), 'getCurrentNodeId').and.returnValue(nodeId1);
     fixture.detectChanges();
   });
 
   addRemovalCriteria();
-  removalCriteriaNameChanged();
-  deleteRemovalCriteria();
 });
 
 function addRemovalCriteria() {
-  describe('addRemovalCriteria', () => {
+  describe('addRemovalCriteria()', () => {
     it('should add a removal criteria', () => {
       const constraint = component.constraint;
       expect(constraint.removalCriteria.length).toEqual(1);
       expect(component.addRemovalCriteria(constraint));
       expect(constraint.removalCriteria.length).toEqual(2);
-    });
-  });
-}
-
-function removalCriteriaNameChanged() {
-  describe('removalCriteriaNameChanged()', () => {
-    removalCriteriaNameChangedIsCompleted();
-    removalCriteriaNameChangedScore();
-    removalCriteriaNameChangedBranchPathTaken();
-    removalCriteriaNameChangedChoiceChosen();
-    removalCriteriaNameChangedIsCorrect();
-    removalCriteriaNameChangedUsedXSubmits();
-    removalCriteriaNameChangedIsVisible();
-    removalCriteriaNameChangedIsVisitable();
-    removalCriteriaNameChangedIsVisited();
-    removalCriteriaNameChangedWroteXNumberWords();
-    removalCriteriaNameChangedAddXNumberNotes();
-    removalCriteriaNameChangedFillXNumberRows();
-    removalCriteriaNameChangedTeacherRemoval();
-  });
-}
-
-function removalCriteriaNameChangedIsCompleted() {
-  it('should handle removal criteria name changed to isCompleted', () => {
-    setRemovalCriteriaName('isCompleted');
-    expectRemovalCriteriaNodeIdValueToEqualNode1();
-  });
-}
-
-function removalCriteriaNameChangedScore() {
-  it('should handle removal criteria name changed to score', () => {
-    setRemovalCriteriaName('score');
-    expectRemovalCriteriaNodeIdValueToEqualNode1();
-    expectEmptyRemovalCriterialParamValue('componentId', 'scores');
-  });
-}
-
-function removalCriteriaNameChangedBranchPathTaken() {
-  it('should handle removal criteria name changed to branch path taken', () => {
-    setRemovalCriteriaName('branchPathTaken');
-    expectEmptyRemovalCriterialParamValue('fromNodeId', 'toNodeId');
-  });
-}
-
-function removalCriteriaNameChangedChoiceChosen() {
-  it('should handle removal criteria name changed to choiceChosen', () => {
-    setRemovalCriteriaName('choiceChosen');
-    expectRemovalCriteriaNodeIdValueToEqualNode1();
-    expectEmptyRemovalCriterialParamValue('componentId', 'choiceIds');
-  });
-}
-
-function removalCriteriaNameChangedIsCorrect() {
-  it('should handle removal criteria name changed to isCorrect', () => {
-    setRemovalCriteriaName('isCorrect');
-    expectRemovalCriteriaNodeIdValueToEqualNode1();
-    expectEmptyRemovalCriterialParamValue('componentId');
-  });
-}
-
-function removalCriteriaNameChangedUsedXSubmits() {
-  it('should handle removal criteria name changed to usedXSubmits', () => {
-    setRemovalCriteriaName('usedXSubmits');
-    expectRemovalCriteriaNodeIdValueToEqualNode1();
-    expectEmptyRemovalCriterialParamValue('componentId', 'requiredSubmitCount');
-  });
-}
-
-function removalCriteriaNameChangedIsVisible() {
-  it('should handle removal criteria name changed to isVisible', () => {
-    setRemovalCriteriaName('isVisible');
-    expectRemovalCriteriaNodeIdValueToEqualNode1();
-  });
-}
-
-function removalCriteriaNameChangedIsVisitable() {
-  it('should handle removal criteria name changed to isVisitable', () => {
-    setRemovalCriteriaName('isVisitable');
-    expectRemovalCriteriaNodeIdValueToEqualNode1();
-  });
-}
-
-function removalCriteriaNameChangedIsVisited() {
-  it('should handle removal criteria name changed to isVisited', () => {
-    setRemovalCriteriaName('isVisited');
-    expectRemovalCriteriaNodeIdValueToEqualNode1();
-  });
-}
-
-function removalCriteriaNameChangedWroteXNumberWords() {
-  it('should handle removal criteria name changed to wroteXNumberOfWords', () => {
-    setRemovalCriteriaName('wroteXNumberOfWords');
-    expectRemovalCriteriaNodeIdValueToEqualNode1();
-    expectEmptyRemovalCriterialParamValue('componentId', 'requiredNumberOfWords');
-  });
-}
-
-function removalCriteriaNameChangedAddXNumberNotes() {
-  it('should handle removal criteria name changed to addXNumberOfNotesOnThisStep', () => {
-    setRemovalCriteriaName('addXNumberOfNotesOnThisStep');
-    expectRemovalCriteriaNodeIdValueToEqualNode1();
-    expectEmptyRemovalCriterialParamValue('requiredNumberOfNotes');
-  });
-}
-
-function removalCriteriaNameChangedFillXNumberRows() {
-  it('should handle removal criteria name changed to fillXNumberOfRows', () => {
-    setRemovalCriteriaName('fillXNumberOfRows');
-    expectRemovalCriteriaNodeIdValueToEqualNode1();
-    expectEmptyRemovalCriterialParamValue('componentId');
-    expect(removalCriteria.params['requiredNumberOfFilledRows']).toEqual('');
-    expect(removalCriteria.params['tableHasHeaderRow']).toEqual(true);
-    expect(removalCriteria.params['requireAllCellsInARowToBeFilled']).toEqual(true);
-  });
-}
-
-function removalCriteriaNameChangedTeacherRemoval() {
-  it('should handle removal criteria name changed to teacherRemoval', () => {
-    setRemovalCriteriaName('teacherRemoval');
-    expect(removalCriteria.params['nodeId']).toBeUndefined();
-  });
-}
-
-function setRemovalCriteriaName(name: string): any {
-  removalCriteria = component.constraint.removalCriteria[0];
-  removalCriteria.name = name;
-  component.removalCriteriaNameChanged(removalCriteria);
-}
-
-function expectEmptyRemovalCriterialParamValue(...paramNames: string[]): void {
-  paramNames.forEach((paramName) => {
-    expect(removalCriteria.params[paramName]).toEqual('');
-  });
-}
-
-function expectRemovalCriteriaNodeIdValueToEqualNode1(): void {
-  expect(removalCriteria.params['nodeId']).toEqual(nodeId1);
-}
-
-function deleteRemovalCriteria() {
-  describe('deleteRemovalCriteria', () => {
-    it('should delete removal criteria', () => {
-      const constraint = component.constraint;
-      expect(constraint.removalCriteria.length).toEqual(1);
-      spyOn(window, 'confirm').and.returnValue(true);
-      component.deleteRemovalCriteria(constraint, 0);
-      expect(constraint.removalCriteria.length).toEqual(0);
     });
   });
 }
