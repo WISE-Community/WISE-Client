d1d52b3a2d0a44fcab3fce5c8e6dfb037354e00d
Jonathan Lim-Breitbart <breity10@gmail.com>
2023-05-05 13:46:44 -0700

    Add typing to input object params

--- src/assets/wise5/common/object/object.ts
+++ src/assets/wise5/common/object/object.ts
@@ -2,6 +2,9 @@ export function copy(obj: any): any {
   return typeof obj === 'undefined' ? undefined : JSON.parse(JSON.stringify(obj));
 }
 
-export function serverSaveTimeComparator(object1: any, object2: any): number {
+export function serverSaveTimeComparator(
+  object1: { serverSaveTime: number },
+  object2: { serverSaveTime: number }
+): number {
   return object1.serverSaveTime - object2.serverSaveTime;
 }
