3f461b9ca9f2cde136382f597672038ddccbd081
Jonathan Lim-Breitbart <breity10@gmail.com>
2023-05-10 11:14:28 -0700

    Removed unused instances of function

--- src/assets/wise5/services/configService.ts
+++ src/assets/wise5/services/configService.ts
@@ -801,22 +801,6 @@ export class ConfigService {
     return content;
   }
 
-  getAvatarColorForWorkgroupId(workgroupId) {
-    const avatarColors = [
-      '#E91E63',
-      '#9C27B0',
-      '#CDDC39',
-      '#2196F3',
-      '#FDD835',
-      '#43A047',
-      '#795548',
-      '#EF6C00',
-      '#C62828',
-      '#607D8B'
-    ];
-    return avatarColors[workgroupId % 10];
-  }
-
   /**
    * Get the project assets folder path
    * @param includeHost whether to include the host in the URL
