f9a204b456269c4f27a5edc849904c69db9aa0ea
Hiroki Terashima <honchikun@gmail.com>
2023-05-21 14:15:41 -0700

    Pass in WISEComponent to PreviewComponentDialogComponent #1275

--- src/assets/wise5/authoringTool/components/preview-component-button/preview-component-button.component.ts
+++ src/assets/wise5/authoringTool/components/preview-component-button/preview-component-button.component.ts
@@ -1,28 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PreviewComponentDialogComponent } from '../preview-component-dialog/preview-component-dialog.component';
+import { ProjectService } from '../../../services/projectService';
+import { ComponentFactory } from '../../../common/ComponentFactory';
 
 @Component({
   selector: 'preview-component-button',
   templateUrl: 'preview-component-button.component.html'
 })
 export class PreviewComponentButtonComponent implements OnInit {
-  @Input()
-  componentId: string;
+  @Input() componentId: string;
+  @Input() nodeId: string;
 
-  @Input()
-  nodeId: string;
-
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private projectService: ProjectService) {}
 
   ngOnInit(): void {}
 
-  popUpComponentPreview(event: any): void {
+  protected popUpComponentPreview(event: any): void {
     event.stopPropagation();
-    const dialogRef = this.dialog.open(PreviewComponentDialogComponent, {
+    const content = this.projectService.injectAssetPaths(
+      this.projectService.getComponent(this.nodeId, this.componentId)
+    );
+    this.dialog.open(PreviewComponentDialogComponent, {
+      data: new ComponentFactory().getComponent(content, this.nodeId),
       panelClass: 'dialog-lg'
     });
-    dialogRef.componentInstance.nodeId = this.nodeId;
-    dialogRef.componentInstance.componentId = this.componentId;
   }
 }
