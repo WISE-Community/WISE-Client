4b0fb26d61dc11219c611f4c598d6bed9e5c07f9
Geoffrey Kwan <geoffreykwan@gmail.com>
2023-05-26 15:21:41 -0400

    refactor(Graph): Clean up table data point code
    #1289

--- src/assets/wise5/components/graph/util.ts
+++ src/assets/wise5/components/graph/util.ts
@@ -18,37 +18,47 @@ export function addPointFromTableIntoData(
 ): void {
   const xText = xCell.text;
   const yText = yCell.text;
-  if (xText != null && xText !== '' && yText != null && yText !== '') {
+  if (!isEmpty(xText) && !isEmpty(yText)) {
     data.push(createDataPointFromTable(xText, yText, tooltipHeader));
   } else {
     data.push([]);
   }
 }
 
+function isEmpty(value: any): boolean {
+  return value == null || value === '';
+}
+
 function createDataPointFromTable(xText: string, yText: string, tooltipHeader: string): any {
-  let point: any;
   const xNumber = Number(xText);
   const yNumber = Number(yText);
-  if (!isNaN(xNumber) && !isNaN(yNumber)) {
-    point = {
-      x: xNumber,
-      y: yNumber
-    };
-    if (tooltipHeader != null) {
-      point.tooltipHeader = tooltipHeader;
-    }
+  return !isNaN(xNumber) && !isNaN(yNumber)
+    ? createPointObject(xNumber, yNumber, tooltipHeader)
+    : createPointArray(xNumber, xText, yNumber);
+}
+
+function createPointObject(xNumber: number, yNumber: number, tooltipHeader: string): any {
+  const point: any = {
+    x: xNumber,
+    y: yNumber
+  };
+  if (tooltipHeader != null) {
+    point.tooltipHeader = tooltipHeader;
+  }
+  return point;
+}
+
+function createPointArray(xNumber: number, xText: string, yNumber: number): any[] {
+  const point = [];
+  if (!isNaN(xNumber)) {
+    point.push(xNumber);
   } else {
-    point = [];
-    if (!isNaN(xNumber)) {
-      point.push(xNumber);
-    } else {
-      point.push(xText);
-    }
-    if (!isNaN(yNumber)) {
-      point.push(yNumber);
-    } else {
-      point.push(null);
-    }
+    point.push(xText);
+  }
+  if (!isNaN(yNumber)) {
+    point.push(yNumber);
+  } else {
+    point.push(null);
   }
   return point;
 }
