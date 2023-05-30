9d9b98985333bc502903fdb8dae9082d076dcb15
Geoffrey Kwan <geoffreykwan@gmail.com>
2023-05-30 12:41:19 -0400

    refactor(Graph): Clean up data explorer manager code
    #1292

--- src/assets/wise5/components/graph/data-explorer-manager.spec.ts
+++ src/assets/wise5/components/graph/data-explorer-manager.spec.ts
@@ -28,68 +28,75 @@ describe('DataExplorerManager', () => {
 
 function handleDataExplorer() {
   describe('handleDataExplorer()', () => {
-    it('should process data explorer student data when x column contains numbers', () => {
-      const studentData = createStudentData(
-        [
-          [{ text: '% in poverty' }, { text: casesColumnName }],
-          [{ text: '10' }, { text: '100' }],
-          [{ text: '20' }, { text: '200' }]
-        ],
-        true
-      );
-      expect(activeTrial.series).toEqual([]);
-      const regressionSeries = component.handleDataExplorer(studentData);
-      expect(activeTrial.series).toEqual([
-        {
-          type: 'scatter',
-          name: casesColumnName,
-          color: blueColor,
-          yAxis: 0,
-          data: [
-            { x: 10, y: 100 },
-            { x: 20, y: 200 }
-          ]
-        }
-      ]);
-      expect(regressionSeries).toEqual([
-        {
-          type: 'line',
-          name: 'Regression Line',
-          color: blueColor,
-          data: [
-            [10, 100],
-            [20, 200]
-          ],
-          yAxis: 0,
-          enableMouseTracking: false
-        }
-      ]);
-    });
+    handleDataExplorerXColumnNumbers();
+    handleDataExplorerXColumnLabels();
+  });
+}
 
-    it('should process data explorer student data when x column contains labels', () => {
-      const studentData = createStudentData(
-        [
-          [{ text: 'County' }, { text: casesColumnName }],
-          [{ text: 'Alameda' }, { text: '100' }],
-          [{ text: 'Contra Costa' }, { text: '200' }]
+function handleDataExplorerXColumnNumbers() {
+  it('should process data explorer student data when x column contains numbers', () => {
+    const studentData = createStudentData(
+      [
+        [{ text: '% in poverty' }, { text: casesColumnName }],
+        [{ text: '10' }, { text: '100' }],
+        [{ text: '20' }, { text: '200' }]
+      ],
+      true
+    );
+    expect(activeTrial.series).toEqual([]);
+    const regressionSeries = component.handleDataExplorer(studentData);
+    expect(activeTrial.series).toEqual([
+      {
+        type: 'scatter',
+        name: casesColumnName,
+        color: blueColor,
+        yAxis: 0,
+        data: [
+          { x: 10, y: 100 },
+          { x: 20, y: 200 }
+        ]
+      }
+    ]);
+    expect(regressionSeries).toEqual([
+      {
+        type: 'line',
+        name: 'Regression Line',
+        color: blueColor,
+        data: [
+          [10, 100],
+          [20, 200]
         ],
-        false
-      );
-      expect(activeTrial.series).toEqual([]);
-      component.handleDataExplorer(studentData);
-      expect(activeTrial.series).toEqual([
-        {
-          type: 'scatter',
-          name: casesColumnName,
-          color: blueColor,
-          yAxis: 0,
-          data: [
-            ['Alameda', 100],
-            ['Contra Costa', 200]
-          ]
-        }
-      ]);
-    });
+        yAxis: 0,
+        enableMouseTracking: false
+      }
+    ]);
+  });
+}
+
+function handleDataExplorerXColumnLabels() {
+  it('should process data explorer student data when x column contains labels', () => {
+    const studentData = createStudentData(
+      [
+        [{ text: 'County' }, { text: casesColumnName }],
+        [{ text: 'Alameda' }, { text: '100' }],
+        [{ text: 'Contra Costa' }, { text: '200' }]
+      ],
+      false
+    );
+    expect(activeTrial.series).toEqual([]);
+    component.handleDataExplorer(studentData);
+    expect(activeTrial.series).toEqual([
+      {
+        type: 'scatter',
+        name: casesColumnName,
+        color: blueColor,
+        yAxis: 0,
+        data: [
+          ['Alameda', 100],
+          ['Contra Costa', 200]
+        ]
+      }
+    ]);
   });
 }
 
--- src/assets/wise5/components/graph/data-explorer-manager.ts
+++ src/assets/wise5/components/graph/data-explorer-manager.ts
@@ -1,54 +1,120 @@
 import * as covariance from 'compute-covariance';
 import { addPointFromTableIntoData, calculateMean, isMultipleYAxes, isSingleYAxis } from './util';
+import { Series } from './domain/series';
 
 export class DataExplorerManager {
   dataExplorerColors: string[] = ['blue', 'orange', 'purple', 'black', 'green'];
 
   constructor(private xAxis: any, private yAxis: any, private activeTrial: any) {}
 
-  handleDataExplorer(studentData: any): any[] {
-    const dataExplorerSeries = studentData.dataExplorerSeries;
-    const graphType = studentData.dataExplorerGraphType;
-    const tooltipHeaderColumn = studentData.dataExplorerTooltipHeaderColumn;
+  handleDataExplorer(studentData: any): Series[] {
     this.xAxis.title.text = studentData.dataExplorerXAxisLabel;
     this.setYAxisLabels(studentData);
     this.setXAxisLabels(studentData);
+    return this.addAllSeries(studentData);
+  }
 
+  private addAllSeries(studentData: any): Series[] {
     const allRegressionSeries = [];
+    const dataExplorerSeries = studentData.dataExplorerSeries;
     for (let seriesIndex = 0; seriesIndex < dataExplorerSeries.length; seriesIndex++) {
       const xColumn = dataExplorerSeries[seriesIndex].xColumn;
       const yColumn = dataExplorerSeries[seriesIndex].yColumn;
       const yAxis = dataExplorerSeries[seriesIndex].yAxis;
       if (yColumn != null) {
         const color = this.dataExplorerColors[seriesIndex];
-        const name = dataExplorerSeries[seriesIndex].name;
-        const series = this.generateDataExplorerSeries(
-          studentData.tableData,
+        this.addSeries(
+          seriesIndex,
+          dataExplorerSeries,
+          studentData,
           xColumn,
           yColumn,
-          graphType,
-          name,
+          yAxis,
+          color,
+          studentData.dataExplorerTooltipHeaderColumn
+        );
+        this.handleRegressionSeries(
+          studentData,
+          xColumn,
+          yColumn,
           color,
           yAxis,
-          tooltipHeaderColumn
+          allRegressionSeries
         );
-        if (series.yAxis == null) {
-          this.setSeriesYAxisIndex(series, seriesIndex);
-        }
-        this.activeTrial.series.push(series);
-        if (graphType === 'scatter' && studentData.isDataExplorerScatterPlotRegressionLineEnabled) {
-          const singleRegressionSeries = this.generateDataExplorerRegressionSeries(
-            studentData.tableData,
-            xColumn,
-            yColumn,
-            color,
-            yAxis
-          );
-          allRegressionSeries.push(singleRegressionSeries);
-        }
       }
     }
     return allRegressionSeries;
+  }
+
+  private addSeries(
+    seriesIndex: number,
+    dataExplorerSeries: any[],
+    studentData: any,
+    xColumn: number,
+    yColumn: number,
+    yAxis: any,
+    color: string,
+    tooltipHeaderColumn: number
+  ): void {
+    const series = this.generateDataExplorerSeries(
+      studentData.tableData,
+      xColumn,
+      yColumn,
+      studentData.dataExplorerGraphType,
+      dataExplorerSeries[seriesIndex].name,
+      color,
+      yAxis,
+      tooltipHeaderColumn
+    );
+    if (series.yAxis == null) {
+      this.setSeriesYAxisIndex(series, seriesIndex);
+    }
+    this.activeTrial.series.push(series);
+  }
+
+  private handleRegressionSeries(
+    studentData: any,
+    xColumn: number,
+    yColumn: number,
+    color: string,
+    yAxis: any,
+    allRegressionSeries: any[]
+  ): void {
+    if (this.shouldAddRegressionSeries(studentData)) {
+      this.addRegressionSeries(
+        studentData.tableData,
+        xColumn,
+        yColumn,
+        color,
+        yAxis,
+        allRegressionSeries
+      );
+    }
+  }
+
+  private shouldAddRegressionSeries(studentData: any): boolean {
+    return (
+      studentData.dataExplorerGraphType === 'scatter' &&
+      studentData.isDataExplorerScatterPlotRegressionLineEnabled
+    );
+  }
+
+  private addRegressionSeries(
+    tableData: any[][],
+    xColumn: number,
+    yColumn: number,
+    color: string,
+    yAxis: number,
+    allRegressionSeries: Series[]
+  ): void {
+    const singleRegressionSeries = this.generateDataExplorerRegressionSeries(
+      tableData,
+      xColumn,
+      yColumn,
+      color,
+      yAxis
+    );
+    allRegressionSeries.push(singleRegressionSeries);
   }
 
   private setYAxisLabels(studentData: any): void {
@@ -64,7 +130,7 @@ export class DataExplorerManager {
     }
   }
 
-  private setSeriesYAxisIndex(series: any, seriesIndex: number): void {
+  private setSeriesYAxisIndex(series: Series, seriesIndex: number): void {
     if (isMultipleYAxes(this.yAxis) && this.yAxis.length == 2) {
       if (seriesIndex === 0 || seriesIndex === 1) {
         series.yAxis = seriesIndex;
@@ -78,24 +144,14 @@ export class DataExplorerManager {
     const thisComponent = this;
     this.xAxis.labels = {
       formatter: function () {
-        if (
-          this.value + 1 < studentData.tableData.length &&
-          studentData.isDataExplorerEnabled != null &&
-          studentData.dataExplorerSeries != null &&
-          studentData.tableData != null
-        ) {
+        if (thisComponent.shouldGenerateDataExplorerLabel(this.value, studentData)) {
           // try to convert the x value number to a category string on the x axis
           const textValue = thisComponent.getXColumnTextValue(
             studentData.dataExplorerSeries,
             studentData.tableData,
             this.value
           );
-          if (
-            typeof textValue === 'string' &&
-            textValue !== '' &&
-            !thisComponent.isNA(textValue) &&
-            isNaN(parseFloat(textValue))
-          ) {
+          if (thisComponent.shouldGetColumnLabel(textValue)) {
             return studentData.tableData[this.value + 1][studentData.dataExplorerSeries[0].xColumn]
               .text;
           }
@@ -103,6 +159,24 @@ export class DataExplorerManager {
         return this.value;
       }
     };
+  }
+
+  private shouldGenerateDataExplorerLabel(value: number, studentData: any): boolean {
+    return (
+      value + 1 < studentData.tableData.length &&
+      studentData.isDataExplorerEnabled != null &&
+      studentData.dataExplorerSeries != null &&
+      studentData.tableData != null
+    );
+  }
+
+  private shouldGetColumnLabel(textValue: string): boolean {
+    return (
+      typeof textValue === 'string' &&
+      textValue !== '' &&
+      !this.isNA(textValue) &&
+      isNaN(parseFloat(textValue))
+    );
   }
 
   private getXColumnTextValue(
@@ -129,8 +203,8 @@ export class DataExplorerManager {
     color: string,
     yAxis: any,
     tooltipHeaderColumn: number
-  ): any {
-    const series = {
+  ): Series {
+    const series: Series = {
       type: graphType,
       name: name,
       color: color,
@@ -189,7 +263,7 @@ export class DataExplorerManager {
     yColumn: number,
     color: string,
     yAxis: number
-  ): any {
+  ): Series {
     const regressionLineData = this.calculateRegressionLineData(tableData, xColumn, yColumn);
     return {
       type: 'line',
