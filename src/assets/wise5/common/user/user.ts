8ea27c33d06241c3d45acf82d0f875ff7fb1755e
Jonathan Lim-Breitbart <breity10@gmail.com>
2023-06-07 09:49:06 -0700

    Use localeCompare for username comparator - Fix tests (assume workgroup.username always exists)

--- src/assets/wise5/common/user/user.ts
+++ src/assets/wise5/common/user/user.ts
@@ -1,10 +1,5 @@
 export function usernameComparator(object1: { username: string }, object2: { username: string }) {
   const username1 = object1.username.toLowerCase();
   const username2 = object2.username.toLowerCase();
-  if (username1 < username2) {
-    return -1;
-  } else if (username1 > username2) {
-    return 1;
-  }
-  return 0;
+  return username1.localeCompare(username2);
 }
