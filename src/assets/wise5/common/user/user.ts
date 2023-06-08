161e639a3645d9311e31c55677862d4c5fab5b44
Jonathan Lim-Breitbart <breity10@gmail.com>
2023-06-08 09:59:09 -0700

    Reinstate null check in username comparator

--- src/assets/wise5/common/user/user.ts
+++ src/assets/wise5/common/user/user.ts
@@ -1,4 +1,7 @@
 export function usernameComparator(object1: { username: string }, object2: { username: string }) {
+  if (object1.username == null || object2.username == null) {
+    return 0;
+  }
   const username1 = object1.username.toLowerCase();
   const username2 = object2.username.toLowerCase();
   return username1.localeCompare(username2);
