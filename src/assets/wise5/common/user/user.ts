caa922028612d662df2e02181ab4227dba2f112f
Jonathan Lim-Breitbart <breity10@gmail.com>
2023-06-09 09:16:01 -0700

    Refactor to a single return statement

--- src/assets/wise5/common/user/user.ts
+++ src/assets/wise5/common/user/user.ts
@@ -1,8 +1,7 @@
 export function usernameComparator(object1: { username: string }, object2: { username: string }) {
-  if (object1.username == null || object2.username == null) {
-    return 0;
-  }
-  const username1 = object1.username.toLowerCase();
-  const username2 = object2.username.toLowerCase();
-  return username1.localeCompare(username2);
+  const username1 = object1.username;
+  const username2 = object2.username;
+  return object1.username == null || object2.username == null
+    ? 0
+    : username1.toLowerCase().localeCompare(username2.toLowerCase());
 }
