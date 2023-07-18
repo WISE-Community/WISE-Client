3ae484cc9cf8b81bb29510660fd87ad3b80105c6
Hiroki Terashima <honchikun@gmail.com>
2023-07-17 17:01:36 -0700

    Clean up code #1342

--- src/assets/wise5/authoringTool/node/node-authoring/node-authoring.component.ts
+++ src/assets/wise5/authoringTool/node/node-authoring/node-authoring.component.ts
@@ -24,9 +24,9 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
   styleUrls: ['./node-authoring.component.scss']
 })
 export class NodeAuthoringComponent implements OnInit {
-  components: any = [];
+  components: ComponentContent[] = [];
   componentsToChecked = {};
-  componentsToIsExpanded = {};
+  componentsToExpanded = {};
   currentNodeCopy: any;
   isAnyComponentSelected: boolean = false;
   isGroupNode: boolean;
@@ -48,7 +48,7 @@ export class NodeAuthoringComponent implements OnInit {
     private nodeService: NodeService,
     private notificationService: NotificationService,
     private projectService: TeacherProjectService,
-    private teacherDataService: TeacherDataService,
+    private dataService: TeacherDataService,
     private upgrade: UpgradeModule
   ) {}
 
@@ -57,7 +57,7 @@ export class NodeAuthoringComponent implements OnInit {
     this.nodeId = this.upgrade.$injector.get('$stateParams').nodeId;
     this.node = this.projectService.getNode(this.nodeId);
     this.isGroupNode = this.projectService.isGroupNode(this.nodeId);
-    this.teacherDataService.setCurrentNodeByNodeId(this.nodeId);
+    this.dataService.setCurrentNodeByNodeId(this.nodeId);
     this.nodeJson = this.projectService.getNodeById(this.nodeId);
     this.nodePosition = this.projectService.getNodePositionById(this.nodeId);
     this.components = this.projectService.getComponents(this.nodeId);
@@ -104,7 +104,7 @@ export class NodeAuthoringComponent implements OnInit {
 
   ngOnDestroy(): void {
     if (this.$state.current.name !== 'root.at.project.node') {
-      this.teacherDataService.setCurrentNode(null);
+      this.dataService.setCurrentNode(null);
     }
     this.subscriptions.unsubscribe();
   }
@@ -114,7 +114,7 @@ export class NodeAuthoringComponent implements OnInit {
   }
 
   protected close(): void {
-    this.teacherDataService.setCurrentNode(null);
+    this.dataService.setCurrentNode(null);
     this.scrollToTopOfPage();
   }
 
@@ -182,14 +182,10 @@ export class NodeAuthoringComponent implements OnInit {
     this.upgrade.$injector.get('$state').go('root.at.project.node.advanced');
   }
 
-  protected getSelectedComponents(): string[] {
-    return this.components.filter((component: any) => this.componentsToChecked[component.id]);
-  }
-
-  protected getSelectedComponentIds(): string[] {
-    return this.components
-      .filter((component: any) => this.componentsToChecked[component.id])
-      .map((component: any) => component.id);
+  protected getSelectedComponents(): ComponentContent[] {
+    return this.components.filter(
+      (component: ComponentContent) => this.componentsToChecked[component.id]
+    );
   }
 
   private clearComponentsToChecked(): void {
@@ -236,8 +232,8 @@ export class NodeAuthoringComponent implements OnInit {
   protected deleteComponents(): void {
     this.scrollToTopOfPage();
     if (this.confirmDeleteComponent(this.getSelectedComponentNumbersAndTypes())) {
-      const componentIdAndTypes = this.getSelectedComponentIds()
-        .map((componentId) => this.node.deleteComponent(componentId))
+      const componentIdAndTypes = this.getSelectedComponents()
+        .map((component) => this.node.deleteComponent(component.id))
         .map((component) => ({ componentId: component.id, type: component.type }));
       this.afterDeleteComponent(componentIdAndTypes);
     }
@@ -308,7 +304,7 @@ export class NodeAuthoringComponent implements OnInit {
         $('#content').scrollTop(componentElement.offset().top - 200);
         for (const newComponent of newComponents) {
           temporarilyHighlightElement(newComponent.id);
-          this.componentsToIsExpanded[newComponent.id] = expandComponents;
+          this.componentsToExpanded[newComponent.id] = expandComponents;
         }
       }
     });
@@ -339,17 +335,17 @@ export class NodeAuthoringComponent implements OnInit {
   }
 
   protected toggleComponent(componentId: string): void {
-    this.componentsToIsExpanded[componentId] = !this.componentsToIsExpanded[componentId];
+    this.componentsToExpanded[componentId] = !this.componentsToExpanded[componentId];
   }
 
   protected setAllComponentsIsExpanded(isExpanded: boolean): void {
     this.components.forEach((component) => {
-      this.componentsToIsExpanded[component.id] = isExpanded;
+      this.componentsToExpanded[component.id] = isExpanded;
     });
   }
 
   protected getNumberOfComponentsExpanded(): number {
-    return Object.values(this.componentsToIsExpanded).filter((value) => value).length;
+    return Object.values(this.componentsToExpanded).filter((value) => value).length;
   }
 
   private setShowSaveButtonForAllComponents(node: Node, showSaveButton: boolean): void {
@@ -360,7 +356,7 @@ export class NodeAuthoringComponent implements OnInit {
       .forEach((component) => (component.showSaveButton = showSaveButton));
   }
 
-  protected dropComponent(event: CdkDragDrop<string[]>): void {
+  protected dropComponent(event: CdkDragDrop<ComponentContent[]>): void {
     moveItemInArray(this.components, event.previousIndex, event.currentIndex);
     this.projectService.saveProject();
   }
