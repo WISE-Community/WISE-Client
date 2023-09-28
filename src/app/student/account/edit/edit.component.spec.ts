892483bf9800ba21b900b207a499ad61cc8fc2f3
Jonathan Lim-Breitbart <breity10@gmail.com>
2023-09-28 09:25:45 -0700

    refactor(EditProfileComponent): reduce duplication (#1432)

--- src/app/student/account/edit/edit.component.spec.ts
+++ src/app/student/account/edit/edit.component.spec.ts
@@ -6,20 +6,22 @@ import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 @Component({ selector: 'app-edit-password', template: '' })
 class EditPasswordComponent {}
 
-@Component({ selector: 'app-edit-profile', template: '' })
-class EditProfileComponent {}
+@Component({ selector: 'student-edit-profile', template: '' })
+class StudentEditProfileComponent {}
 
 describe('EditComponent', () => {
   let component: EditComponent;
   let fixture: ComponentFixture<EditComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [EditComponent],
-      providers: [],
-      schemas: [NO_ERRORS_SCHEMA]
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [EditComponent],
+        providers: [],
+        schemas: [NO_ERRORS_SCHEMA]
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EditComponent);
