f40afba18cfb2a205213b88124ef55ac09064f4d
Geoffrey Kwan <geoffreykwan@gmail.com>
2023-10-03 17:24:49 -0400

    chore(Manage Students): Clean up code and comment and run translation
    #1437

--- src/app/services/updateWorkgroupService.ts
+++ src/app/services/updateWorkgroupService.ts
@@ -1,21 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ConfigService } from '../../assets/wise5/services/configService';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class UpdateWorkgroupService {
-  constructor(private ConfigService: ConfigService, private http: HttpClient) {}
+  constructor(private configService: ConfigService, private http: HttpClient) {}
 
   /**
-   * Move student between workgroups, or to/from workgroup and unassigned students
+   * Move student to a workgroup
    * @param userId Student User ID
-   * @param workgroupIdFrom Workgroup ID to move student from. -1 if student is not in a workgroup
-   * @param workgroupIdTo Workgroup ID to move student to. -1 if moving student to unassigned
-   * @param periodId Period ID the student is in.
+   * @param workgroupIdTo Workgroup ID to move student to
+   * @return Observable of move student response
    */
-  moveMember(userId: number, workgroupIdTo: number) {
+  moveMember(userId: number, workgroupIdTo: number): Observable<any> {
     return this.http.post(
-      `/api/teacher/run/${this.ConfigService.getRunId()}/workgroup/move-user/${userId}`,
+      `/api/teacher/run/${this.configService.getRunId()}/workgroup/move-user/${userId}`,
       {
         workgroupIdTo: workgroupIdTo
       }
