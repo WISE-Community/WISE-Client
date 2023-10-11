5f812750921745f92f9e583b2b984bd624aed117
Jonathan Lim-Breitbart <breity10@gmail.com>
2023-10-11 16:29:54 -0700

    Remove unused description field from NewNodeTemplate

--- src/assets/wise5/authoringTool/addNode/choose-new-node-template/choose-new-node-template.component.ts
+++ src/assets/wise5/authoringTool/addNode/choose-new-node-template/choose-new-node-template.component.ts
@@ -11,25 +11,21 @@ export class ChooseNewNodeTemplate {
   protected templates: NewNodeTemplate[] = [
     {
       label: $localize`Create Your Own`,
-      description: $localize`Create Your Own Description`,
       icon: 'mode_edit',
       route: 'add-your-own'
     },
     {
-      label: $localize`Import from Another Unit`,
-      description: $localize`Import a step from another unit`,
+      label: $localize`Import From Another Unit`,
       icon: 'system_update_alt',
       route: 'import-step/choose-unit'
     },
     {
       label: $localize`Automated Assessment`,
-      description: $localize`Automated Assessment`,
       icon: 'fact_check',
       route: 'automated-assessment/choose-item'
     },
     {
       label: $localize`Interactive Simulation`,
-      description: $localize`Add an existing interactive simulation`,
       icon: 'video_settings',
       route: 'simulation/choose-item'
     }
