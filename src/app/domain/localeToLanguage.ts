679b45b1db5dd06ded3c022d4239895e40f2e465
Hiroki Terashima <honchikun@gmail.com>
2023-12-04 19:34:58 -0800

    Alphabetized language choices and changed "Dutch (Netherlands)" => "Dutch"

--- src/app/domain/localeToLanguage.ts
+++ src/app/domain/localeToLanguage.ts
@@ -1,12 +1,12 @@
 export const localeToLanguage: { [locale: string]: string } = {
-  en_US: $localize`English`,
-  es: $localize`Spanish`,
-  de: $localize`German`,
-  ja: $localize`Japanese`,
-  it: $localize`Italian`,
-  ko: $localize`Korean`,
-  nl: $localize`Dutch (Netherlands)`,
-  vi: $localize`Vietnamese`,
   zh_CN: $localize`Chinese (Simplified)`,
-  zh_TW: $localize`Chinese (Traditional)`
+  zh_TW: $localize`Chinese (Traditional)`,
+  nl: $localize`Dutch`,
+  en_US: $localize`English`,
+  de: $localize`German`,
+  it: $localize`Italian`,
+  ja: $localize`Japanese`,
+  ko: $localize`Korean`,
+  es: $localize`Spanish`,
+  vi: $localize`Vietnamese`
 };
