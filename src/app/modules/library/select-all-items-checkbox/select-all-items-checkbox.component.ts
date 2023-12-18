29ea0b67bd69dfdb83f2ad3e8849c9792c2f3cd6
Hiroki Terashima <honchikun@gmail.com>
2023-12-18 12:48:19 -0800

    - Use SelectAllItemsStatus string literal union keys ('none' | 'some' | 'all') to indicate the current status of the SelectAllItemsCheckboxComponent. - Move status calculation from PersonalLibraryComponent to the SelectAllItemsCheckboxComponent. This requires passing in numSelectedItems and numAllItems. - Call PersonalLibraryComponent.(un)selectAllProjects() directly as a   result of the output from the SelectAllItemsCheckboxComponent

--- src/app/modules/library/select-all-items-checkbox/select-all-items-checkbox.component.ts
+++ src/app/modules/library/select-all-items-checkbox/select-all-items-checkbox.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MAT_CHECKBOX_DEFAULT_OPTIONS } from '@angular/material/checkbox';
 
+type SelectAllItemsStatus = 'none' | 'some' | 'all';
+
 @Component({
   selector: 'select-all-items-checkbox',
   templateUrl: './select-all-items-checkbox.component.html',
@@ -8,11 +10,27 @@ import { MAT_CHECKBOX_DEFAULT_OPTIONS } from '@angular/material/checkbox';
 })
 export class SelectAllItemsCheckboxComponent {
   @Input() label: string = $localize`items`;
-  @Input() selectedAllItems: boolean = false;
-  @Input() selectedSomeItems: boolean = false;
-  @Output() selectAllItemsEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input() numAllItems: number;
+  @Input() numSelectedItems: number;
+  @Output() selectAllItemsEvent: EventEmitter<void> = new EventEmitter<void>();
+  @Output() selectNoItemsEvent: EventEmitter<void> = new EventEmitter<void>();
+  protected status: SelectAllItemsStatus;
+
+  ngOnChanges(): void {
+    if (this.numSelectedItems == 0) {
+      this.status = 'none';
+    } else if (this.numSelectedItems < this.numAllItems) {
+      this.status = 'some';
+    } else {
+      this.status = 'all';
+    }
+  }
 
   protected select(): void {
-    this.selectAllItemsEvent.emit(!(this.selectedAllItems || this.selectedSomeItems));
+    if (this.status === 'none') {
+      this.selectAllItemsEvent.emit();
+    } else {
+      this.selectNoItemsEvent.emit();
+    }
   }
 }
