d0ddf1cb9b5e721f93c234a630d68c9367655f1c
Hiroki Terashima <honchikun@gmail.com>
2024-01-30 11:18:43 -0800

    feat(Multiple languages): Save changes to existing translations (#1602)
    POSTs to an endpoint to save the translations for a locale each time an existing translation text is updated.

--- src/assets/wise5/authoringTool/components/translatable-input/translatable-input.component.ts
+++ src/assets/wise5/authoringTool/components/translatable-input/translatable-input.component.ts
@@ -1,16 +1,19 @@
 import { Component, ContentChild, ElementRef, Input, Signal, computed } from '@angular/core';
 import { TeacherProjectService } from '../../../services/teacherProjectService';
+import { EditProjectTranslationService } from '../../../services/editProjectTranslationService';
 import { CommonModule } from '@angular/common';
 import { MatInput, MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { TranslateProjectService } from '../../../services/translateProjectService';
 import { MatLabel } from '@angular/material/form-field';
 import { Language } from '../../../../../app/domain/language';
+import { Subject, Subscription, debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   standalone: true,
   selector: 'translatable-input',
   imports: [CommonModule, FormsModule, MatInputModule],
+  providers: [EditProjectTranslationService],
   styleUrls: ['./translatable-input.component.scss'],
   templateUrl: './translatable-input.component.html'
 })
@@ -20,11 +23,15 @@ export class TranslatableInputComponent {
   @ContentChild(MatInput) defaultLanguageInput: MatInput;
   @ContentChild(MatLabel, { read: ElementRef }) defaultLanguageLabelRef: ElementRef;
   protected defaultLanguageText: Signal<string>;
+  private i18nId: string;
   @Input() key: string;
   protected showTranslationInput: Signal<boolean>;
-  protected translatedText: Signal<string>;
+  protected translationText: Signal<string>;
+  protected translationTextChanged: Subject<string> = new Subject<string>();
+  protected translationTextChangedSubscription: Subscription;
 
   constructor(
+    private editProjectTranslationService: EditProjectTranslationService,
     private projectService: TeacherProjectService,
     private translateProjectService: TranslateProjectService
   ) {
@@ -33,10 +40,10 @@ export class TranslatableInputComponent {
   }
 
   ngOnInit(): void {
-    const i18nId = this.content[`${this.key}.i18n`]?.id;
-    this.translatedText = computed(() =>
+    this.i18nId = this.content[`${this.key}.i18n`]?.id;
+    this.translationText = computed(() =>
       this.showTranslationInput()
-        ? this.translateProjectService.currentTranslations()[i18nId]?.value
+        ? this.translateProjectService.currentTranslations()[this.i18nId]?.value
         : ''
     );
     this.defaultLanguageText = computed(() =>
@@ -46,9 +53,25 @@ export class TranslatableInputComponent {
           }`
         : ''
     );
+    this.translationTextChangedSubscription = this.translationTextChanged
+      .pipe(debounceTime(1000), distinctUntilChanged())
+      .subscribe((text: string) => {
+        this.saveTranslationText(text);
+      });
   }
 
-  protected saveTranslationText(text: string): void {
-    // TODO: save translation text to server
+  ngOnDestory(): void {
+    this.translationTextChangedSubscription.unsubscribe();
+  }
+
+  private saveTranslationText(text: string): void {
+    this.projectService.broadcastSavingProject();
+    const currentTranslations = this.translateProjectService.currentTranslations();
+    currentTranslations[this.i18nId] = { value: text, modified: new Date().getTime() };
+    this.editProjectTranslationService
+      .saveCurrentTranslations(currentTranslations)
+      .subscribe(() => {
+        this.projectService.broadcastProjectSaved();
+      });
   }
 }
