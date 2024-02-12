172b78d32409ba34040ab0c9539d64f14feb9f56
Geoffrey Kwan <geoffreykwan@gmail.com>
2024-02-12 15:45:49 -0500

    refactor(Export): Create component for step and component checkboxes

--- src/assets/wise5/classroomMonitor/dataExport/data-export.module.ts
+++ src/assets/wise5/classroomMonitor/dataExport/data-export.module.ts
@@ -3,10 +3,11 @@ import { DataExportComponent } from './data-export/data-export.component';
 import { ExportStepVisitsComponent } from './export-step-visits/export-step-visits.component';
 import { StudentTeacherCommonModule } from '../../../../app/student-teacher-common.module';
 import { RouterModule } from '@angular/router';
+import { SelectStepAndComponentCheckboxesComponent } from './select-step-and-component-checkboxes/select-step-and-component-checkboxes.component';
 
 @NgModule({
   imports: [RouterModule, StudentTeacherCommonModule],
-  declarations: [DataExportComponent, ExportStepVisitsComponent],
-  exports: [DataExportComponent, ExportStepVisitsComponent]
+  declarations: [DataExportComponent, ExportStepVisitsComponent, SelectStepAndComponentCheckboxesComponent],
+  exports: [DataExportComponent, ExportStepVisitsComponent, SelectStepAndComponentCheckboxesComponent]
 })
 export class DataExportModule {}
