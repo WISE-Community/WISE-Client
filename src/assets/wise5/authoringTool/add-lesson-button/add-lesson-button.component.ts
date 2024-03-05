76c656eb104656c69715761206b738dfe69dbd1a
Geoffrey Kwan <geoffreykwan@gmail.com>
2024-03-05 18:04:44 -0500

    feat(Authoring): Use add lesson button component when no lessons
    #1671

--- src/assets/wise5/authoringTool/add-lesson-button/add-lesson-button.component.ts
+++ src/assets/wise5/authoringTool/add-lesson-button/add-lesson-button.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { TeacherProjectService } from '../../services/teacherProjectService';
 import { temporarilyHighlightElement } from '../../common/dom/dom';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'add-lesson-button',
@@ -8,28 +9,46 @@ import { temporarilyHighlightElement } from '../../common/dom/dom';
   styleUrls: ['./add-lesson-button.component.scss']
 })
 export class AddLessonButtonComponent {
+  @Input() active: boolean;
   @Input() lessonId: string;
+  @ViewChild(MatMenuTrigger) menuTrigger: MatMenuTrigger;
 
   constructor(private projectService: TeacherProjectService) {}
 
+  protected addLesson(): void {
+    if (this.lessonId == null) {
+      this.addFirstLesson();
+    } else {
+      this.menuTrigger.openMenu();
+    }
+  }
+
   protected addLessonBefore(): void {
-    const newLesson = this.projectService.createGroup('New Lesson');
     const previousLessonId = this.projectService.getPreviousNodeId(this.lessonId);
     if (previousLessonId == null) {
-      const insertLocation = this.projectService.isActive(this.lessonId)
-        ? 'group0'
-        : 'inactiveGroups';
-      this.projectService.createNodeInside(newLesson, insertLocation);
+      this.addFirstLesson();
     } else {
+      const newLesson = this.createNewLesson();
       this.projectService.createNodeAfter(newLesson, previousLessonId);
+      this.updateProject(newLesson.id);
     }
+  }
+
+  private addFirstLesson(): void {
+    const newLesson = this.createNewLesson();
+    const insertLocation = this.active ? 'group0' : 'inactiveGroups';
+    this.projectService.createNodeInside(newLesson, insertLocation);
     this.updateProject(newLesson.id);
   }
 
   protected addLessonAfter(): void {
-    const newLesson = this.projectService.createGroup('New Lesson');
+    const newLesson = this.createNewLesson();
     this.projectService.createNodeAfter(newLesson, this.lessonId);
     this.updateProject(newLesson.id);
+  }
+
+  private createNewLesson(): any {
+    return this.projectService.createGroup('New Lesson');
   }
 
   private updateProject(newNodeId: string): void {
