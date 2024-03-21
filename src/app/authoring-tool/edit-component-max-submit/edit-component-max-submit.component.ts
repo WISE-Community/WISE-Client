c937b088b1a7306ad7c20d56cd7e1df74f3d11a1
Geoffrey Kwan <geoffreykwan@gmail.com>
2024-03-21 14:36:56 -0400

    fix(Authoring): Fix max submit authoring (#1706)

--- src/app/authoring-tool/edit-component-max-submit/edit-component-max-submit.component.ts
+++ src/app/authoring-tool/edit-component-max-submit/edit-component-max-submit.component.ts
@@ -1,33 +1,9 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, Subscription } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { EditComponentFieldComponent } from '../edit-component-field.component';
 
 @Component({
   selector: 'edit-component-max-submit',
   templateUrl: './edit-component-max-submit.component.html',
   styleUrls: ['./edit-component-max-submit.component.scss']
 })
-export class EditComponentMaxSubmitComponent implements OnInit {
-  @Input()
-  maxSubmitCount: number;
-
-  @Output()
-  maxSubmitCountChange: EventEmitter<number> = new EventEmitter<number>();
-
-  maxSubmitCountDebouncer: Subject<number> = new Subject<number>();
-  maxSubmitCountDebouncerSubscription: Subscription;
-
-  constructor() {}
-
-  ngOnInit(): void {
-    this.maxSubmitCountDebouncerSubscription = this.maxSubmitCountDebouncer
-      .pipe(debounceTime(1000), distinctUntilChanged())
-      .subscribe(() => {
-        this.maxSubmitCountChange.emit(this.maxSubmitCount);
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.maxSubmitCountDebouncerSubscription.unsubscribe();
-  }
-}
+export class EditComponentMaxSubmitComponent extends EditComponentFieldComponent {}
