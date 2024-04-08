80941191b27cf372e692f2da839731cb210eae28
Hiroki Terashima <honchikun@gmail.com>
2024-04-08 14:50:27 -0700

    feat(Multiple languages): Copy default language in HTML component (#1704)
    Remove the distinctUntilChanged filter. This was causing successive content copying from default languages to different languages to fail.
    
    Co-authored-by: Jonathan Lim-Breitbart <breity10@gmail.com>

--- src/assets/wise5/authoringTool/components/translatable-rich-text-editor/translatable-rich-text-editor.component.ts
+++ src/assets/wise5/authoringTool/components/translatable-rich-text-editor/translatable-rich-text-editor.component.ts
@@ -1,22 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AbstractTranslatableFieldComponent } from '../abstract-translatable-field/abstract-translatable-field.component';
 import { WiseTinymceEditorModule } from '../../../directives/wise-tinymce-editor/wise-tinymce-editor.module';
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatTabGroup, MatTabsModule } from '@angular/material/tabs';
 import { insertWiseLinks, replaceWiseLinks } from '../../../common/wise-link/wise-link';
 import { ConfigService } from '../../../services/configService';
 import { TeacherProjectTranslationService } from '../../../services/teacherProjectTranslationService';
 import { TeacherProjectService } from '../../../services/teacherProjectService';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   standalone: true,
   selector: 'translatable-rich-text-editor',
-  imports: [CommonModule, MatDialogModule, MatTabsModule, WiseTinymceEditorModule],
-  templateUrl: './translatable-rich-text-editor.component.html'
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatTabsModule,
+    WiseTinymceEditorModule
+  ],
+  templateUrl: './translatable-rich-text-editor.component.html',
+  styleUrls: ['./translatable-rich-text-editor.component.scss']
 })
 export class TranslatableRichTextEditorComponent extends AbstractTranslatableFieldComponent {
   protected html: string = '';
+  @ViewChild(MatTabGroup) private tabs: MatTabGroup;
 
   constructor(
     private configService: ConfigService,
@@ -40,6 +51,21 @@ export class TranslatableRichTextEditorComponent extends AbstractTranslatableFie
       this.configService.removeAbsoluteAssetPaths(this.html)
     );
     this.defaultLanguageTextChanged.next(this.content[this.key]);
+  }
+
+  protected copyDefaultLanguageText(): void {
+    if (
+      this.translationText == undefined ||
+      this.translationText === '' ||
+      confirm(
+        $localize`Are you sure you want to replace the content in ${
+          this.currentLanguage().language
+        } with content in ${this.defaultLanguage.language} for this item?`
+      )
+    ) {
+      this.translationTextChanged.next(this.html);
+      this.tabs.selectedIndex = 0;
+    }
   }
 
   protected saveTranslationText(text: string): void {
