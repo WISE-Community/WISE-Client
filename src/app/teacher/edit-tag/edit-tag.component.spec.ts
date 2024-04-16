49ecd789064bab609b3ae00af846b516097300e1
Geoffrey Kwan <geoffreykwan@gmail.com>
2024-04-16 10:42:24 -0400

    feat(Tag): Use inline editing instead of popup dialogs to edit tags

--- src/app/teacher/edit-tag/edit-tag.component.spec.ts
+++ src/app/teacher/edit-tag/edit-tag.component.spec.ts
@@ -2,8 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditTagComponent } from './edit-tag.component';
 import { ProjectTagService } from '../../../assets/wise5/services/projectTagService';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { FormControl, Validators } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('EditTagComponent', () => {
   let component: EditTagComponent;
@@ -11,13 +11,16 @@ describe('EditTagComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [BrowserAnimationsModule, EditTagComponent, HttpClientTestingModule],
+      imports: [
+        BrowserAnimationsModule,
+        EditTagComponent,
+        HttpClientTestingModule,
+        MatSnackBarModule
+      ],
       providers: [ProjectTagService]
     });
     fixture = TestBed.createComponent(EditTagComponent);
     component = fixture.componentInstance;
-    component.nameControl = new FormControl('', [Validators.required]);
-    component.colorControl = new FormControl('');
     fixture.detectChanges();
   });
 
