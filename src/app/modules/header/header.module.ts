ec66c0049d1c3d6a89dc2407ab60c6c31f2ce409
Hiroki Terashima <honchikun@gmail.com>
2024-04-26 15:54:56 -0700

    refactor(HeaderLinksComponent): Convert to standalone component

--- src/app/modules/header/header.module.ts
+++ src/app/modules/header/header.module.ts
@@ -8,7 +8,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './header.component';
-import { HeaderSigninComponent } from './header-signin/header-signin.component';
 import { HeaderLinksComponent } from './header-links/header-links.component';
 import { HeaderAccountMenuComponent } from './header-account-menu/header-account-menu.component';
 import { ConfigService } from '../../services/config.service';
@@ -25,12 +24,12 @@ const materialModules = [
 @NgModule({
   imports: [
     CommonModule,
-    HeaderSigninComponent,
+    HeaderLinksComponent,
     FlexLayoutModule,
     AppRoutingModule,
     materialModules
   ],
-  declarations: [HeaderComponent, HeaderLinksComponent, HeaderAccountMenuComponent],
+  declarations: [HeaderComponent, HeaderAccountMenuComponent],
   providers: [ConfigService, UserService],
   exports: [HeaderComponent]
 })
