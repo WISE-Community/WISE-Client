41cbb5a1ec1a1f781f28615f5f37e61fe62c3cd5
Geoffrey Kwan <geoffreykwan@gmail.com>
2024-04-28 13:36:42 -0400

    refactor(Authoring): Create select component component

--- src/app/authoring-tool/select-step-and-component/select-step-and-component.component.ts
+++ src/app/authoring-tool/select-step-and-component/select-step-and-component.component.ts
@@ -1,76 +1,33 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ProjectService } from '../../../assets/wise5/services/projectService';
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
 import { ReferenceComponent } from '../../domain/referenceComponent';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { SelectStepComponent } from '../select-step/select-step.component';
+import { SelectComponentComponent } from '../select-component/select-component.component';
 
 @Component({
   selector: 'select-step-and-component',
   templateUrl: './select-step-and-component.component.html',
   styleUrls: ['./select-step-and-component.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, MatFormFieldModule, MatSelectModule, SelectStepComponent]
+  imports: [SelectComponentComponent, SelectStepComponent]
 })
-export class SelectStepAndComponentComponent implements OnInit {
+export class SelectStepAndComponentComponent {
   @Input() allowedComponentTypes: string[] = [];
   @Output() componentChange: EventEmitter<ReferenceComponent> = new EventEmitter();
-  protected components: any[] = [];
-  protected componentToIsAllowed: Map<string, boolean> = new Map<string, boolean>();
   @Input() referenceComponent: ReferenceComponent;
   @Output() stepChange: EventEmitter<ReferenceComponent> = new EventEmitter();
   @Input() thisComponentId: string;
 
-  constructor(private projectService: ProjectService) {}
-
-  ngOnInit(): void {
-    if (this.referenceComponent.nodeId != null) {
-      this.calculateComponents(this.referenceComponent.nodeId);
-      if (this.referenceComponent.componentId == null) {
-        this.automaticallySetComponentIfPossible(this.referenceComponent.nodeId);
-      }
-    }
-  }
-
-  private calculateComponents(nodeId: string): void {
-    this.components = this.projectService.getComponents(nodeId);
-    for (const component of this.components) {
-      this.componentToIsAllowed.set(
-        component.id,
-        this.allowedComponentTypes.includes(component.type)
-      );
-    }
-  }
+  constructor(private changeDetector: ChangeDetectorRef) {}
 
   protected stepChanged(nodeId: string): void {
     this.referenceComponent.nodeId = nodeId;
-    this.referenceComponent.componentId = null;
-    this.automaticallySetComponentIfPossible(nodeId);
-    this.calculateComponents(nodeId);
+    this.changeDetector.detectChanges();
     this.stepChange.emit(this.referenceComponent);
   }
 
-  protected componentChanged(): void {
+  protected componentChanged(componentId: string): void {
+    this.referenceComponent.componentId = componentId;
+    this.changeDetector.detectChanges();
     this.componentChange.emit(this.referenceComponent);
-  }
-
-  private automaticallySetComponentIfPossible(nodeId: string): void {
-    let numAllowedComponents = 0;
-    let allowedComponent = null;
-    for (const component of this.projectService.getComponents(nodeId)) {
-      if (
-        this.allowedComponentTypes.includes(component.type) &&
-        component.id !== this.thisComponentId
-      ) {
-        numAllowedComponents += 1;
-        allowedComponent = component;
-      }
-    }
-    if (numAllowedComponents === 1) {
-      this.referenceComponent.componentId = allowedComponent.id;
-      this.componentChanged();
-    }
   }
 }
--- src/app/authoring-tool/select-step-and-component/select-step-and-component.harness.ts
+++ src/app/authoring-tool/select-step-and-component/select-step-and-component.harness.ts
@@ -1,7 +1,9 @@
 import { ComponentHarness } from '@angular/cdk/testing';
 import { SelectStepHarness } from '../select-step/select-step.harness';
+import { SelectComponentHarness } from '../select-component/select-component.harness';
 
 export class SelectStepAndComponentHarness extends ComponentHarness {
   static hostSelector = 'select-step-and-component';
+  getSelectComponent = this.locatorFor(SelectComponentHarness);
   getSelectStep = this.locatorFor(SelectStepHarness);
 }
