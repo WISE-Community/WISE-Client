19b3b009d9558c06ee6af5622688d3ddb5ff6572
Jonathan Lim-Breitbart <breity10@gmail.com>
2024-05-06 14:34:58 -0700

    feat(Translation): Make source language text display more visible (#1777)

--- src/assets/wise5/authoringTool/components/translatable-input/translatable-input.component.ts
+++ src/assets/wise5/authoringTool/components/translatable-input/translatable-input.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { AbstractTranslatableFieldComponent } from '../abstract-translatable-field/abstract-translatable-field.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
   selector: 'translatable-input',
-  imports: [CommonModule, FormsModule, MatInputModule],
-  styleUrls: ['./translatable-input.component.scss'],
-  templateUrl: './translatable-input.component.html'
+  imports: [CommonModule, FormsModule, MatIconModule, MatInputModule],
+  styleUrls: ['../abstract-translatable-field/abstract-translatable-field.component.scss'],
+  templateUrl: './translatable-input.component.html',
+  encapsulation: ViewEncapsulation.None
 })
 export class TranslatableInputComponent extends AbstractTranslatableFieldComponent {}
--- src/assets/wise5/authoringTool/components/translatable-textarea/translatable-textarea.component.ts
+++ src/assets/wise5/authoringTool/components/translatable-textarea/translatable-textarea.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { AbstractTranslatableFieldComponent } from '../abstract-translatable-field/abstract-translatable-field.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
   selector: 'translatable-textarea',
-  imports: [CommonModule, FormsModule, MatInputModule],
-  styleUrls: ['./translatable-textarea.component.scss'],
-  templateUrl: './translatable-textarea.component.html'
+  imports: [CommonModule, FormsModule, MatIconModule, MatInputModule],
+  styleUrls: ['../abstract-translatable-field/abstract-translatable-field.component.scss'],
+  templateUrl: './translatable-textarea.component.html',
+  encapsulation: ViewEncapsulation.None
 })
 export class TranslatableTextareaComponent extends AbstractTranslatableFieldComponent {}
