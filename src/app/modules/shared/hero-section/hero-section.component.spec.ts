97841ed35dfc4f8e1357d4fb4b3d752c852b9943
Hiroki Terashima <honchikun@gmail.com>
2024-05-06 09:32:49 -0700

    refactor(HeroSectionComponent): Convert to standalone

--- src/app/modules/shared/hero-section/hero-section.component.spec.ts
+++ src/app/modules/shared/hero-section/hero-section.component.spec.ts
@@ -1,24 +1,20 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HeroSectionComponent } from './hero-section.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('HeroSectionComponent', () => {
   let component: HeroSectionComponent;
   let fixture: ComponentFixture<HeroSectionComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [HeroSectionComponent],
-      imports: [],
-      schemas: [NO_ERRORS_SCHEMA]
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeroSectionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [HeroSectionComponent]
+      }).compileComponents();
+      fixture = TestBed.createComponent(HeroSectionComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
