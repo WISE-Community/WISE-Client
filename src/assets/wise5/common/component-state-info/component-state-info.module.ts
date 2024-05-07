7c6dc6999b19ddf0d1a8e6b0b6228eb38e5296dd
Hiroki Terashima <honchikun@gmail.com>
2024-05-07 11:39:24 -0700

    refactor(SaveTimeMessageComponent): Convert to standalone

--- src/assets/wise5/common/component-state-info/component-state-info.module.ts
+++ src/assets/wise5/common/component-state-info/component-state-info.module.ts
@@ -1,12 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { SaveTimeMessageComponent } from '../save-time-message/save-time-message.component';
 import { ComponentStateInfoComponent } from './component-state-info.component';
 
 @NgModule({
-  declarations: [ComponentStateInfoComponent, SaveTimeMessageComponent],
-  imports: [CommonModule, MatTooltipModule],
+  declarations: [ComponentStateInfoComponent],
+  imports: [SaveTimeMessageComponent],
   exports: [ComponentStateInfoComponent, SaveTimeMessageComponent]
 })
 export class ComponentStateInfoModule {}
