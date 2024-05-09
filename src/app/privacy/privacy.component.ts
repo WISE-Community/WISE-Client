18c03257b7596a50e89e0b4661aa13ab90639b6b
Hiroki Terashima <honchikun@gmail.com>
2024-05-09 10:07:28 -0700

    refactor(PrivacyComponent): Convert to standalone

--- src/app/privacy/privacy.component.ts
+++ src/app/privacy/privacy.component.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-privacy',
-  templateUrl: './privacy.component.html',
-  styleUrls: ['./privacy.component.scss']
+  standalone: true,
+  styleUrl: './privacy.component.scss',
+  templateUrl: './privacy.component.html'
 })
-export class PrivacyComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class PrivacyComponent {}
