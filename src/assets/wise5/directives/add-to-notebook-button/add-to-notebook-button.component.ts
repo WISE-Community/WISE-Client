da44b477ed75b7c9d56c93c100526c2c4f78c3cd
Hiroki Terashima <honchikun@gmail.com>
2024-05-16 15:08:17 -0700

    refactor(AddToNotebookButtonComponent): Convert to standalone (#1802)

--- src/assets/wise5/directives/add-to-notebook-button/add-to-notebook-button.component.ts
+++ src/assets/wise5/directives/add-to-notebook-button/add-to-notebook-button.component.ts
@@ -1,17 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
+  imports: [MatButtonModule, MatIconModule],
   selector: 'add-to-notebook-button',
+  standalone: true,
   templateUrl: 'add-to-notebook-button.component.html'
 })
-export class AddToNotebookButton {
-  @Input()
-  isDisabled: boolean;
-
-  @Output()
-  snipImage = new EventEmitter<void>();
-
-  addToNotebook() {
-    this.snipImage.next();
-  }
+export class AddToNotebookButtonComponent {
+  @Input() isDisabled: boolean;
+  @Output() snipImage = new EventEmitter<void>();
 }
