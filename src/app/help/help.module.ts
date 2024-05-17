8be513a8456c8481148ae9f24290b810cc16dfb6
Hiroki Terashima <honchikun@gmail.com>
2024-05-16 18:06:55 -0700

    Remove unused imports

--- src/app/help/help.module.ts
+++ src/app/help/help.module.ts
@@ -1,25 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HelpComponent } from './help.component';
 import { HelpRoutingModule } from './help-routing.module';
-import { SharedModule } from '../modules/shared/shared.module';
 import { GettingStartedComponent } from './faq/getting-started/getting-started.component';
 import { TeacherFaqComponent } from './faq/teacher-faq/teacher-faq.component';
 import { StudentFaqComponent } from './faq/student-faq/student-faq.component';
 import { HelpHomeComponent } from './help-home/help-home.component';
-import { MatDividerModule } from '@angular/material/divider';
-import { CallToActionComponent } from '../modules/shared/call-to-action/call-to-action.component';
 
 @NgModule({
   imports: [
-    CallToActionComponent,
-    CommonModule,
     GettingStartedComponent,
     HelpComponent,
     HelpHomeComponent,
     HelpRoutingModule,
-    MatDividerModule,
-    SharedModule,
     StudentFaqComponent,
     TeacherFaqComponent
   ]
