01437a91478f1a73b70d57d60136a5f75a1984c5
Hiroki Terashima <honchikun@gmail.com>
2024-05-20 15:14:23 -0700

    refactor(DiscourseRecentActivityComponent): Convert to standalone

--- src/app/teacher/discourse-recent-activity/discourse-recent-activity.component.ts
+++ src/app/teacher/discourse-recent-activity/discourse-recent-activity.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { DiscourseFeedComponent } from '../../discourse-feed/discourse-feed.component';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
+  imports: [CommonModule, FlexLayoutModule, MatIconModule],
   selector: 'discourse-recent-activity',
-  templateUrl: 'discourse-recent-activity.component.html',
-  styleUrls: ['discourse-recent-activity.component.scss']
+  standalone: true,
+  styleUrl: 'discourse-recent-activity.component.scss',
+  templateUrl: 'discourse-recent-activity.component.html'
 })
-export class DiscourseRecentActivityComponent extends DiscourseFeedComponent {
-}
+export class DiscourseRecentActivityComponent extends DiscourseFeedComponent {}
