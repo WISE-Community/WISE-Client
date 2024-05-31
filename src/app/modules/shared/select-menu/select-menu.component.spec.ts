2e9a9b0f43289548ead104d37f41ab6bd2fc218d
Geoffrey Kwan <geoffreykwan@gmail.com>
2024-05-31 16:28:11 -0700

    refactor(SelectMenu): Convert to standalone (#1819)

--- src/app/modules/shared/select-menu/select-menu.component.spec.ts
+++ src/app/modules/shared/select-menu/select-menu.component.spec.ts
@@ -1,17 +1,18 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { SelectMenuComponent } from './select-menu.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SelectMenuComponent', () => {
   let component: SelectMenuComponent;
   let fixture: ComponentFixture<SelectMenuComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [SelectMenuComponent],
-      schemas: [NO_ERRORS_SCHEMA]
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [BrowserAnimationsModule, SelectMenuComponent]
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SelectMenuComponent);
