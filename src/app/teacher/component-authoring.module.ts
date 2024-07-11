d83d45dc5dd397110c663eeb5a40a538fba43034
Geoffrey Kwan <geoffreykwan@gmail.com>
2024-07-11 11:42:18 -0400

    refactor(Component Constraint Authoring): Remove unnecessary import

--- src/app/teacher/component-authoring.module.ts
+++ src/app/teacher/component-authoring.module.ts
@@ -86,7 +86,6 @@ import { EditComponentAdvancedButtonComponent } from '../../assets/wise5/authori
 import { AiChatAuthoringComponent } from '../../assets/wise5/components/aiChat/ai-chat-authoring/ai-chat-authoring.component';
 import { EditAiChatAdvancedComponent } from '../../assets/wise5/components/aiChat/edit-ai-chat-advanced/edit-ai-chat-advanced.component';
 import { RequiredErrorLabelComponent } from '../../assets/wise5/authoringTool/node/advanced/required-error-label/required-error-label.component';
-import { EditConstraintRemovalCriteriaComponent } from '../../assets/wise5/authoringTool/constraint/edit-constraint-removal-criteria/edit-constraint-removal-criteria.component';
 
 @NgModule({
   declarations: [
@@ -173,7 +172,6 @@ import { EditConstraintRemovalCriteriaComponent } from '../../assets/wise5/autho
   ],
   imports: [
     ComponentConstraintAuthoringComponent,
-    EditConstraintRemovalCriteriaComponent,
     EditComponentAdvancedButtonComponent,
     PeerGroupingAuthoringModule,
     RequiredErrorLabelComponent,
