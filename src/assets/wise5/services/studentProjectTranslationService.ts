c12857368feee522cab4b7ee5cd2fe1b4fc9cdcf
Hiroki Terashima <honchikun@gmail.com>
2024-07-29 09:11:52 -0700

    feat(Multilingual Unit): Log selected language and load previous-selected language on VLE start (#1866)

--- src/assets/wise5/services/studentProjectTranslationService.ts
+++ src/assets/wise5/services/studentProjectTranslationService.ts
@@ -4,9 +4,23 @@ import { copy } from '../common/object/object';
 import { Translations } from '../../../app/domain/translations';
 import { ProjectTranslationService } from './projectTranslationService';
 import { toSignal, toObservable } from '@angular/core/rxjs-interop';
+import { Language } from '../../../app/domain/language';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from './configService';
+import { ProjectService } from './projectService';
+import { StudentDataService } from './studentDataService';
 
 @Injectable()
 export class StudentProjectTranslationService extends ProjectTranslationService {
+  constructor(
+    protected configService: ConfigService,
+    private dataService: StudentDataService,
+    protected http: HttpClient,
+    protected projectService: ProjectService
+  ) {
+    super(configService, http, projectService);
+  }
+
   currentTranslations = toSignal(
     toObservable(this.projectService.currentLanguage).pipe(
       switchMap((language) =>
@@ -18,7 +32,20 @@ export class StudentProjectTranslationService extends ProjectTranslationService 
     { initialValue: {} }
   );
 
-  translate(locale = 'en_US'): Promise<any> {
+  async switchLanguage(language: Language, requester: 'student' | 'system'): Promise<void> {
+    this.projectService.setCurrentLanguage(language);
+    await this.translate(language.locale);
+    this.dataService.saveVLEEvent(
+      this.dataService.getCurrentNodeId(),
+      null,
+      null,
+      'Language',
+      'languageSelected',
+      { language: language.locale, requester: requester }
+    );
+  }
+
+  private translate(locale = 'en_US'): Promise<any> {
     const project = this.revertToOriginalProject();
     return lastValueFrom(
       this.projectService.getLocale().hasTranslationsToApply(locale)
