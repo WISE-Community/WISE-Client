ed85cf9b08a756bc2339d6d33333353ae5e8a55b
Hiroki Terashima <honchikun@gmail.com>
2024-08-15 09:00:13 -0700

    refactor(GradingEditComponentMaxScore): get node and set max score directly instead of calling function in TeacherProjectService

--- src/assets/wise5/services/teacherProjectService.ts
+++ src/assets/wise5/services/teacherProjectService.ts
@@ -208,20 +208,6 @@ export class TeacherProjectService extends ProjectService {
   }
 
   /**
-   * Check if a node id is already being used in the project
-   * @param nodeId check if this node id is already being used in the project
-   * @return whether the node id is already being used in the project
-   */
-  isNodeIdUsed(nodeId) {
-    for (const node of this.getNodes().concat(this.getInactiveNodes())) {
-      if (node.id === nodeId) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  /**
    * Set a field in the transition logic of a node
    */
   setTransitionLogicField(nodeId, field, value) {
@@ -1881,13 +1867,6 @@ export class TeacherProjectService extends ProjectService {
       }
     }
     return maxScore;
-  }
-
-  setMaxScoreForComponent(nodeId: string, componentId: string, maxScore: number): void {
-    const component = this.getComponent(nodeId, componentId);
-    if (component != null) {
-      component.maxScore = maxScore;
-    }
   }
 
   hasGroupStartId(nodeId) {
