e610abe51c80e91cb958ad910d4e6a1e2bda649e
Geoffrey Kwan <geoffreykwan@gmail.com>
2024-08-22 15:20:13 -0400

    build(Nginx): Update to 1.26.2

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -29,8 +29,14 @@ sudo -u ubuntu -g ubuntu mkdir $WISE_BUILD_FILES
 sudo -u ubuntu -g ubuntu aws s3 sync --exclude "legacy.war" s3://wise-build-files $WISE_BUILD_FILES
 chmod u+x $WISE_BUILD_FILES/sync.sh
 
-echo "Installing Nginx"
-apt-get install nginx -y
+echo "Update apt repository to be able to use latest stable Nginx packages"
+apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list
+apt update
+
+echo "Installing Nginx 1.26.2"
+apt-get install nginx=1.26.2-1~$(lsb_release -sc) -y
 
 echo "Adding ip to nginx.conf"
 sed 's/http {/http {\n        add_header ip $server_addr;/' -i /etc/nginx/nginx.conf
