9d0aebfc349828385759369b52218f023ccef528
Hiroki Terashima <honchikun@gmail.com>
2024-11-05 15:27:21 -0800

    refactor(InsertNodeAfterButtonComponent): Convert to standalone

--- src/assets/wise5/authoringTool/choose-node-location/insert-node-after-button/insert-node-after-button.component.ts
+++ src/assets/wise5/authoringTool/choose-node-location/insert-node-after-button/insert-node-after-button.component.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule],
   selector: 'insert-node-after-button',
+  standalone: true,
   templateUrl: './insert-node-after-button.component.html'
 })
 export class InsertNodeAfterButtonComponent {
-  @Input() protected disabled: boolean;
-  @Output() protected insertEvent = new EventEmitter();
+  @Input() disabled: boolean;
+  @Output() insertEvent = new EventEmitter();
 }
