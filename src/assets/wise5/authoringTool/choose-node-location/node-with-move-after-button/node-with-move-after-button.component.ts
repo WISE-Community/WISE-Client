ed6da083a4f301c8bd883cbf7fe17102d10541ca
Hiroki Terashima <honchikun@gmail.com>
2024-11-06 08:15:49 -0800

    refactor(NodeWithMoveAfterButtonComponent): Convert to standalone

--- src/assets/wise5/authoringTool/choose-node-location/node-with-move-after-button/node-with-move-after-button.component.ts
+++ src/assets/wise5/authoringTool/choose-node-location/node-with-move-after-button/node-with-move-after-button.component.ts
@@ -1,7 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NodeIconAndTitleComponent } from '../node-icon-and-title/node-icon-and-title.component';
+import { InsertNodeAfterButtonComponent } from '../insert-node-after-button/insert-node-after-button.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    InsertNodeAfterButtonComponent,
+    NodeIconAndTitleComponent
+  ],
   selector: 'node-with-move-after-button',
+  standalone: true,
   templateUrl: './node-with-move-after-button.component.html'
 })
 export class NodeWithMoveAfterButtonComponent {
