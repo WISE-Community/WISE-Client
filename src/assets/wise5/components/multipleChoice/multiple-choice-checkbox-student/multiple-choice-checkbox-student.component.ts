220c359dc94e1019322d8507f632f521e6d9fa36
Hiroki Terashima <honchikun@gmail.com>
2024-11-13 10:11:55 -0800

    refactor(MultipleChoiceComponents): Split MCStudentComponent to MCRadioStudentComponent and MCCheckoxStudentComponent (#1989)

---
+++ src/assets/wise5/components/multipleChoice/multiple-choice-checkbox-student/multiple-choice-checkbox-student.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+@Component({
+  imports: [CommonModule, FormsModule, MatCheckboxModule],
+  selector: 'multiple-choice-checkbox-student',
+  standalone: true,
+  templateUrl: './multiple-choice-checkbox-student.component.html'
+})
+export class MultipleChoiceCheckboxStudentComponent {
+  @Input() choices: any[];
+  @Input() studentChoices: string[];
+  @Input() isDisabled: boolean;
+  @Input() showFeedback: boolean;
+  @Input() componentHasCorrectAnswer: boolean;
+  @Output() updateStudentChoices = new EventEmitter<string>();
+}
