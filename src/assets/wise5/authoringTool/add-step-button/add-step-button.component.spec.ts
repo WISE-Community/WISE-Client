6a9513e98d5c71a8fe3a1d9b77c45f91e1d1e032
Hiroki Terashima <honchikun@gmail.com>
2024-11-20 08:56:54 -0800

    test(AddStepButtonComponent): Add tests using harnesses

--- src/assets/wise5/authoringTool/add-step-button/add-step-button.component.spec.ts
+++ src/assets/wise5/authoringTool/add-step-button/add-step-button.component.spec.ts
@@ -1,32 +1,129 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddStepButtonComponent } from './add-step-button.component';
 import { TeacherProjectService } from '../../services/teacherProjectService';
-import { StudentTeacherCommonServicesModule } from '../../../../app/student-teacher-common-services.module';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-
-let teacherProjectService: TeacherProjectService;
+import { ActivatedRoute, Router } from '@angular/router';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatMenuHarness } from '@angular/material/menu/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AddStepTarget } from '../../../../app/domain/addStepTarget';
 
 describe('AddStepButtonComponent', () => {
   let component: AddStepButtonComponent;
   let fixture: ComponentFixture<AddStepButtonComponent>;
+  let loader: HarnessLoader;
+  let projectServiceSpy: jasmine.SpyObj<TeacherProjectService>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let route: ActivatedRoute;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [AddStepButtonComponent, StudentTeacherCommonServicesModule],
+  beforeEach(async () => {
+    projectServiceSpy = jasmine.createSpyObj('TeacherProjectService', [
+      'isFirstStepInLesson',
+      'isBranchPoint',
+      'isNodeInAnyBranchPath',
+      'getParentGroupId',
+      'getNodesByToNodeId',
+      'isFirstNodeInBranchPath'
+    ]);
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    await TestBed.configureTestingModule({
+      imports: [AddStepButtonComponent, NoopAnimationsModule],
       providers: [
-        TeacherProjectService,
-        provideHttpClient(withInterceptorsFromDi()),
-        provideRouter([])
+        { provide: TeacherProjectService, useValue: projectServiceSpy },
+        { provide: Router, useValue: routerSpy },
+        { provide: ActivatedRoute, useValue: {} }
       ]
-    });
-    teacherProjectService = TestBed.inject(TeacherProjectService);
+    }).compileComponents();
+
+    route = TestBed.inject(ActivatedRoute);
     fixture = TestBed.createComponent(AddStepButtonComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    component.nodeId = 'node1';
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should disable button when no actions are available', async () => {
+    projectServiceSpy.isFirstStepInLesson.and.returnValue(false);
+    projectServiceSpy.isBranchPoint.and.returnValue(true);
+    projectServiceSpy.isNodeInAnyBranchPath.and.returnValue(true);
+    fixture.detectChanges();
+
+    const button = await loader.getHarness(MatButtonHarness);
+    expect(await button.isDisabled()).toBeTruthy();
+  });
+
+  it('should show all menu items when all actions are available', async () => {
+    projectServiceSpy.isFirstStepInLesson.and.returnValue(true);
+    projectServiceSpy.isBranchPoint.and.returnValue(false);
+    projectServiceSpy.isNodeInAnyBranchPath.and.returnValue(false);
+    fixture.detectChanges();
+
+    const button = await loader.getHarness(MatButtonHarness);
+    await button.click();
+
+    const menu = await loader.getHarness(MatMenuHarness);
+    const items = await menu.getItems();
+    expect(items.length).toBe(3);
+  });
+
+  it('should navigate to add step before view when first in lesson', async () => {
+    projectServiceSpy.isFirstStepInLesson.and.returnValue(true);
+    projectServiceSpy.getParentGroupId.and.returnValue('group1');
+    fixture.detectChanges();
+
+    const button = await loader.getHarness(MatButtonHarness);
+    await button.click();
+    const menu = await loader.getHarness(MatMenuHarness);
+    const items = await menu.getItems();
+    expect(items.length).toBe(3);
+    await items[0].click();
+
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['add-node', 'choose-template'], {
+      relativeTo: route,
+      state: jasmine.any(AddStepTarget)
+    });
+  });
+
+  it('should navigate to add step after view', async () => {
+    projectServiceSpy.isFirstStepInLesson.and.returnValue(false);
+    projectServiceSpy.isBranchPoint.and.returnValue(false);
+    fixture.detectChanges();
+
+    const button = await loader.getHarness(MatButtonHarness);
+    await button.click();
+    const menu = await loader.getHarness(MatMenuHarness);
+    const items = await menu.getItems();
+    expect(items.length).toBe(2);
+    await items[0].click();
+
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['add-node', 'choose-template'], {
+      relativeTo: route,
+      state: jasmine.any(AddStepTarget)
+    });
+  });
+
+  it('should navigate to create branch view', async () => {
+    projectServiceSpy.isFirstStepInLesson.and.returnValue(false);
+    projectServiceSpy.isBranchPoint.and.returnValue(false);
+    projectServiceSpy.isNodeInAnyBranchPath.and.returnValue(false);
+    fixture.detectChanges();
+
+    const button = await loader.getHarness(MatButtonHarness);
+    await button.click();
+    const menu = await loader.getHarness(MatMenuHarness);
+    const items = await menu.getItems();
+    expect(items.length).toBe(2);
+    await items[1].click();
+
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['create-branch'], {
+      relativeTo: route,
+      state: { targetId: 'node1' }
+    });
   });
 });
