9e06773949ee11ae622407af5348c88f77c472b0
Hiroki Terashima <honchikun@gmail.com>
2024-11-19 17:37:28 -0800

    refactor(AddStepButtonComponent): Only show add step before option for first step in lesson. Clean up code.

--- src/assets/wise5/authoringTool/add-step-button/add-step-button.component.spec.ts
+++ src/assets/wise5/authoringTool/add-step-button/add-step-button.component.spec.ts
@@ -2,8 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddStepButtonComponent } from './add-step-button.component';
 import { TeacherProjectService } from '../../services/teacherProjectService';
 import { StudentTeacherCommonServicesModule } from '../../../../app/student-teacher-common-services.module';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { MatIconModule } from '@angular/material/icon';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
@@ -15,16 +13,14 @@ describe('AddStepButtonComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [AddStepButtonComponent, MatIconModule, StudentTeacherCommonServicesModule],
+      imports: [AddStepButtonComponent, StudentTeacherCommonServicesModule],
       providers: [
         TeacherProjectService,
         provideHttpClient(withInterceptorsFromDi()),
-        provideHttpClientTesting(),
         provideRouter([])
       ]
     });
     teacherProjectService = TestBed.inject(TeacherProjectService);
-    spyOn(teacherProjectService, 'isBranchMergePoint').and.returnValue(false);
     fixture = TestBed.createComponent(AddStepButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
