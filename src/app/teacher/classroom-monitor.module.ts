e946da93b2e4520f8c800d656098c77d1413d03c
Hiroki Terashima <honchikun@gmail.com>
2024-11-21 09:36:11 -0800

    refactor(ComponentGradingComponent): Convert to standalone (#1998)

--- src/app/teacher/classroom-monitor.module.ts
+++ src/app/teacher/classroom-monitor.module.ts
@@ -7,7 +7,6 @@ import { ComponentSelectComponent } from '../classroom-monitor/component-select/
 import { StepInfoComponent } from '../classroom-monitor/step-info/step-info.component';
 import { PeerGroupGradingModule } from './peer-group-grading.module';
 import { SelectPeriodModule } from './select-period.module';
-import { ComponentGradingModule } from './component-grading.module';
 import { TeacherSummaryDisplay } from '../../assets/wise5/directives/teacher-summary-display/teacher-summary-display.component';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { StudentTeacherCommonModule } from '../student-teacher-common.module';
@@ -37,6 +36,7 @@ import { RouterModule } from '@angular/router';
 import { SaveIndicatorComponent } from '../../assets/wise5/common/save-indicator/save-indicator.component';
 import { PreviewComponentComponent } from '../../assets/wise5/authoringTool/components/preview-component/preview-component.component';
 import { StepToolsComponent } from '../../assets/wise5/common/stepTools/step-tools.component';
+import { ComponentGradingComponent } from '../../assets/wise5/classroomMonitor/classroomMonitorComponents/component-grading.component';
 
 @NgModule({
   declarations: [
@@ -61,7 +61,7 @@ import { StepToolsComponent } from '../../assets/wise5/common/stepTools/step-too
     ViewComponentRevisionsComponent
   ],
   imports: [
-    ComponentGradingModule,
+    ComponentGradingComponent,
     ComponentNewWorkBadgeComponent,
     ComponentSelectComponent,
     ComponentStudentModule,
