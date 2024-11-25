90db6a80bf165590039e7c5d94f41b33a57e788b
Hiroki Terashima <honchikun@gmail.com>
2024-11-25 10:01:29 -0800

    Don't emit outsideClick event if a dialog is opened. This fixes issue where choosing asset was exiting edit mode.

--- src/assets/wise5/directives/outside-click.directive.ts
+++ src/assets/wise5/directives/outside-click.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, EventEmitter, HostListener, Output, ElementRef } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Directive({
   selector: '[outsideClick]',
@@ -7,11 +8,17 @@ import { Directive, EventEmitter, HostListener, Output, ElementRef } from '@angu
 export class OutsideClickDirective {
   @Output() outsideClick: EventEmitter<MouseEvent> = new EventEmitter();
 
-  constructor(private elementRef: ElementRef) {}
+  constructor(
+    private elementRef: ElementRef,
+    private dialog: MatDialog
+  ) {}
 
   @HostListener('document:mousedown', ['$event'])
   onClick(event: MouseEvent): void {
-    if (!this.elementRef.nativeElement.contains(event.target)) {
+    if (
+      !this.elementRef.nativeElement.contains(event.target) &&
+      this.dialog.openDialogs.length == 0
+    ) {
       this.outsideClick.emit(event);
     }
   }
