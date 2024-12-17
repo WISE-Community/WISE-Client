133de222b8e76f921c10c0aa3e897fd817bbbf6d
Hiroki Terashima <honchikun@gmail.com>
2024-12-17 10:06:52 -0800

    refactor(NotificationsMenuComponent): Convert to standalone (#2022)

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/shared/notifications-menu/notifications-menu.component.spec.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/shared/notifications-menu/notifications-menu.component.spec.ts
@@ -1,36 +1,32 @@
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialog } from '@angular/material/dialog';
 import { of } from 'rxjs';
 import { Notification } from '../../../../../../app/domain/notification';
 import { DialogWithConfirmComponent } from '../../../../directives/dialog-with-confirm/dialog-with-confirm.component';
 import { NotificationService } from '../../../../services/notificationService';
 import { ClassroomMonitorTestingModule } from '../../../classroom-monitor-testing.module';
 import { NotificationsMenuComponent } from './notifications-menu.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
 let component: NotificationsMenuComponent;
 let dismissNotificationSpy: jasmine.Spy;
 let fixture: ComponentFixture<NotificationsMenuComponent>;
 const NODE_ID_1: string = 'node1';
 const NODE_ID_2: string = 'node1';
-const NODE_ROUTE = 'root.cm.unit.node';
 const notification1 = new Notification({ nodeId: NODE_ID_1 });
 const notification2 = new Notification({ nodeId: NODE_ID_2 });
-let stateGoSpy: jasmine.Spy;
-
 describe('NotificationsMenuComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    declarations: [DialogWithConfirmComponent, NotificationsMenuComponent],
-    imports: [ClassroomMonitorTestingModule,
-        MatDialogModule,
-        MatIconModule,
-        MatToolbarModule],
-    providers: [{ provide: MatDialog, useValue: { open: () => { } } }, provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
-}).compileComponents();
+      declarations: [DialogWithConfirmComponent],
+      imports: [ClassroomMonitorTestingModule, NotificationsMenuComponent],
+      providers: [
+        { provide: MatDialog, useValue: { open: () => {} } },
+        provideHttpClientTesting(),
+        provideRouter([])
+      ]
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -38,7 +34,6 @@ describe('NotificationsMenuComponent', () => {
     component = fixture.componentInstance;
     component.state = { go: () => {} };
     dismissNotificationSpy = spyOn(TestBed.inject(NotificationService), 'dismissNotification');
-    stateGoSpy = spyOn(component.state, 'go');
     fixture.detectChanges();
   });
 
@@ -57,7 +52,7 @@ function confirmDismissAllNotifications() {
         }
       } as any);
       component.newNotifications = [notification1, notification2];
-      component.confirmDismissAllNotifications();
+      component['confirmDismissAllNotifications']();
       expect(dialogOpenSpy).toHaveBeenCalled();
       expect(dismissNotificationSpy).toHaveBeenCalledTimes(2);
     });
@@ -67,7 +62,7 @@ function confirmDismissAllNotifications() {
 function dismissNotification() {
   describe('dismissNotification', () => {
     it('should dismiss notification', () => {
-      component.dismissNotification(notification1);
+      component['dismissNotification'](notification1);
       expect(dismissNotificationSpy).toHaveBeenCalledWith(notification1);
     });
   });
@@ -76,8 +71,9 @@ function dismissNotification() {
 function visitNode() {
   describe('visitNode', () => {
     it('should visit node', () => {
-      component.visitNode(notification1);
-      expect(stateGoSpy).toHaveBeenCalledWith(NODE_ROUTE, { nodeId: NODE_ID_1 });
+      const routerSpy = spyOn(component['router'], 'navigate');
+      component['visitNode'](notification1);
+      expect(routerSpy).toHaveBeenCalled();
     });
   });
 }
