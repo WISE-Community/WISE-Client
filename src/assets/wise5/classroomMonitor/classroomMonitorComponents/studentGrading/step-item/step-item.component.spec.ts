8b5e5dd782cce19144c5bfedf652e3165895f476
Hiroki Terashima <honchikun@gmail.com>
2024-12-20 10:20:27 -0800

    refactor(StepItemComponent): Convert to standalone (#2028)

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/studentGrading/step-item/step-item.component.spec.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/studentGrading/step-item/step-item.component.spec.ts
@@ -2,19 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ClassroomMonitorTestingModule } from '../../../classroom-monitor-testing.module';
 import { StepItemComponent } from './step-item.component';
 import { TeacherProjectService } from '../../../../services/teacherProjectService';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 let component: StepItemComponent;
 let fixture: ComponentFixture<StepItemComponent>;
 let teacherProjectService: TeacherProjectService;
-
 describe('StepItemComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [StepItemComponent],
-      imports: [ClassroomMonitorTestingModule],
-      providers: [TeacherProjectService],
-      schemas: [NO_ERRORS_SCHEMA]
+      imports: [ClassroomMonitorTestingModule, StepItemComponent],
+      providers: [TeacherProjectService]
     }).compileComponents();
   });
 
