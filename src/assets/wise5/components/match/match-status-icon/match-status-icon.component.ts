f940cfe36c1d36f1ad934ab2e2083fe6f91e5ac6
Aaron <aarondetre@gmail.com>
2025-01-15 21:29:39 -0800

    refactor(MatchStatusIcon): Moved HTML to template

--- src/assets/wise5/components/match/match-status-icon/match-status-icon.component.ts
+++ src/assets/wise5/components/match/match-status-icon/match-status-icon.component.ts
@@ -5,7 +5,19 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [MatIconModule],
   selector: 'match-status-icon',
   standalone: true,
-  templateUrl: 'match-status-icon.component.html'
+  template: `
+    @switch (status) {
+      @case ('correct') {
+        <mat-icon class="mat-18" aria-label="Correct" i18n-aria-label>check</mat-icon>
+      }
+      @case ('warn') {
+        <mat-icon class="mat-18" aria-label="Warning" i18n-aria-label>warning</mat-icon>
+      }
+      @case ('incorrect') {
+        <mat-icon class="mat-18" aria-label="Incorrect" i18n-aria-label>clear</mat-icon>
+      }
+    }
+  `
 })
 export class MatchStatusIconComponent {
   @Input() status: string;
