3e14f3e0e48058ed0401d728fb68a470675aab81
Aaron Detre <aarondetre@gmail.com>
2025-01-17 11:39:55 -0800

    refactor(AddChoiceButton): Convert to standalone

--- src/assets/wise5/components/match/match-student/add-choice-button/add-choice-button.component.ts
+++ src/assets/wise5/components/match/match-student/add-choice-button/add-choice-button.component.ts
@@ -1,10 +1,26 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
+  imports: [MatIconModule, MatButtonModule],
   selector: 'add-choice-button',
-  templateUrl: 'add-choice-button.component.html'
+  standalone: true,
+  template: `
+    <button
+      mat-mini-fab
+      color="primary"
+      (click)="onClick.next()"
+      [disabled]="isDisabled"
+      matTooltip="Add item"
+      matTooltipPosition="above"
+      i18n-matTooltip
+    >
+      <mat-icon>add</mat-icon>
+    </button>
+  `
 })
-export class AddChoiceButton {
+export class AddChoiceButtonComponent {
   @Input() isDisabled: boolean;
   @Output() onClick = new EventEmitter<void>();
 }
--- src/assets/wise5/components/match/match-student/match-student.module.ts
+++ src/assets/wise5/components/match/match-student/match-student.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { AddChoiceButton } from './add-choice-button/add-choice-button.component';
+import { AddChoiceButtonComponent } from './add-choice-button/add-choice-button.component';
 import { AddMatchChoiceDialog } from './add-match-choice-dialog/add-match-choice-dialog';
 import { MatchStudent } from './match-student.component';
 import { StudentComponentModule } from '../../../../../app/student/student.component.module';
@@ -8,14 +8,17 @@ import { MatchStudentChoiceReuse } from './match-student-choice-reuse/match-stud
 import { MatchStudentDefault } from './match-student-default/match-student-default.component';
 
 @NgModule({
-  declarations: [
-    AddChoiceButton,
+  declarations: [ 
     AddMatchChoiceDialog,
     MatchStudent,
     MatchStudentDefault,
     MatchStudentChoiceReuse
   ],
-  imports: [MatchCommonModule, StudentComponentModule],
+  imports: [
+    AddChoiceButtonComponent, 
+    MatchCommonModule, 
+    StudentComponentModule
+  ],
   exports: [MatchStudent]
 })
 export class MatchStudentModule {}
