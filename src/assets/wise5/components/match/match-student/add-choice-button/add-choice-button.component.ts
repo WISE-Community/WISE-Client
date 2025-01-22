9e14481d1574a1c807ee0afee23ce594b06d5acc
Aaron Detre <168240861+Aaron-Detre@users.noreply.github.com>
2025-01-21 17:25:09 -0800

    refactor(AddChoiceButton): Convert to standalone (#2053)
    Co-authored-by: Hiroki Terashima <honchikun@gmail.com>

--- src/assets/wise5/components/match/match-student/add-choice-button/add-choice-button.component.ts
+++ src/assets/wise5/components/match/match-student/add-choice-button/add-choice-button.component.ts
@@ -1,10 +1,27 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule],
   selector: 'add-choice-button',
-  templateUrl: 'add-choice-button.component.html'
+  standalone: true,
+  template: `
+    <button
+      mat-mini-fab
+      color="primary"
+      (click)="onClick.next()"
+      [disabled]="isDisabled"
+      matTooltip="Add item"
+      matTooltipPosition="above"
+      i18n-matTooltip
+    >
+      <mat-icon>add</mat-icon>
+    </button>
+  `
 })
-export class AddChoiceButton {
+export class AddChoiceButtonComponent {
   @Input() isDisabled: boolean;
   @Output() onClick = new EventEmitter<void>();
 }
