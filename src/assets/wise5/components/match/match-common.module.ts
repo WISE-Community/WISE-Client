b1adb3873a37fc9cf7287e1dc366d9e9a6032fe4
Aaron Detre <aarondetre@gmail.com>
2025-01-23 00:57:26 -0800

    refactor(MatchChoiceItem): Convert to standalone

--- src/assets/wise5/components/match/match-common.module.ts
+++ src/assets/wise5/components/match/match-common.module.ts
@@ -5,18 +5,17 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatchChoiceItemComponent } from './match-choice-item/match-choice-item.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { DeleteChoiceButtonComponent } from './delete-choice-button/delete-choice-button.component';
-import { MatchChoiceItem } from './match-choice-item/match-choice-item.component';
 import { MatchStatusIconComponent } from './match-status-icon/match-status-icon.component';
 import { MatchFeedbackSectionComponent } from './match-student/match-feedback-section/match-feedback-section.component';
 
 @NgModule({
-  declarations: [MatchChoiceItem],
   imports: [
     CommonModule,
     DeleteChoiceButtonComponent,
@@ -24,6 +23,7 @@ import { MatchFeedbackSectionComponent } from './match-student/match-feedback-se
     FlexLayoutModule,
     MatButtonModule,
     MatCardModule,
+    MatchChoiceItemComponent,
     MatchFeedbackSectionComponent,
     MatchStatusIconComponent,
     MatDialogModule,
@@ -41,7 +41,7 @@ import { MatchFeedbackSectionComponent } from './match-student/match-feedback-se
     MatButtonModule,
     MatCardModule,
     MatDialogModule,
-    MatchChoiceItem,
+    MatchChoiceItemComponent,
     MatchFeedbackSectionComponent,
     MatchStatusIconComponent,
     MatFormFieldModule,
