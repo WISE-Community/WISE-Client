2bd7c70f8aae805f52a2dad6918ce2eb96b5711c
Aaron Detre <aarondetre@gmail.com>
2025-01-23 00:57:26 -0800

    refactor(MatchChoiceItem): Convert to standalone

--- src/assets/wise5/components/match/match-choice-item/match-choice-item.component.ts
+++ src/assets/wise5/components/match/match-choice-item/match-choice-item.component.ts
@@ -1,24 +1,26 @@
 import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatchStatusIconComponent } from '../match-status-icon/match-status-icon.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { NgClass } from '@angular/common';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
+  imports: [FlexLayoutModule, MatchStatusIconComponent, MatIconModule, MatCardModule, NgClass],
   selector: 'match-choice-item',
-  templateUrl: 'match-choice-item.component.html',
-  styleUrls: ['match-choice-item.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  standalone: true,
+  styleUrl: 'match-choice-item.component.scss',
+  templateUrl: 'match-choice-item.component.html'
 })
 export class MatchChoiceItem {
-  @Input()
-  buckets: any;
+  @Input() buckets: any;
 
-  @Input()
-  hasCorrectAnswer: boolean;
+  @Input() hasCorrectAnswer: boolean;
 
-  @Input()
-  isDisabled: boolean;
+  @Input() isDisabled: boolean;
 
-  @Input()
-  item: any;
+  @Input() item: any;
 
-  @Output()
-  onStudentDataChanged = new EventEmitter();
+  @Output() onStudentDataChanged = new EventEmitter();
 }
--- src/assets/wise5/components/match/match-common.module.ts
+++ src/assets/wise5/components/match/match-common.module.ts
@@ -5,24 +5,25 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatchChoiceItem } from './match-choice-item/match-choice-item.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { DeleteChoiceButton } from './delete-choice-button/delete-choice-button.component';
-import { MatchChoiceItem } from './match-choice-item/match-choice-item.component';
 import { MatchStatusIconComponent } from './match-status-icon/match-status-icon.component';
 import { MatchFeedbackSectionComponent } from './match-student/match-feedback-section/match-feedback-section.component';
 
 @NgModule({
-  declarations: [DeleteChoiceButton, MatchChoiceItem],
+  declarations: [DeleteChoiceButton],
   imports: [
     CommonModule,
     DragDropModule,
     FlexLayoutModule,
     MatButtonModule,
     MatCardModule,
+    MatchChoiceItem,
     MatchFeedbackSectionComponent,
     MatchStatusIconComponent,
     MatDialogModule,
