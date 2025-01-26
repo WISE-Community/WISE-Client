ccbf5dc5187ec22560232c34d8784a80cfd66d29
Hiroki Terashima <honchikun@gmail.com>
2025-01-26 15:21:33 -0800

    Add CommonModule. Removed empty lines in variable declaration section

--- src/assets/wise5/components/match/match-choice-item/match-choice-item.component.ts
+++ src/assets/wise5/components/match/match-choice-item/match-choice-item.component.ts
@@ -1,13 +1,21 @@
 import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import { DeleteChoiceButtonComponent } from '../delete-choice-button/delete-choice-button.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatchStatusIconComponent } from '../match-status-icon/match-status-icon.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { NgClass } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
-  imports: [FlexLayoutModule, MatCardModule, MatchStatusIconComponent, MatIconModule, NgClass],
+  imports: [
+    CommonModule,
+    DeleteChoiceButtonComponent,
+    FlexLayoutModule,
+    MatCardModule,
+    MatchStatusIconComponent,
+    MatIconModule
+  ],
   selector: 'match-choice-item',
   standalone: true,
   styleUrl: 'match-choice-item.component.scss',
@@ -15,12 +23,8 @@ import { NgClass } from '@angular/common';
 })
 export class MatchChoiceItemComponent {
   @Input() buckets: any;
-
   @Input() hasCorrectAnswer: boolean;
-
   @Input() isDisabled: boolean;
-
   @Input() item: any;
-
   @Output() onStudentDataChanged = new EventEmitter();
 }
