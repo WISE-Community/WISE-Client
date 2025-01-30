2f509908e69eb303be19f5178b8f32126247f1a7
Hiroki Terashima <honchikun@gmail.com>
2025-01-30 06:05:43 -0800

    refactor(MatchShowWorkComponent): Convert to standalone (#2075)

--- src/assets/wise5/components/showWork/show-work-student/show-work-student.module.ts
+++ src/assets/wise5/components/showWork/show-work-student/show-work-student.module.ts
@@ -8,12 +8,12 @@ import { DrawShowWorkModule } from '../../draw/draw-show-work/draw-show-work.mod
 import { EmbeddedShowWorkModule } from '../../embedded/embedded-show-work/embedded-show-work.module';
 import { GraphShowWorkModule } from '../../graph/graph-show-work/graph-show-work.module';
 import { LabelShowWorkModule } from '../../label/label-show-work/label-show-work.module';
-import { MatchShowWorkModule } from '../../match/match-show-work/match-show-work-module';
 import { OpenResponseShowWorkModule } from '../../openResponse/open-response-show-work/open-response-show-work.module';
 import { TableShowWorkModule } from '../../table/table-show-work/table-show-work.module';
 import { ShowWorkStudentComponent } from './show-work-student.component';
 import { MultipleChoiceShowWorkComponent } from '../../multipleChoice/multiple-choice-show-work/multiple-choice-show-work.component';
 import { DialogGuidanceShowWorkComponent } from '../../dialogGuidance/dialog-guidance-show-work/dialog-guidance-show-work.component';
+import { MatchShowWorkComponent } from '../../match/match-show-work/match-show-work.component';
 
 @NgModule({
   declarations: [ShowWorkStudentComponent],
@@ -28,7 +28,7 @@ import { DialogGuidanceShowWorkComponent } from '../../dialogGuidance/dialog-gui
     EmbeddedShowWorkModule,
     GraphShowWorkModule,
     LabelShowWorkModule,
-    MatchShowWorkModule,
+    MatchShowWorkComponent,
     MultipleChoiceShowWorkComponent,
     OpenResponseShowWorkModule,
     TableShowWorkModule
