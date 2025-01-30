2acad72a5f1ad8f1bae8271570a194b534351ad7
Hiroki Terashima <honchikun@gmail.com>
2025-01-30 05:35:26 -0800

    refactor(MatchShowWorkComponent): Convert to standalone

--- src/app/teacher/component-show-work.module.ts
+++ src/app/teacher/component-show-work.module.ts
@@ -7,11 +7,11 @@ import { DrawShowWorkModule } from '../../assets/wise5/components/draw/draw-show
 import { EmbeddedShowWorkModule } from '../../assets/wise5/components/embedded/embedded-show-work/embedded-show-work.module';
 import { GraphShowWorkModule } from '../../assets/wise5/components/graph/graph-show-work/graph-show-work.module';
 import { LabelShowWorkModule } from '../../assets/wise5/components/label/label-show-work/label-show-work.module';
-import { MatchShowWorkModule } from '../../assets/wise5/components/match/match-show-work/match-show-work-module';
 import { OpenResponseShowWorkModule } from '../../assets/wise5/components/openResponse/open-response-show-work/open-response-show-work.module';
 import { TableShowWorkModule } from '../../assets/wise5/components/table/table-show-work/table-show-work.module';
 import { MultipleChoiceShowWorkComponent } from '../../assets/wise5/components/multipleChoice/multiple-choice-show-work/multiple-choice-show-work.component';
 import { DialogGuidanceShowWorkComponent } from '../../assets/wise5/components/dialogGuidance/dialog-guidance-show-work/dialog-guidance-show-work.component';
+import { MatchShowWorkComponent } from '../../assets/wise5/components/match/match-show-work/match-show-work.component';
 
 @NgModule({
   imports: [
@@ -24,7 +24,7 @@ import { DialogGuidanceShowWorkComponent } from '../../assets/wise5/components/d
     EmbeddedShowWorkModule,
     GraphShowWorkModule,
     LabelShowWorkModule,
-    MatchShowWorkModule,
+    MatchShowWorkComponent,
     MultipleChoiceShowWorkComponent,
     OpenResponseShowWorkModule,
     TableShowWorkModule
@@ -39,7 +39,7 @@ import { DialogGuidanceShowWorkComponent } from '../../assets/wise5/components/d
     EmbeddedShowWorkModule,
     GraphShowWorkModule,
     LabelShowWorkModule,
-    MatchShowWorkModule,
+    MatchShowWorkComponent,
     MultipleChoiceShowWorkComponent,
     OpenResponseShowWorkModule,
     TableShowWorkModule
