2daa8b96a2c755027ec6940b5b5f242cdef57d28
Jonathan Lim-Breitbart <breity10@gmail.com>
2025-02-05 09:13:13 -0800

    feat(DialogGuidance): Update styles for deteceted ideas display

--- src/assets/wise5/components/dialogGuidance/detected-ideas/detected-ideas.component.ts
+++ src/assets/wise5/components/dialogGuidance/detected-ideas/detected-ideas.component.ts
@@ -2,12 +2,13 @@ import { Component, Input } from '@angular/core';
 import { CRaterRubric, getUniqueIdeas } from '../../common/cRater/CRaterRubric';
 import { CRaterIdea } from '../../common/cRater/CRaterIdea';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   selector: 'detected-ideas',
   standalone: true,
-  styleUrl: './detected-ideas.component.scss',
+  styleUrls: ['./detected-ideas.component.scss'],
   templateUrl: './detected-ideas.component.html'
 })
 export class DetectedIdeasComponent {
