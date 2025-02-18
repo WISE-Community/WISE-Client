a3aa0018e0282c029bf02d0c5a8436aa5ddfc788
Hiroki Terashima <honchikun@gmail.com>
2025-02-18 15:19:04 -0800

    Convert LabelShowWorkComponent to standalone. Remove unnecessary LabelShowWorkModule

--- src/assets/wise5/components/showWork/show-work-student/show-work-student.module.ts
+++ src/assets/wise5/components/showWork/show-work-student/show-work-student.module.ts
@@ -7,13 +7,13 @@ import { DiscussionStudentModule } from '../../discussion/discussion-student/dis
 import { DrawShowWorkModule } from '../../draw/draw-show-work/draw-show-work.module';
 import { EmbeddedShowWorkModule } from '../../embedded/embedded-show-work/embedded-show-work.module';
 import { GraphShowWorkModule } from '../../graph/graph-show-work/graph-show-work.module';
-import { LabelShowWorkModule } from '../../label/label-show-work/label-show-work.module';
 import { OpenResponseShowWorkModule } from '../../openResponse/open-response-show-work/open-response-show-work.module';
 import { TableShowWorkModule } from '../../table/table-show-work/table-show-work.module';
 import { ShowWorkStudentComponent } from './show-work-student.component';
 import { MultipleChoiceShowWorkComponent } from '../../multipleChoice/multiple-choice-show-work/multiple-choice-show-work.component';
 import { DialogGuidanceShowWorkComponent } from '../../dialogGuidance/dialog-guidance-show-work/dialog-guidance-show-work.component';
 import { MatchShowWorkComponent } from '../../match/match-show-work/match-show-work.component';
+import { LabelShowWorkComponent } from '../../label/label-show-work/label-show-work.component';
 
 @NgModule({
   declarations: [ShowWorkStudentComponent],
@@ -27,7 +27,7 @@ import { MatchShowWorkComponent } from '../../match/match-show-work/match-show-w
     DrawShowWorkModule,
     EmbeddedShowWorkModule,
     GraphShowWorkModule,
-    LabelShowWorkModule,
+    LabelShowWorkComponent,
     MatchShowWorkComponent,
     MultipleChoiceShowWorkComponent,
     OpenResponseShowWorkModule,
