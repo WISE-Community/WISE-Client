8419d77ec1cd39bcf3bd472be115ca8669445de0
Hiroki Terashima <honchikun@gmail.com>
2025-02-20 13:02:46 -0800

    Remove ManageUserComponent import since it's imported in ManageTeamComponent.

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/manageStudents/manage-students.module.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/manageStudents/manage-students.module.ts
@@ -4,7 +4,6 @@ import { ManageStudentsComponent } from './manage-students/manage-students.compo
 import { ManageTeamsComponent } from './manage-teams/manage-teams.component';
 import { ManagePeriodComponent } from './manage-period/manage-period.component';
 import { ManageTeamComponent } from './manage-team/manage-team.component';
-import { ManageUserComponent } from './manage-user/manage-user.component';
 import { ShowStudentInfoComponent } from './show-student-info/show-student-info.component';
 import { ManageShowStudentInfoComponent } from './manage-show-student-info/manage-show-student-info.component';
 import { RemoveUserConfirmDialogComponent } from './remove-user-confirm-dialog/remove-user-confirm-dialog.component';
@@ -18,7 +17,6 @@ import { PasswordModule } from '../../../../../app/password/password.module';
 @NgModule({
   imports: [
     ManageTeamComponent,
-    ManageUserComponent,
     PasswordModule,
     ShowStudentInfoComponent,
     StudentTeacherCommonModule
