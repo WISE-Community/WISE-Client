c76f16705140fc2faae3e97b542110eea4ed19a9
Hiroki Terashima <honchikun@gmail.com>
2025-03-12 15:00:40 -0700

    feat(Grade By Component): Show component average score (#2112)
    Co-authored-by: Jonathan Lim-Breitbart <breity10@gmail.com>

---
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/component-average-score/component-average-score.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input } from '@angular/core';
+import { Node } from '../../../common/Node';
+import { AnnotationService } from '../../../services/annotationService';
+import { DecimalPipe } from '@angular/common';
+
+@Component({
+  imports: [DecimalPipe],
+  selector: 'component-average-score',
+  template: `
+    @if (avgScore) {
+      {{ avgScore | number: '1.0-1' }}
+    } @else {
+      -
+    }
+  `
+})
+export class ComponentAverageScoreComponent {
+  protected avgScore: number | 'NA';
+  @Input() component: any;
+  @Input() node: Node;
+  @Input() periodId: number;
+
+  constructor(private annotationService: AnnotationService) {}
+
+  ngOnChanges(): void {
+    if (this.component && this.node) {
+      const annotations = this.getLatestScoreAnnotations();
+      const totalScore = annotations.reduce((sumSoFar, a) => sumSoFar + a.data.value, 0);
+      this.avgScore = totalScore / annotations.length;
+    }
+  }
+
+  private getLatestScoreAnnotations() {
+    return this.annotationService
+      .getAnnotationsByNodeIdComponentId(this.node.id, this.component.id)
+      .filter((annotation) => this.periodId === -1 || annotation.periodId === this.periodId)
+      .filter((annotation) => ['score', 'autoScore'].includes(annotation.type))
+      .reduceRight((soFar, currentA) => {
+        if (!soFar.some((soFarA) => soFarA.toWorkgroupId === currentA.toWorkgroupId)) {
+          soFar.push(currentA);
+        }
+        return soFar;
+      }, []);
+  }
+}
