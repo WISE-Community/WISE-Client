9706ce75326774b4e68ec04a9a674d0c031be634
Hiroki Terashima <honchikun@gmail.com>
2025-03-14 10:56:01 -0700

    test(ComponentWorkgroupItemComponent): add test to ensure that onUpdatedEvent is emitted when button is clicked

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/component-workgroup-item/component-workgroup-item.component.spec.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/component-workgroup-item/component-workgroup-item.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ComponentWorkgroupItemComponent } from './component-workgroup-item.component';
-import { ClassroomMonitorTestingModule } from '../../classroom-monitor-testing.module';
+import { MockProvider } from 'ng-mocks';
+import { TeacherProjectService } from '../../../services/teacherProjectService';
+import { By } from '@angular/platform-browser';
 
+let component: ComponentWorkgroupItemComponent;
+let fixture: ComponentFixture<ComponentWorkgroupItemComponent>;
 describe('ComponentWorkgroupItemComponent', () => {
-  let component: ComponentWorkgroupItemComponent;
-  let fixture: ComponentFixture<ComponentWorkgroupItemComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ClassroomMonitorTestingModule, ComponentWorkgroupItemComponent]
+      imports: [ComponentWorkgroupItemComponent],
+      providers: [MockProvider(TeacherProjectService)]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ComponentWorkgroupItemComponent);
     component = fixture.componentInstance;
+    component.workgroupId = 1;
     component.workgroupData = {};
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  clickOnButton_EmitOnUpdatedEvent();
 });
+
+function clickOnButton_EmitOnUpdatedEvent() {
+  describe('click on button', () => {
+    it('should emit onUpdateExpand event', () => {
+      const spy = spyOn(component.onUpdateExpand, 'emit');
+      fixture.debugElement.query(By.css('button')).nativeElement.click();
+      expect(spy).toHaveBeenCalledWith({ workgroupId: 1, value: true });
+    });
+  });
+}
