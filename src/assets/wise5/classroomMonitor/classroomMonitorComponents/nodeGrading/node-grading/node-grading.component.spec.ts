40875938b10e4e3802658ca3b0a3b62d4d307032
Hiroki Terashima <honchikun@gmail.com>
2025-03-17 08:42:00 -0700

    Fix issue where node completion wasn't being calculated. Refactor code and add test.

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/nodeGrading/node-grading/node-grading.component.spec.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/nodeGrading/node-grading/node-grading.component.spec.ts
@@ -1,31 +1,78 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NodeGradingComponent } from './node-grading.component';
-import { ClassroomMonitorTestingModule } from '../../../classroom-monitor-testing.module';
 import { TeacherProjectService } from '../../../../services/teacherProjectService';
 import { TeacherDataService } from '../../../../services/teacherDataService';
 import { provideRouter } from '@angular/router';
+import { MockProviders } from 'ng-mocks';
+import { ClassroomStatusService } from '../../../../services/classroomStatusService';
+import { Node } from '../../../../common/Node';
+import { Observable, Subject } from 'rxjs';
+
+let classroomStatusService: ClassroomStatusService;
+let component: NodeGradingComponent;
+let dataService: TeacherDataService;
+let fixture: ComponentFixture<NodeGradingComponent>;
+let nodeCompletionSpy;
+
+class MockDataService {
+  private currentNodeChangedSource: Subject<any> = new Subject<any>();
+  public currentNodeChanged$ = this.currentNodeChangedSource.asObservable();
+  private currentPeriodChangedSource: Subject<any> = new Subject<any>();
+  public currentPeriodChanged$: Observable<any> = this.currentPeriodChangedSource.asObservable();
+
+  getCurrentPeriodId(): number {
+    return 1;
+  }
+
+  setCurrentNodeByNodeId(): void {}
+
+  setCurrentPeriod(period: any): void {
+    this.currentPeriodChangedSource.next({
+      previousPeriod: null,
+      currentPeriod: period
+    });
+  }
+}
 
 describe('NodeGradingComponent', () => {
-  let component: NodeGradingComponent;
-  let fixture: ComponentFixture<NodeGradingComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ClassroomMonitorTestingModule, NodeGradingComponent],
-      providers: [provideRouter([])]
+      imports: [NodeGradingComponent],
+      providers: [
+        MockProviders(ClassroomStatusService, TeacherProjectService),
+        provideRouter([]),
+        { provide: TeacherDataService, useClass: MockDataService }
+      ]
     }).compileComponents();
     const projectService = TestBed.inject(TeacherProjectService);
+    classroomStatusService = TestBed.inject(ClassroomStatusService);
     spyOn(projectService, 'nodeHasWork').and.returnValue(false);
+    const node = new Node();
+    node.components = [{ id: 'component1' }];
+    spyOn(projectService, 'getNode').and.returnValue(node);
     spyOn(projectService, 'getComponents').and.returnValue([{ id: 'abc', type: 'MultipleChoice' }]);
     spyOn(projectService, 'componentHasWork').and.returnValue(true);
-    const dataService = TestBed.inject(TeacherDataService);
+    dataService = TestBed.inject(TeacherDataService);
     spyOn(dataService, 'getCurrentPeriodId').and.returnValue(1);
+    nodeCompletionSpy = spyOn(classroomStatusService, 'getNodeCompletion').and.returnValue({
+      completionPct: 50
+    });
     fixture = TestBed.createComponent(NodeGradingComponent);
     component = fixture.componentInstance;
+    component.nodeId = 'node1';
+    component.ngOnInit();
     component.ngOnChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  periodChanged_RecalculateNodeCompletion();
 });
+
+function periodChanged_RecalculateNodeCompletion() {
+  describe('period changed', () => {
+    it('recalculates node completion', () => {
+      expect(nodeCompletionSpy).toHaveBeenCalledTimes(1);
+      dataService.setCurrentPeriod({ periodId: 1 });
+      expect(nodeCompletionSpy).toHaveBeenCalledTimes(2);
+    });
+  });
+}
--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/nodeGrading/node-grading/node-grading.component.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/nodeGrading/node-grading/node-grading.component.ts
@@ -15,7 +15,6 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 @Component({
   imports: [CommonModule, FlexLayoutModule, MatIconModule, RouterModule, SelectComponentComponent],
   selector: 'node-grading',
-  standalone: true,
   templateUrl: './node-grading.component.html'
 })
 export class NodeGradingComponent {
@@ -26,7 +25,7 @@ export class NodeGradingComponent {
   protected nodeCompletionPercent: number;
   @Input() nodeId: string;
   protected numRubrics: number;
-  protected periodId: number;
+  private periodId: number;
   private subscriptions: Subscription = new Subscription();
 
   constructor(
@@ -43,6 +42,9 @@ export class NodeGradingComponent {
       this.dataService.currentNodeChanged$.subscribe(({ currentNode }) => {
         this.node = currentNode;
       })
+    );
+    this.subscriptions.add(
+      this.dataService.currentPeriodChanged$.subscribe(() => this.setPeriod())
     );
     setTimeout(() => {
       // allow the current change detection cycle to complete before triggering the navigation
@@ -62,20 +64,32 @@ export class NodeGradingComponent {
   private setNode(): void {
     this.hasWork = this.projectService.nodeHasWork(this.nodeId);
     this.node = this.projectService.getNode(this.nodeId);
-    this.nodeAverageScore = this.classroomStatusService.getNodeAverageScore(
-      this.nodeId,
-      this.dataService.getCurrentPeriodId()
-    );
-    this.nodeCompletionPercent = this.classroomStatusService.getNodeCompletion(
-      this.nodeId,
-      this.dataService.getCurrentPeriodId()
-    ).completionPct;
     this.components = this.projectService
       .getComponents(this.nodeId)
       .filter((component) => this.projectService.componentHasWork(component));
     this.numRubrics = this.node.getNumRubrics();
     this.dataService.setCurrentNodeByNodeId(this.nodeId);
+    this.setPeriod();
+  }
+
+  private setPeriod(): void {
     this.periodId = this.dataService.getCurrentPeriodId();
+    this.setNodeAverageScore();
+    this.setNodeCompletionPercent();
+  }
+
+  private setNodeAverageScore(): void {
+    this.nodeAverageScore = this.classroomStatusService.getNodeAverageScore(
+      this.nodeId,
+      this.periodId
+    );
+  }
+
+  private setNodeCompletionPercent(): void {
+    this.nodeCompletionPercent = this.classroomStatusService.getNodeCompletion(
+      this.nodeId,
+      this.periodId
+    ).completionPct;
   }
 
   protected showRubric(): void {
