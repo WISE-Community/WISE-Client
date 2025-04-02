543d4d5d6cf385ac5df9796fce6f70f933b6f715
Aaron Detre <aarondetre@gmail.com>
2025-04-02 13:41:42 -0700

    refactor(EditOpenResponseAdvancedComponent): Replace ngIf and ngFor statements (#2129)
    Co-authored-by: Hiroki Terashima <honchikun@gmail.com>

--- src/assets/wise5/components/openResponse/edit-open-response-advanced/edit-open-response-advanced.component.ts
+++ src/assets/wise5/components/openResponse/edit-open-response-advanced/edit-open-response-advanced.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { EditAdvancedComponentComponent } from '../../../../../app/authoring-tool/edit-advanced-component/edit-advanced-component.component';
+import { ComponentContent } from '../../../common/ComponentContent';
 import { CRaterService } from '../../../services/cRaterService';
+import { EditAdvancedComponentComponent } from '../../../../../app/authoring-tool/edit-advanced-component/edit-advanced-component.component';
 import { NotebookService } from '../../../services/notebookService';
-import { TeacherProjectService } from '../../../services/teacherProjectService';
 import { OpenResponseContent } from '../OpenResponseContent';
 import { TeacherNodeService } from '../../../services/teacherNodeService';
+import { TeacherProjectService } from '../../../services/teacherProjectService';
 
 @Component({
-    selector: 'edit-open-response-advanced',
-    templateUrl: 'edit-open-response-advanced.component.html',
-    styleUrls: ['edit-open-response-advanced.component.scss'],
-    standalone: false
+  selector: 'edit-open-response-advanced',
+  templateUrl: 'edit-open-response-advanced.component.html',
+  styleUrls: ['edit-open-response-advanced.component.scss'],
+  standalone: false
 })
 export class EditOpenResponseAdvancedComponent extends EditAdvancedComponentComponent {
   allowedConnectedComponentTypes = ['OpenResponse'];
@@ -129,9 +130,8 @@ export class EditOpenResponseAdvancedComponent extends EditAdvancedComponentComp
   }
 
   multipleAttemptScoringRuleDeleteClicked(index: number): void {
-    const multipleAttemptScoringRule = this.componentContent.cRater.multipleAttemptScoringRules[
-      index
-    ];
+    const multipleAttemptScoringRule =
+      this.componentContent.cRater.multipleAttemptScoringRules[index];
     const scoreSequence = multipleAttemptScoringRule.scoreSequence;
     let previousScore = '';
     let currentScore = '';
@@ -261,7 +261,7 @@ export class EditOpenResponseAdvancedComponent extends EditAdvancedComponentComp
     }
   }
 
-  getComponents(nodeId: string): any[] {
+  getComponents(nodeId: string): ComponentContent[] {
     return this.teacherProjectService.getComponents(nodeId);
   }
 
