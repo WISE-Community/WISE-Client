3a8b37681a9cf39acbca2a865359f290abe5091d
Aaron Detre <aarondetre@gmail.com>
2025-04-08 13:59:48 -0700

    feat(Dialog Guidance): Summary display for teachers (#2115)
    Co-authored-by: Jonathan Lim-Breitbart <breity10@gmail.com>

---
+++ src/assets/wise5/directives/teacher-summary-display/dialog-guidance-teacher-summary-display.component.spec.ts
@@ -0,0 +1,135 @@
+import { AnnotationService } from '../../services/annotationService';
+import { ComponentFixture } from '@angular/core/testing';
+import { ComponentState } from '../../../../app/domain/componentState';
+import { ConfigService } from '../../services/configService';
+import { CRaterIdea } from '../../components/common/cRater/CRaterIdea';
+import { CRaterRubric } from '../../components/common/cRater/CRaterRubric';
+import { CRaterService } from '../../services/cRaterService';
+import { DialogGuidanceTeacherSummaryDisplayComponent } from './dialog-guidance-teacher-summary-display.component';
+import { MockProviders } from 'ng-mocks';
+import { Observable, of } from 'rxjs';
+import { SummaryService } from '../../components/summary/summaryService';
+import { TeacherDataService } from '../../services/teacherDataService';
+import { TeacherProjectService } from '../../services/teacherProjectService';
+import { TestBed } from '@angular/core/testing';
+
+let component: DialogGuidanceTeacherSummaryDisplayComponent;
+let fixture: ComponentFixture<DialogGuidanceTeacherSummaryDisplayComponent>;
+describe('DialogGuidanceTeacherSummaryDisplayComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DialogGuidanceTeacherSummaryDisplayComponent],
+      providers: [
+        MockProviders(
+          AnnotationService,
+          ConfigService,
+          CRaterService,
+          TeacherDataService,
+          TeacherProjectService,
+          SummaryService
+        )
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DialogGuidanceTeacherSummaryDisplayComponent);
+    component = fixture.componentInstance;
+    component.doRender = true;
+    // Set up component?
+  });
+  beforeEach(() => {
+    spyOn(TestBed.inject(ConfigService), 'isPreview').and.returnValue(false);
+    spyOn(TestBed.inject(ConfigService), 'isAuthoring').and.returnValue(false);
+    spyOn(TestBed.inject(ConfigService), 'isStudentRun').and.returnValue(false);
+    spyOn(TestBed.inject(ConfigService), 'getNumberOfWorkgroupsInPeriod').and.returnValue(1);
+  });
+  ngOnInit();
+});
+
+function ngOnInit() {
+  describe('ngOnChanges()', () => {
+    ngInit_NoIdeasDetected_ShowMessage();
+    ngInit_IdeasDetected_ShowSummary();
+    ngInit_ManyIdeasDetected_ShowTopAndBottomThree();
+  });
+}
+
+function ngInit_NoIdeasDetected_ShowMessage() {
+  describe('no ideas detected', () => {
+    beforeEach(() => {
+      spyOn(TestBed.inject(CRaterService), 'getCRaterRubric').and.returnValue(
+        generateMockRubric(3, 0)
+      );
+      spyOn(TestBed.inject(SummaryService), 'getLatestClassmateStudentWork').and.returnValue(
+        generateMockStudentWork(0)
+      );
+    });
+    it('shows message to teacher', () => {
+      component.ngOnInit();
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelector('.notice').textContent).toContain(
+        "Your students' ideas will show up here as they are detected in the dialog."
+      );
+    });
+  });
+}
+
+function ngInit_IdeasDetected_ShowSummary() {
+  describe('ideas detected', () => {
+    beforeEach(() => {
+      spyOn(TestBed.inject(CRaterService), 'getCRaterRubric').and.returnValue(
+        generateMockRubric(3, 1)
+      );
+      spyOn(TestBed.inject(SummaryService), 'getLatestClassmateStudentWork').and.returnValue(
+        generateMockStudentWork(1)
+      );
+    });
+    it('shows summary display', () => {
+      component.ngOnInit();
+      fixture.detectChanges();
+
+      expect(fixture.nativeElement.querySelector('h3').textContent).toEqual('Most Common:');
+    });
+  });
+}
+
+function ngInit_ManyIdeasDetected_ShowTopAndBottomThree() {
+  describe('more than 3 ideas detected', () => {
+    beforeEach(() => {
+      spyOn(TestBed.inject(CRaterService), 'getCRaterRubric').and.returnValue(
+        generateMockRubric(4, 4)
+      );
+      spyOn(TestBed.inject(SummaryService), 'getLatestClassmateStudentWork').and.returnValue(
+        generateMockStudentWork(4)
+      );
+    });
+    it('shows only top and bottom three ideas', () => {
+      component.ngOnInit();
+      fixture.detectChanges();
+      expect(fixture.nativeElement.querySelectorAll('#most-common-ideas > li').length).toEqual(3);
+      expect(fixture.nativeElement.querySelectorAll('#least-common-ideas > li').length).toEqual(3);
+    });
+  });
+}
+
+function generateMockRubric(numIdeas: number, numDetected: number): CRaterRubric {
+  const ideas = [];
+  for (let i = 0; i < numIdeas; i++) {
+    const idea = new CRaterIdea('idea ' + (i + 1), numDetected > 0 ? true : false);
+    ideas.push(idea);
+    numDetected--;
+  }
+  return new CRaterRubric({ ideas: ideas });
+}
+
+function generateMockStudentWork(numIdeasDetected: number): Observable<ComponentState[]> {
+  const ideas = [];
+  for (let i = 0; i < numIdeasDetected; i++) {
+    ideas.push({ name: 'idea ' + (i + 1), detected: true });
+  }
+  return of([
+    new ComponentState({
+      workgroupId: 1,
+      studentData: { responses: [{ ideas: ideas }] }
+    })
+  ]);
+}
---
+++ src/assets/wise5/directives/teacher-summary-display/dialog-guidance-teacher-summary-display.component.ts
@@ -0,0 +1,121 @@
+import { AnnotationService } from '../../services/annotationService';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ComponentState } from '../../../../app/domain/componentState';
+import { ConfigService } from '../../services/configService';
+import { CRaterIdea } from '../../components/common/cRater/CRaterIdea';
+import { CRaterRubric } from '../../components/common/cRater/CRaterRubric';
+import { CRaterService } from '../../services/cRaterService';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { SummaryService } from '../../components/summary/summaryService';
+import { TeacherDataService } from '../../services/teacherDataService';
+import { TeacherProjectService } from '../../services/teacherProjectService';
+import { TeacherSummaryDisplayComponent } from './teacher-summary-display.component';
+
+@Component({
+  imports: [CommonModule, MatCardModule, MatIconModule],
+  selector: 'dialog-guidance-teacher-summary-display',
+  styles: `
+    h3 {
+      margin-bottom: 8px;
+    }
+
+    .idea {
+      @apply px-2 py-1 rounded-md bg-gray-100 my-1 text-sm;
+    }
+
+    .mat-icon {
+      vertical-align: middle;
+    }
+  `,
+  templateUrl: 'dialog-guidance-teacher-summary-display.component.html'
+})
+export class DialogGuidanceTeacherSummaryDisplayComponent extends TeacherSummaryDisplayComponent {
+  protected allIdeas: { id: string; text: string; count: number }[] = [];
+  protected ideaCountMap: Map<string, Set<number>> = new Map<string, Set<number>>();
+  protected leastCommonIdeas: { id: string; text: string; count: number }[] = [];
+  protected mostCommonIdeas: { id: string; text: string; count: number }[] = [];
+  private rubric: CRaterRubric;
+  protected seeAllIdeas: boolean;
+
+  constructor(
+    protected annotationService: AnnotationService,
+    protected configService: ConfigService,
+    private cRaterService: CRaterService,
+    protected dataService: TeacherDataService,
+    protected projectService: TeacherProjectService,
+    protected summaryService: SummaryService
+  ) {
+    super(annotationService, configService, dataService, projectService, summaryService);
+  }
+
+  ngOnInit(): void {
+    this.rubric = this.cRaterService.getCRaterRubric(this.nodeId, this.componentId);
+    this.getLatestWork().subscribe((componentStates) => {
+      this.extractIdeas(componentStates);
+      this.allIdeas = this.getAllIdeas();
+      if (!this.allIdeas.some((idea) => this.ideaCountMap.get(idea.id)?.size > 0)) {
+        this.doRender = false;
+      } else {
+        const sortedIdeas = this.sortIdeas();
+        this.mostCommonIdeas = [...sortedIdeas].splice(0, 3);
+        this.leastCommonIdeas = [...sortedIdeas]
+          .splice(sortedIdeas.length - 3, sortedIdeas.length)
+          .reverse();
+      }
+    });
+  }
+
+  private getAllIdeas(): { id: string; text: string; count: number }[] {
+    return this.rubric.getIdeas().map((idea) => ({
+      id: idea.name,
+      text: this.useIdeaTextOrId(idea.name, idea.text),
+      count: this.ideaCountMap.get(idea.name)?.size ?? 0
+    }));
+  }
+
+  private useIdeaTextOrId(id: string, text: string): string {
+    return text ?? 'idea ' + id;
+  }
+
+  private extractIdeas(componentStates: ComponentState[]): void {
+    componentStates.forEach((componentState) =>
+      this.getDetectedIdeas(componentState).forEach((idea) => {
+        if (this.ideaCountMap.has(idea.name)) {
+          this.ideaCountMap.get(idea.name).add(componentState.workgroupId);
+        } else {
+          this.ideaCountMap.set(idea.name, new Set([componentState.workgroupId]));
+        }
+      })
+    );
+  }
+
+  private getDetectedIdeas(componentState: ComponentState): CRaterIdea[] {
+    return componentState.studentData.responses.flatMap(
+      (response) =>
+        response.ideas
+          ?.filter((idea) => idea.detected)
+          .map((idea) => new CRaterIdea(idea.name, idea.detected)) ?? []
+    );
+  }
+
+  private sortIdeas(): { id: string; text: string; count: number }[] {
+    return [...this.ideaCountMap.entries()]
+      .sort((a, b) => b[1].size - a[1].size)
+      .map((mapIterator) => ({
+        id: mapIterator[0],
+        text: this.getIdeaText(mapIterator[0]),
+        count: mapIterator[1].size
+      }));
+  }
+
+  private getIdeaText(id: string): string {
+    return this.useIdeaTextOrId(id, this.rubric.getIdea(id).text);
+  }
+
+  protected toggleSeeAllIdeas(event: Event): void {
+    event.preventDefault();
+    this.seeAllIdeas = !this.seeAllIdeas;
+  }
+}
