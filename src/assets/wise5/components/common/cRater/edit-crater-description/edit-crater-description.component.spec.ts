750888dcba45984f0e4463e291819ca614607641
Aaron Detre <aarondetre@gmail.com>
2025-04-15 15:28:43 -0700

    Added tests for EditCRaterInfo and EditCRaterDescription

--- src/assets/wise5/components/common/cRater/edit-crater-description/edit-crater-description.component.spec.ts
+++ src/assets/wise5/components/common/cRater/edit-crater-description/edit-crater-description.component.spec.ts
@@ -1,23 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { EditCRaterDescriptionComponent } from './edit-crater-description.component';
+import { TeacherProjectService } from '../../../../services/teacherProjectService';
+
+class MockTeacherProjectService {
+  nodeChanged() {}
+}
 
 describe('EditCRaterDescriptionComponent', () => {
   let component: EditCRaterDescriptionComponent;
   let fixture: ComponentFixture<EditCRaterDescriptionComponent>;
+  let nodeChangedSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditCRaterDescriptionComponent]
-    })
-    .compileComponents();
+      imports: [EditCRaterDescriptionComponent],
+      providers: [{ provide: TeacherProjectService, useClass: MockTeacherProjectService }]
+    }).compileComponents();
+    nodeChangedSpy = spyOn(TestBed.inject(TeacherProjectService), 'nodeChanged');
 
     fixture = TestBed.createComponent(EditCRaterDescriptionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should save changes to JSON', fakeAsync(() => {
+    const textarea = fixture.nativeElement.querySelector('textarea');
+    textarea.value = 'Testing';
+    textarea.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    tick();
+    fixture.whenStable().then(() => {
+      expect(nodeChangedSpy).toHaveBeenCalled();
+      expect(component.cRaterRubric.description).toEqual('Testing');
+    });
+  }));
+
+  it('should populate the input field with JSON data', () => {
+    component.cRaterRubric.description = 'Test';
+    fixture.detectChanges();
+    expect(
+      fixture.nativeElement.querySelector('textarea').getAttribute('ng-reflect-model')
+    ).toEqual('Test');
   });
 });
--- src/assets/wise5/components/common/cRater/edit-crater-info/edit-crater-info.component.spec.ts
+++ src/assets/wise5/components/common/cRater/edit-crater-info/edit-crater-info.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditCRaterInfoComponent } from './edit-crater-info.component';
+import { TeacherProjectService } from '../../../../services/teacherProjectService';
+import { CRaterRubric } from '../CRaterRubric';
+
+class MockTeacherProjectService {
+  nodeChanged() {}
+}
 
 describe('EditCRaterInfoComponent', () => {
   let component: EditCRaterInfoComponent;
@@ -8,16 +14,23 @@ describe('EditCRaterInfoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditCRaterInfoComponent]
-    })
-    .compileComponents();
+      imports: [EditCRaterInfoComponent],
+      providers: [{ provide: TeacherProjectService, useClass: MockTeacherProjectService }]
+    }).compileComponents();
+    spyOn(TestBed.inject(TeacherProjectService), 'nodeChanged');
 
     fixture = TestBed.createComponent(EditCRaterInfoComponent);
     component = fixture.componentInstance;
+    component.cRaterRubric = new CRaterRubric({ description: '', ideas: [] });
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have a description section and idea descriptions section', () => {
+    expect(
+      fixture.nativeElement.querySelectorAll('edit-crater-description').length
+    ).toBeGreaterThan(0);
+    expect(
+      fixture.nativeElement.querySelectorAll('edit-crater-idea-descriptions').length
+    ).toBeGreaterThan(0);
   });
 });
