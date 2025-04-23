fb4914db1a04c63aa119c27f44434a08d159ca79
Aaron Detre <aarondetre@gmail.com>
2025-04-23 16:00:56 -0700

    Test commit to see whether messages are run when tests fail (non-develop)

--- src/assets/wise5/components/openResponse/edit-open-response-advanced/edit-open-response-advanced.component.spec.ts
+++ src/assets/wise5/components/openResponse/edit-open-response-advanced/edit-open-response-advanced.component.spec.ts
@@ -106,7 +106,7 @@ describe('EditOpenResponseAdvancedComponent', () => {
 function enableCRaterClicked() {
   describe('enableCRaterClicked', () => {
     it('should handle enable CRater clicked', () => {
-      // TEST
+      expect(component.componentContent.cRater).toEqual(component.createCRaterObject());
       fixture.nativeElement.querySelectorAll('.checkbox').item(1).click();
       expect(component.componentContent.enableCRater);
     });
