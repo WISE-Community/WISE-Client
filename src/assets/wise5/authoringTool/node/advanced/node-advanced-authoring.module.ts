d0d855b176d2b79470a1e0d7907354badb87049d
Hiroki Terashima <honchikun@gmail.com>
2025-07-07 14:32:48 -0700

    Convert NodeAdvancedConstraintAuthoringComponent to standalone and clean up code

--- src/assets/wise5/authoringTool/node/advanced/node-advanced-authoring.module.ts
+++ src/assets/wise5/authoringTool/node/advanced/node-advanced-authoring.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
-import { NodeAdvancedConstraintAuthoringComponent } from './constraint/node-advanced-constraint-authoring.component';
 import { NodeAdvancedGeneralAuthoringComponent } from './general/node-advanced-general-authoring.component';
 import { NodeAdvancedJsonAuthoringComponent } from './json/node-advanced-json-authoring.component';
 import { NodeAdvancedAuthoringComponent } from './node-advanced-authoring/node-advanced-authoring.component';
 import { NodeAdvancedPathAuthoringComponent } from './path/node-advanced-path-authoring.component';
 import { StudentTeacherCommonModule } from '../../../../../app/student-teacher-common.module';
-import { NodeConstraintAuthoringComponent } from '../../constraint/node-constraint-authoring/node-constraint-authoring.component';
-import { EditNodeRubricComponent } from '../editRubric/edit-node-rubric.component';
 import { RouterModule } from '@angular/router';
 import { TranslatableRichTextEditorComponent } from '../../components/translatable-rich-text-editor/translatable-rich-text-editor.component';
 import { RequiredErrorLabelComponent } from './required-error-label/required-error-label.component';
@@ -15,23 +12,18 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     NodeAdvancedAuthoringComponent,
-    NodeAdvancedConstraintAuthoringComponent,
     NodeAdvancedGeneralAuthoringComponent,
     NodeAdvancedJsonAuthoringComponent,
     NodeAdvancedPathAuthoringComponent
   ],
   exports: [
-    EditNodeRubricComponent,
     NodeAdvancedAuthoringComponent,
-    NodeAdvancedConstraintAuthoringComponent,
     NodeAdvancedGeneralAuthoringComponent,
     NodeAdvancedJsonAuthoringComponent,
     NodeAdvancedPathAuthoringComponent
   ],
   imports: [
     CommonModule,
-    EditNodeRubricComponent,
-    NodeConstraintAuthoringComponent,
     RequiredErrorLabelComponent,
     RouterModule,
     StudentTeacherCommonModule,
