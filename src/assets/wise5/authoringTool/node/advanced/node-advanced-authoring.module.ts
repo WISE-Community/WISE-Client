88a3c790b6186db2f8b8a68d0131b2e7abd3b7ab
Hiroki Terashima <honchikun@gmail.com>
2025-07-11 16:24:07 -0700

    Convert NodeAdvancedPathAuthoringComponent to standalone and clean up code

--- src/assets/wise5/authoringTool/node/advanced/node-advanced-authoring.module.ts
+++ src/assets/wise5/authoringTool/node/advanced/node-advanced-authoring.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { NodeAdvancedAuthoringComponent } from './node-advanced-authoring/node-advanced-authoring.component';
-import { NodeAdvancedPathAuthoringComponent } from './path/node-advanced-path-authoring.component';
 import { StudentTeacherCommonModule } from '../../../../../app/student-teacher-common.module';
 import { RouterModule } from '@angular/router';
 import { TranslatableRichTextEditorComponent } from '../../components/translatable-rich-text-editor/translatable-rich-text-editor.component';
@@ -8,8 +7,8 @@ import { RequiredErrorLabelComponent } from './required-error-label/required-err
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [NodeAdvancedAuthoringComponent, NodeAdvancedPathAuthoringComponent],
-  exports: [NodeAdvancedAuthoringComponent, NodeAdvancedPathAuthoringComponent],
+  declarations: [NodeAdvancedAuthoringComponent],
+  exports: [NodeAdvancedAuthoringComponent],
   imports: [
     CommonModule,
     RequiredErrorLabelComponent,
