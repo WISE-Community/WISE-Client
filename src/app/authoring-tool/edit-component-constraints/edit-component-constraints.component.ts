7c5b722bfd3100cd6fb3651927e5d57bef6cfc04
Hiroki Terashima <honchikun@gmail.com>
2025-07-11 18:03:29 -0700

    Remove FlexLayoutModule dep from EditComponentConstraintsComponent and clean up code

--- src/app/authoring-tool/edit-component-constraints/edit-component-constraints.component.ts
+++ src/app/authoring-tool/edit-component-constraints/edit-component-constraints.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ConstraintsAuthoringComponent } from '../../../assets/wise5/authoringTool/constraint/constraints-authoring/constraints-authoring.component';
 import { CommonModule } from '@angular/common';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,18 +9,17 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { ComponentContent } from '../../../assets/wise5/common/ComponentContent';
 
 @Component({
-    imports: [
-        CommonModule,
-        ComponentConstraintAuthoringComponent,
-        FlexLayoutModule,
-        MatButtonModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatTooltipModule
-    ],
-    selector: 'edit-component-constraints',
-    styleUrl: './edit-component-constraints.component.scss',
-    templateUrl: './edit-component-constraints.component.html'
+  imports: [
+    CommonModule,
+    ComponentConstraintAuthoringComponent,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatTooltipModule
+  ],
+  selector: 'edit-component-constraints',
+  styleUrl: './edit-component-constraints.component.scss',
+  templateUrl: './edit-component-constraints.component.html'
 })
 export class EditComponentConstraintsComponent extends ConstraintsAuthoringComponent {
   @Input() componentContent: ComponentContent;
