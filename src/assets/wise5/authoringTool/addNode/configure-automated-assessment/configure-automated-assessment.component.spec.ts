d3612e94a97be6270cb4034db86a8ad2050da237
Hiroki Terashima <honchikun@gmail.com>
2025-07-30 15:02:13 -0700

    Clean up ConfigureAutomatedAssessmentComponent

--- src/assets/wise5/authoringTool/addNode/configure-automated-assessment/configure-automated-assessment.component.spec.ts
+++ src/assets/wise5/authoringTool/addNode/configure-automated-assessment/configure-automated-assessment.component.spec.ts
@@ -1,13 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ConfigureAutomatedAssessmentComponent } from './configure-automated-assessment.component';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { StudentTeacherCommonServicesModule } from '../../../../../app/student-teacher-common-services.module';
-import { CopyNodesService } from '../../../services/copyNodesService';
-import { TeacherProjectService } from '../../../services/teacherProjectService';
-import { InsertNodesService } from '../../../services/insertNodesService';
-import { InsertFirstNodeInBranchPathService } from '../../../services/insertFirstNodeInBranchPathService';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { MockProviders } from 'ng-mocks';
+import { ConfigService } from '../../../services/configService';
+import { CopyNodesService } from '../../../services/copyNodesService';
+import { InsertFirstNodeInBranchPathService } from '../../../services/insertFirstNodeInBranchPathService';
+import { InsertNodesService } from '../../../services/insertNodesService';
+import { TeacherProjectService } from '../../../services/teacherProjectService';
+import { ConfigureAutomatedAssessmentComponent } from './configure-automated-assessment.component';
 
 describe('ConfigureAutomatedAssessmentComponent', () => {
   let component: ConfigureAutomatedAssessmentComponent;
@@ -15,15 +14,15 @@ describe('ConfigureAutomatedAssessmentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [],
-      imports: [ConfigureAutomatedAssessmentComponent, StudentTeacherCommonServicesModule],
+      imports: [ConfigureAutomatedAssessmentComponent],
       providers: [
-        CopyNodesService,
-        InsertFirstNodeInBranchPathService,
-        InsertNodesService,
-        TeacherProjectService,
-        provideHttpClient(withInterceptorsFromDi()),
-        provideHttpClientTesting(),
+        MockProviders(
+          ConfigService,
+          CopyNodesService,
+          InsertFirstNodeInBranchPathService,
+          InsertNodesService,
+          TeacherProjectService
+        ),
         provideRouter([])
       ]
     }).compileComponents();
