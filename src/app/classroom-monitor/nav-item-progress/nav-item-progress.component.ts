ea2d964556172770a0fb40866bc6636d9179da69
Hiroki Terashima <honchikun@gmail.com>
2025-08-21 12:41:55 -0700

    Remove FlexLayout from NavItemProgressComponent

--- src/app/classroom-monitor/nav-item-progress/nav-item-progress.component.ts
+++ src/app/classroom-monitor/nav-item-progress/nav-item-progress.component.ts
@@ -1,13 +1,11 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
-  imports: [FlexLayoutModule, MatProgressBarModule, MatTooltipModule],
+  imports: [MatProgressBarModule, MatTooltipModule],
   selector: 'nav-item-progress',
-  styleUrl: 'nav-item-progress.component.scss',
   templateUrl: 'nav-item-progress.component.html'
 })
 export class NavItemProgressComponent {
