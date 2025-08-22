900a2ce3007ea6e30c02bb323ff2864ea8e41e84
Hiroki Terashima <honchikun@gmail.com>
2025-08-21 21:35:41 -0700

    Remove FlexLayout from PeerGroupAssignedWorkgroupsComponent (partial)

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/peer-group/peer-group-assigned-workgroups/peer-group-assigned-workgroups.component.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/peer-group/peer-group-assigned-workgroups/peer-group-assigned-workgroups.component.ts
@@ -1,24 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-    selector: 'peer-group-assigned-workgroups',
-    templateUrl: './peer-group-assigned-workgroups.component.html',
-    styleUrls: [
-        '../peer-group-workgroups-container/peer-group-workgroups-container.component.scss',
-        './peer-group-assigned-workgroups.component.scss'
-    ],
-    standalone: false
+  selector: 'peer-group-assigned-workgroups',
+  templateUrl: './peer-group-assigned-workgroups.component.html',
+  styleUrls: [
+    '../peer-group-workgroups-container/peer-group-workgroups-container.component.scss',
+    './peer-group-assigned-workgroups.component.scss'
+  ],
+  standalone: false
 })
-export class PeerGroupAssignedWorkgroupsComponent implements OnInit {
-  @Input()
-  groupings: any[];
-
-  @Output()
-  moveWorkgroup: EventEmitter<any> = new EventEmitter<any>();
-
-  constructor() {}
-
-  ngOnInit() {}
+export class PeerGroupAssignedWorkgroupsComponent {
+  @Input() groupings: any[];
+  @Output() moveWorkgroup: EventEmitter<any> = new EventEmitter<any>();
 
   emitMoveWorkgroup(event: any): void {
     this.moveWorkgroup.emit(event);
