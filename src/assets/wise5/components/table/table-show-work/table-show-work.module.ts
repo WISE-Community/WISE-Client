fab6297ac555bd74de363576f231bcfdefe3697d
Jonathan Lim-Breitbart <breity10@gmail.com>
2025-08-25 12:21:22 -0700

    Fix Data Explorer fields in TableShowWork

--- src/assets/wise5/components/table/table-show-work/table-show-work.module.ts
+++ src/assets/wise5/components/table/table-show-work/table-show-work.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { TableCommonModule } from '../table-common.module';
 import { TableShowWorkComponent } from './table-show-work.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [TableShowWorkComponent],
-  imports: [TableCommonModule],
+  imports: [MatFormFieldModule, MatSelectModule, TableCommonModule],
   exports: [TableShowWorkComponent]
 })
 export class TableShowWorkModule {}
