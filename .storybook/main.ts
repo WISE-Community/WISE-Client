ac1be5260eade0f6c8bb3743698015fceb65739d
Jonathan Lim-Breitbart <breity10@gmail.com>
2025-09-12 15:23:56 -0700

    feat(Design System): Upgrade to Storybook 9

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,10 +4,9 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
-    '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs'
   ],
   framework: {
     name: '@storybook/angular',
