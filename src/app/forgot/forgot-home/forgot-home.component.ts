b9bf812c6a7b89663055ee77fed0842e37132b43
Hiroki Terashima <honchikun@gmail.com>
2025-09-15 10:41:31 -0700

    Remove FlexLayoutModule dep from HeaderLinksComponent, FooterComponent, HeaderAccountMenuComponent, AnnouncementComponent, ForgotHomeComponent, HelpHomeComponent, HeaderComponent

--- src/app/forgot/forgot-home/forgot-home.component.ts
+++ src/app/forgot/forgot-home/forgot-home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { CallToActionComponent } from '../../modules/shared/call-to-action/call-to-action.component';
 
 @Component({
-    imports: [CallToActionComponent, FlexLayoutModule],
-    styleUrl: './forgot-home.component.scss',
-    templateUrl: './forgot-home.component.html'
+  imports: [CallToActionComponent],
+  styleUrl: './forgot-home.component.scss',
+  templateUrl: './forgot-home.component.html'
 })
 export class ForgotHomeComponent {}
--- src/app/help/help-home/help-home.component.ts
+++ src/app/help/help-home/help-home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CallToActionComponent } from '../../modules/shared/call-to-action/call-to-action.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
-    imports: [CallToActionComponent, FlexLayoutModule],
-    selector: 'app-help-home',
-    styleUrl: './help-home.component.scss',
-    templateUrl: './help-home.component.html'
+  imports: [CallToActionComponent],
+  selector: 'app-help-home',
+  styleUrl: './help-home.component.scss',
+  templateUrl: './help-home.component.html'
 })
 export class HelpHomeComponent {}
