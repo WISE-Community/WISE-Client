d3b95038327e7e9bd0dc4288ac3474939ae4c04c
Hiroki Terashima <honchikun@gmail.com>
2025-09-16 14:37:36 -0700

    Remove FlexLayout from PeerChatStudentModule and clean up related files

--- src/assets/wise5/components/peerChat/peer-chat-student/peer-chat-student.module.ts
+++ src/assets/wise5/components/peerChat/peer-chat-student/peer-chat-student.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { StudentComponentModule } from '../../../../../app/student/student.component.module';
 import { PeerChatModule } from '../peer-chat.module';
 import { PeerChatStudentComponent } from './peer-chat-student.component';
 
 @NgModule({
   declarations: [PeerChatStudentComponent],
-  imports: [CommonModule, FlexLayoutModule, PeerChatModule, StudentComponentModule],
+  imports: [CommonModule, PeerChatModule, StudentComponentModule],
   exports: [PeerChatStudentComponent]
 })
 export class PeerChatStudentModule {}
