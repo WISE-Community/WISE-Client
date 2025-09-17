d2dea31cf554cf5daf11fd3b8c69d789b2b0e3ea
Hiroki Terashima <honchikun@gmail.com>
2025-09-17 08:43:02 -0700

    Remove FlexLayout from GradingStepToolsComponent and clean up related files

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/grading-step-tools/grading-step-tools.component.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/grading-step-tools/grading-step-tools.component.ts
@@ -5,7 +5,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -20,7 +19,6 @@ import { ActivatedRoute, Router } from '@angular/router';
   encapsulation: ViewEncapsulation.None,
   imports: [
     CommonModule,
-    FlexLayoutModule,
     FormsModule,
     MatButtonModule,
     MatFormFieldModule,
@@ -31,8 +29,8 @@ import { ActivatedRoute, Router } from '@angular/router';
     NodeIconComponent
   ],
   selector: 'grading-step-tools',
-  templateUrl: '../../../common/stepTools/step-tools.component.html',
-  styleUrl: '../../../common/stepTools/step-tools.component.scss'
+  styleUrl: '../../../common/stepTools/step-tools.component.scss',
+  templateUrl: '../../../common/stepTools/step-tools.component.html'
 })
 export class GradingStepToolsComponent extends StepToolsComponent {
   constructor(
