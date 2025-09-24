a697c21a2066e0c601a8059952a2e9ac9d52e34b
Hiroki Terashima <honchikun@gmail.com>
2025-09-24 13:03:50 -0700

    Convert StudentAssetsComponent to standalone and clean up related files.

--- src/assets/wise5/vle/studentAsset/student-assets-dialog/student-assets-dialog.module.ts
+++ src/assets/wise5/vle/studentAsset/student-assets-dialog/student-assets-dialog.module.ts
@@ -2,12 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { StudentAssetsModule } from '../student-assets/student-assets-module';
 import { StudentAssetsDialogComponent } from './student-assets-dialog.component';
+import { StudentAssetsComponent } from '../student-assets/student-assets.component';
 
 @NgModule({
   declarations: [StudentAssetsDialogComponent],
-  imports: [CommonModule, MatButtonModule, MatDialogModule, StudentAssetsModule],
+  imports: [CommonModule, MatButtonModule, MatDialogModule, StudentAssetsComponent],
   exports: [StudentAssetsDialogComponent]
 })
 export class StudentAssetsDialogModule {}
