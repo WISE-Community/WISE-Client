1c9d10701f4798bd6e6a853e50bcb571ab649a29
Jonathan Lim-Breitbart <breity10@gmail.com>
2025-11-03 12:58:56 -0800

    feat(Design System): Customize Card, create stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  staticDirs: ['../src/stories/assets'],
   addons: [
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
