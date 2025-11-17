bdcf5fe0fd4855cef97e28ef8c4cabf3c472d602
Hiroki Terashima <honchikun@gmail.com>
2025-11-17 14:56:11 -0800

    Remove PeerGroupGradingModule. Redundant.

--- src/app/teacher/classroom-monitor.module.ts
+++ src/app/teacher/classroom-monitor.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
-import { PeerGroupGradingModule } from './peer-group-grading.module';
 import { ComponentStudentModule } from '../../assets/wise5/components/component/component-student.module';
 import { StudentTeacherCommonServicesModule } from '../student-teacher-common-services.module';
 
 @NgModule({
-  imports: [ComponentStudentModule, PeerGroupGradingModule, StudentTeacherCommonServicesModule]
+  imports: [ComponentStudentModule, StudentTeacherCommonServicesModule]
 })
 export class ClassroomMonitorModule {}
