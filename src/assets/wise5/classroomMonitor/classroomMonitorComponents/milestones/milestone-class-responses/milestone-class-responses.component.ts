e9e6e60a8ba5ef21982c107818cb01bc375e4eaf
Hiroki Terashima <honchikun@gmail.com>
2025-11-17 17:27:29 -0800

    Replace ngFor with new syntax

--- src/assets/wise5/classroomMonitor/classroomMonitorComponents/milestones/milestone-class-responses/milestone-class-responses.component.ts
+++ src/assets/wise5/classroomMonitor/classroomMonitorComponents/milestones/milestone-class-responses/milestone-class-responses.component.ts
@@ -8,7 +8,7 @@ import { Notification } from '../../../../../../app/domain/notification';
 import { MatList, MatListItem } from '@angular/material/list';
 import { WorkgroupSelectAutocompleteComponent } from '../../../../../../app/classroom-monitor/workgroup-select/workgroup-select-autocomplete/workgroup-select-autocomplete.component';
 import { MatButton } from '@angular/material/button';
-import { NgClass, NgFor } from '@angular/common';
+import { NgClass } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltip } from '@angular/material/tooltip';
 import { IntersectionObserverModule } from '@ng-web-apis/intersection-observer';
@@ -24,7 +24,6 @@ import { MilestoneWorkgroupItemComponent } from '../milestone-workgroup-item/mil
     NgClass,
     MatTooltip,
     IntersectionObserverModule,
-    NgFor,
     MilestoneWorkgroupItemComponent
   ],
   encapsulation: ViewEncapsulation.None,
