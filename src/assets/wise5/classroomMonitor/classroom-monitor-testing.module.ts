ab81de250cdc0c197fb49250be0c07f7cc3d02dd
Hiroki Terashima <honchikun@gmail.com>
2025-11-17 16:29:47 -0800

    Remove StudentTeacherCommonServicesModule dep from ClassroomMonitorTestingModule

--- src/assets/wise5/classroomMonitor/classroom-monitor-testing.module.ts
+++ src/assets/wise5/classroomMonitor/classroom-monitor-testing.module.ts
@@ -3,10 +3,8 @@ import { ClassroomStatusService } from '../services/classroomStatusService';
 import { TeacherDataService } from '../services/teacherDataService';
 import { TeacherProjectService } from '../services/teacherProjectService';
 import { TeacherWebSocketService } from '../services/teacherWebSocketService';
-import { StudentTeacherCommonServicesModule } from '../../../app/student-teacher-common-services.module';
 import { MatDialogModule } from '@angular/material/dialog';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MilestoneService } from '../services/milestoneService';
 import { TeacherPeerGroupService } from '../services/teacherPeerGroupService';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -15,19 +13,47 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { TeacherPauseScreenService } from '../services/teacherPauseScreenService';
 import { RunStatusService } from '../services/runStatusService';
 import { GradingNodeService } from '../services/gradingNodeService';
+import { ConfigService } from '../services/configService';
+import { NotebookService } from '../services/notebookService';
+import { NotificationService } from '../services/notificationService';
+import { AnnotationService } from '../services/annotationService';
+import { BranchService } from '../services/branchService';
+import { ProjectService } from '../services/projectService';
+import { PathService } from '../services/pathService';
+import { ComponentServiceLookupService } from '../services/componentServiceLookupService';
+import { MockProvider } from 'ng-mocks';
+import { AchievementService } from '../services/achievementService';
+import { StudentAssetService } from '../services/studentAssetService';
+import { NodeService } from '../services/nodeService';
+import { SessionService } from '../services/sessionService';
+import { StudentNodeService } from '../services/studentNodeService';
+import { ConstraintService } from '../services/constraintService';
+import { StudentDataService } from '../services/studentDataService';
+import { NodeStatusService } from '../services/nodeStatusService';
 
 @NgModule({
-  imports: [
-    BrowserAnimationsModule,
-    MatDialogModule,
-    MatSnackBarModule,
-    StudentTeacherCommonServicesModule
-  ],
+  imports: [MatDialogModule, MatSnackBarModule],
   providers: [
+    AnnotationService,
+    MockProvider(AchievementService),
+    BranchService,
+    MockProvider(ComponentServiceLookupService),
+    ConfigService,
+    MockProvider(ConstraintService),
     ClassroomStatusService,
     GradingNodeService,
     MilestoneService,
     MilestoneReportService,
+    { provide: NodeService, useClass: StudentNodeService },
+    NodeStatusService,
+    NotebookService,
+    NotificationService,
+    PathService,
+    ProjectService,
+    SessionService,
+    StudentAssetService,
+    StudentDataService,
+    StudentNodeService,
     TeacherDataService,
     TeacherPauseScreenService,
     TeacherPeerGroupService,
