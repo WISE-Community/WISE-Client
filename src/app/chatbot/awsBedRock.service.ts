55d9375b8a1584c760e1c941ecbd9b5d7c1f4610
Jonathan Lim-Breitbart <breity10@gmail.com>
2026-01-28 17:18:32 -0800

    Revert AwsBedRockService committed by mistake

--- src/app/chatbot/awsBedRock.service.ts
+++ src/app/chatbot/awsBedRock.service.ts
@@ -4,7 +4,7 @@ import { ChatService } from './chat.service';
 @Injectable({ providedIn: 'root' })
 export class AwsBedRockService extends ChatService {
   protected chatEndpoint = '/api/aws-bedrock/chat';
-  protected model: string = 'google.gemma-3-27b-it';
+  protected model: string = 'openai.gpt-oss-20b-1:0';
 
   processResponse(response: string): string {
     return response.replace(/<reasoning>.*?<\/reasoning>/gs, '');
