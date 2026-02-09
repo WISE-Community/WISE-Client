7dcd38b054979403671645b239dd010ed7e2677d
Jonathan Lim-Breitbart <breity10@gmail.com>
2026-02-09 09:57:10 -0800

    feat(Teacher Tools): Show detected ideas for Open Response grading view (#2267)
    Co-authored-by: Hiroki Terashima <honchikun@gmail.com>

--- src/assets/wise5/components/dialogGuidance/dialog-responses/dialog-responses.component.ts
+++ src/assets/wise5/components/dialogGuidance/dialog-responses/dialog-responses.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { ComputerAvatar } from '../../../common/computer-avatar/ComputerAvatar';
 import { CRaterRubric } from '../../common/cRater/CRaterRubric';
-import { CRaterRubricComponent } from '../../common/cRater/crater-rubric/crater-rubric.component';
 import { DetectedIdeasComponent } from '../detected-ideas/detected-ideas.component';
 import { DialogResponse } from '../DialogResponse';
 import { DialogResponseComponent } from '../dialog-response/dialog-response.component';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { RubricEventService } from '../../common/cRater/crater-rubric/RubricEventService';
+import { ShowCRaterRubricComponent } from '../../../../../app/classroom-monitor/show-crater-rubric/show-crater-rubric.component';
 
 @Component({
-  imports: [DetectedIdeasComponent, DialogResponseComponent, MatIconModule],
+  imports: [
+    DetectedIdeasComponent,
+    DialogResponseComponent,
+    MatIconModule,
+    ShowCRaterRubricComponent
+  ],
   selector: 'dialog-responses',
   styleUrl: './dialog-responses.component.scss',
   templateUrl: './dialog-responses.component.html'
@@ -18,36 +21,7 @@ import { RubricEventService } from '../../common/cRater/crater-rubric/RubricEven
 export class DialogResponsesComponent {
   @Input() computerAvatar: ComputerAvatar;
   @Input() cRaterRubric: CRaterRubric;
-  protected hasRubricData: boolean;
   @Input() isWaitingForComputerResponse: boolean;
   @Input() responses: DialogResponse[] = [];
-  private rubricDialog: MatDialogRef<CRaterRubricComponent>;
   @Input() showDetectedIdeas: boolean = false;
-
-  constructor(
-    protected dialog: MatDialog,
-    protected rubricEventService: RubricEventService
-  ) {}
-
-  ngOnInit(): void {
-    this.hasRubricData = this.cRaterRubric?.hasRubricData() ?? false;
-  }
-
-  ngOnDestroy(): void {
-    if (this.rubricEventService.getIsRubricOpen()) {
-      this.rubricDialog.close();
-    }
-  }
-
-  protected openIdeasRubric(): void {
-    if (!this.rubricEventService.getIsRubricOpen()) {
-      this.rubricDialog = this.dialog.open(CRaterRubricComponent, {
-        panelClass: 'dialog-sm',
-        position: { right: '0', bottom: '0' },
-        hasBackdrop: false,
-        data: this.cRaterRubric,
-        autoFocus: false
-      });
-    }
-  }
 }
