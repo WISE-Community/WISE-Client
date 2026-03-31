a6ccd165a9c354fb3d40ed6e4026334b506d2dab
Jonathan Lim-Breitbart <breity10@gmail.com>
2026-03-31 00:01:57 -0700

    fix(LabelService): Replace jQuery selector (#2295)
    * fix(LabelService): Replace jQuery selector
    
    * Add null check tfor safety and to fix tests

--- src/assets/wise5/components/label/labelService.ts
+++ src/assets/wise5/components/label/labelService.ts
@@ -300,7 +300,10 @@ export class LabelService extends ComponentService {
     canvas.selection = false;
     canvas.hoverCursor = 'pointer';
     this.setCanvasDimension(canvas, width, height);
-    $('#canvasParent_' + canvasId).css('height', height + 2);
+    const canvasParent = document.getElementById('canvasParent_' + canvasId);
+    if (canvasParent != null) {
+      canvasParent.style.height = height + 2 + 'px';
+    }
     return canvas;
   }
 
