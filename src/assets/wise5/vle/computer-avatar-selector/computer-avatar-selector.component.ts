1838644e61d85b0bede96b4310946d4e8ed08a15
Hiroki Terashima <honchikun@gmail.com>
2026-04-02 07:49:06 -0700

    style(Computer Avatar Selector): Improve selection flow (#2293)
    Co-authored-by: Jonathan Lim-Breitbart <breity10@gmail.com>

--- src/assets/wise5/vle/computer-avatar-selector/computer-avatar-selector.component.ts
+++ src/assets/wise5/vle/computer-avatar-selector/computer-avatar-selector.component.ts
@@ -2,14 +2,13 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatDividerModule } from '@angular/material/divider';
 import { ComputerAvatar } from '../../common/computer-avatar/ComputerAvatar';
 import { ComputerAvatarService } from '../../services/computerAvatarService';
 import { ComputerAvatarSettings } from '../../common/computer-avatar/ComputerAvatarSettings';
 
 @Component({
-  imports: [FormsModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatDividerModule],
+  imports: [FormsModule, MatButtonModule, MatCardModule, MatDividerModule],
   selector: 'computer-avatar-selector',
   styleUrl: './computer-avatar-selector.component.scss',
   templateUrl: './computer-avatar-selector.component.html'
