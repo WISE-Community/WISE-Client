7479c3a5f01ce1e3e827f0c80d650b493417ddde
Hiroki Terashima <honchikun@gmail.com>
2026-05-06 16:38:43 -0700

    feat(Authoring): Use slider to set activity width

--- src/app/authoring-tool/edit-component-width/edit-component-width.component.ts
+++ src/app/authoring-tool/edit-component-width/edit-component-width.component.ts
@@ -1,20 +1,45 @@
 import { Component } from '@angular/core';
 import { EditComponentFieldComponent } from '../edit-component-field.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
-  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatSliderModule],
   selector: 'edit-component-width',
-  template: `<mat-form-field>
-    <mat-label i18n>Activity Width</mat-label>
-    <input
-      matInput
-      type="number"
-      [(ngModel)]="componentContent.componentWidth"
-      (ngModelChange)="inputChanged.next($event)"
-    />
-  </mat-form-field> `
+  template: `<span i18n>Activity width:</span>
+    <mat-slider
+      min="0"
+      [max]="possibleValues.length - 1"
+      step="1"
+      discrete
+      showTickMarks
+      [displayWith]="formatLabel"
+      style="min-width: 300px;"
+    >
+      <input matSliderThumb (input)="onSliderChange($event)" [value]="selectedIndex" />
+    </mat-slider>
+    <span>{{ selectedValue }}%</span>
+    <p i18n>
+      *Setting the activities' widths determines how they appear on the screen. For example, if two
+      adjacent activities' widths are both set to 50%, they will appear side-by-side.
+    </p>`
 })
-export class EditComponentWidthComponent extends EditComponentFieldComponent {}
+export class EditComponentWidthComponent extends EditComponentFieldComponent {
+  protected possibleValues = [25, 33, 50, 66, 75, 100];
+  protected selectedIndex = 0;
+  protected selectedValue = this.possibleValues[0];
+
+  public override ngOnInit(): void {
+    super.ngOnInit();
+    this.selectedValue = this.componentContent.componentWidth ?? 100;
+    this.selectedIndex = this.possibleValues.indexOf(this.selectedValue);
+  }
+
+  protected formatLabel = (index: number): string => `${this.possibleValues[index]}%`;
+
+  protected onSliderChange(event: any): void {
+    this.selectedValue = this.possibleValues[event.target.value];
+    this.componentContent.componentWidth = this.selectedValue;
+    this.inputChanged.next(this.selectedValue);
+  }
+}
