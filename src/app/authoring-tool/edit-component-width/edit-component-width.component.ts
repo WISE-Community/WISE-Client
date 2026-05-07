cf88411c6f62b778bc60c23d832e64722c05f969
Hiroki Terashima <honchikun@gmail.com>
2026-05-07 16:34:02 -0700

    Replace mat-slider with mat-button-toggle

--- src/app/authoring-tool/edit-component-width/edit-component-width.component.ts
+++ src/app/authoring-tool/edit-component-width/edit-component-width.component.ts
@@ -1,45 +1,42 @@
 import { Component } from '@angular/core';
-import { EditComponentFieldComponent } from '../edit-component-field.component';
 import { FormsModule } from '@angular/forms';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { EditComponentFieldComponent } from '../edit-component-field.component';
 
 @Component({
-  imports: [FormsModule, MatSliderModule],
+  imports: [FormsModule, MatButtonToggleModule, MatIconModule, MatTooltipModule],
   selector: 'edit-component-width',
-  template: `<span i18n>Activity width:</span>&nbsp;
-    <mat-slider
-      min="0"
-      [max]="possibleValues.length - 1"
-      step="1"
-      discrete
-      showTickMarks
-      [displayWith]="formatLabel"
-      style="min-width: 240px;"
+  template: `<span i18n>Activity width</span
+    ><mat-icon
+      matTooltip="*Setting the activity's width determines how it appears on the screen. If two adjacent
+      activities are set to 50% each (or 67% and 33%, for example), they will appear side-by-side."
+      i18n-matTooltip
+      >help</mat-icon
+    ><br />
+    <mat-button-toggle-group
+      [(ngModel)]="selectedValue"
+      (change)="onWidthChange()"
+      aria-label="Activity width"
+      hideSingleSelectionIndicator="true"
     >
-      <input matSliderThumb (input)="onSliderChange($event)" [value]="selectedIndex" />
-    </mat-slider>
-    <span>{{ selectedValue }}%</span>
-    <p i18n>
-      *Setting the activity's width determines how it appears on the screen. If two adjacent
-      activities are set to 50% each (or 67% and 33%, for example), they will appear side-by-side.
-    </p>`
+      @for (value of possibleValues; track $index) {
+        <mat-button-toggle value="{{ value }}">{{ value }}%</mat-button-toggle>
+      }
+    </mat-button-toggle-group>`
 })
 export class EditComponentWidthComponent extends EditComponentFieldComponent {
   protected possibleValues = [25, 33, 50, 67, 75, 100];
-  protected selectedIndex = 0;
-  protected selectedValue = this.possibleValues[0];
+  protected selectedValue = '100';
 
   public override ngOnInit(): void {
     super.ngOnInit();
-    this.selectedValue = this.componentContent.componentWidth ?? 100;
-    this.selectedIndex = this.possibleValues.indexOf(this.selectedValue);
+    this.selectedValue = String(this.componentContent.componentWidth ?? 100);
   }
 
-  protected formatLabel = (index: number): string => `${this.possibleValues[index]}%`;
-
-  protected onSliderChange(event: any): void {
-    this.selectedValue = this.possibleValues[event.target.value];
-    this.componentContent.componentWidth = this.selectedValue;
-    this.inputChanged.next(this.selectedValue);
+  protected onWidthChange(): void {
+    this.componentContent.componentWidth = Number(this.selectedValue);
+    this.inputChanged.next(Number(this.selectedValue));
   }
 }
