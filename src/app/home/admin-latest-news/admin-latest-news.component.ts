807d109d4f205f740d12b2892a35ffe2be8770e8
Aaron Detre <aarondetre@gmail.com>
2026-06-05 12:07:06 -0700

    Created admin latest news component

---
+++ src/app/home/admin-latest-news/admin-latest-news.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-latest-news',
+  imports: [],
+  templateUrl: './admin-latest-news.component.html',
+  styleUrl: './admin-latest-news.component.scss'
+})
+export class AdminLatestNewsComponent {
+
+}
