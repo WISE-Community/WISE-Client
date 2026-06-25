e8d71bfb9b4154fa37348d6c268740746f6517b8
Aaron Detre <aarondetre@gmail.com>
2026-06-24 19:41:55 -0700

    Revert changes in NewsService test file

--- src/app/services/news.service.spec.ts
+++ src/app/services/news.service.spec.ts
@@ -3,19 +3,17 @@ import { TestBed } from '@angular/core/testing';
 import { NewsService } from './news.service';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { News } from '../domain/news';
 
-let service: NewsService;
 describe('NewsService', () => {
-  beforeEach(() => {
+  beforeEach(() =>
     TestBed.configureTestingModule({
       imports: [],
       providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
-    });
-    service = TestBed.inject(NewsService);
-  });
+    })
+  );
 
   it('should be created', () => {
+    const service: NewsService = TestBed.inject(NewsService);
     expect(service).toBeTruthy();
   });
 });
