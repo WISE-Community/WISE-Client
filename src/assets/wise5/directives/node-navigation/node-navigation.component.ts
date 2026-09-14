1ece457d311d07efb2fce053ea9299ea535e4341
Jonathan Lim-Breitbart <breity10@gmail.com>
2026-09-14 15:45:01 -0700

    feat(VLE): Add next button to bottom of step

--- src/assets/wise5/directives/node-navigation/node-navigation.component.ts
+++ src/assets/wise5/directives/node-navigation/node-navigation.component.ts
@@ -1,26 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { NodeService } from '../../services/nodeService';
 import { StudentDataService } from '../../services/studentDataService';
 import { MatButtonModule } from '@angular/material/button';
+import { Directionality } from '@angular/cdk/bidi';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   selector: 'node-navigation',
   templateUrl: './node-navigation.component.html'
 })
 export class NodeNavigationComponent implements OnInit {
   protected hasNextNode: boolean;
   protected hasPrevNode: boolean;
+  protected nextIcon: string;
+  protected prevIcon: string;
+  @Input() showPrevNodeNav: boolean = false;
   private subscriptions: Subscription = new Subscription();
 
   constructor(
     private dataService: StudentDataService,
+    protected dir: Directionality,
     private nodeService: NodeService
   ) {}
 
   ngOnInit(): void {
     this.checkPreviousAndNextNodes();
+    if (this.dir.value === 'rtl') {
+      this.nextIcon = 'chevron_left';
+      this.prevIcon = 'chevron_right';
+    } else {
+      this.nextIcon = 'chevron_right';
+      this.prevIcon = 'chevron_left';
+    }
     this.subscriptions.add(
       this.dataService.currentNodeChanged$.subscribe(() => {
         this.checkPreviousAndNextNodes();
